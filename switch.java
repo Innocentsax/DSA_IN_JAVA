008f156c5624681f34cf983f6d56f24ae8f82338
innocentsax <innocentcharlesudo@gmail.com>
2023-07-15 00:33:33 +0100

    Switch and nested switch statement

---
+++ switch.java
@@ -0,0 +1,55 @@
+package org.example.DSA;
+
+import java.util.Scanner;
+
+public class DSA8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+
+        switch (input) {
+            case "Mango":
+                System.out.println("King of fruits");
+                break;
+            case "Apple":
+                System.out.println("A sweet red fruit");
+                break;
+            case "Orange":
+                System.out.println("Round fruit");
+                break;
+            case "Grapes":
+                System.out.println("Small fruit");
+                break;
+            default:
+                System.out.println("Please enter a valid fruit");
+                break;
+
+        int day = scanner.nextInt();
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid Statement");
+        }
+
+    }
+}
