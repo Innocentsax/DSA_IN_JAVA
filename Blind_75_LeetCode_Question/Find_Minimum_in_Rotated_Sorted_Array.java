81bf0ec21cb5e236b03d1c50caaeeb4e02282707
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-07-30 01:09:30 +0100

    Find Minimum in Rotated Sorted Array

---
+++ Blind_75_LeetCode_Question/Find_Minimum_in_Rotated_Sorted_Array.java
@@ -0,0 +1,61 @@
+/*
+Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
+
+[4,5,6,7,0,1,2] if it was rotated 4 times.
+[0,1,2,4,5,6,7] if it was rotated 7 times.
+Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+
+Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+
+You must write an algorithm that runs in O(log n) time.
+
+ 
+
+Example 1:
+
+Input: nums = [3,4,5,1,2]
+Output: 1
+Explanation: The original array was [1,2,3,4,5] rotated 3 times.
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2]
+Output: 0
+Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.
+Example 3:
+
+Input: nums = [11,13,15,17]
+Output: 11
+Explanation: The original array was [11,13,15,17] and it was rotated 4 times. 
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 5000
+-5000 <= nums[i] <= 5000
+All the integers of nums are unique.
+nums is sorted and rotated between 1 and n times.
+*/
+
+class Solution {
+    public int findMin(int[] nums) {
+        if (nums.length == 1) return nums[0];
+        if (nums.length == 2) return Math.min(nums[0], nums[1]);
+        if (nums[0] < nums[nums.length - 1]) return nums[0];
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            // the array is decreasing at mid + 1
+            if (nums[mid] > nums[mid+1]) return nums[mid+1];
+            // the arrays is decreasing at mid
+            if (nums[mid-1] > nums[mid]) return nums[mid];
+            // discard the sorted part >> increasing part
+            if (nums[left] < nums[mid])
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return 0;
+    }
+}
