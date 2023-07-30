0e3cbf5b256b97bcd6e213ce17aa2917d818fb55
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-07-31 00:28:06 +0100

    Container With Most Water

---
+++ Blind_75_LeetCode_Question/Container_With_Most_Water.java
@@ -0,0 +1,40 @@
+/*
+You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+Return the maximum amount of water a container can store.
+Notice that you may not slant the container.
+
+Example 1:
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. 
+In this case, the max area of water (blue section) the container can contain is 49.
+
+Example 2:
+Input: height = [1,1]
+Output: 1
+
+Constraints:
+n == height.length
+2 <= n <= 105
+0 <= height[i] <= 104
+*/
+
+class Solution {
+    public int maxArea(int[] height) {
+        int ans = 0;
+        int left = 0, right = height.length - 1;
+        while (left < right) {
+            int min = Math.min(height[left], height[right]);
+            ans = Math.max(ans, min * (right - left));
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return ans;
+    }
+    
+}
