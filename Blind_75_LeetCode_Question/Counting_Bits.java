3e58bcb7667423c3b12c5603bf8edf3306855232
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-08-06 01:37:08 +0100

    Counting Bits

---
+++ Blind_75_LeetCode_Question/Counting_Bits.java
@@ -0,0 +1,44 @@
+/*
+Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), 
+ans[i] is the number of 1's in the binary representation of i.
+
+Example 1:
+Input: n = 2
+Output: [0,1,1]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+Example 2:
+
+Input: n = 5
+Output: [0,1,1,2,1,2]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+3 --> 11
+4 --> 100
+5 --> 101
+
+Constraints:
+0 <= n <= 105
+Follow up:
+It is very easy to come up with a solution with a runtime of O(n log n). Can you do it in linear time O(n) and possibly in a single pass?
+Can you do it without using any built-in function (i.e., like __builtin_popcount in C++)?
+*/
+
+class Solution {
+    public int[] countBits(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        int offset = 1;
+        for(int i = 1; i <= n; i++){
+            if(offset * 2 == i){// if current number "i" is power of 2
+                offset = i;
+            }
+            dp[i] = 1 + dp[i - offset];
+        }
+        return dp;
+    }// Time Complexity = 0(n), Space complexity = 0(n).
+}
