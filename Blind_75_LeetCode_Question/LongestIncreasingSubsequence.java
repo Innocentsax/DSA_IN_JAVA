b0526812bdead0f9a2a03e0a14e7092a3d84a62b
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-08-25 03:49:16 +0100

    Update LongestIncreasingSubsequence.java

--- Blind_75_LeetCode_Question/LongestIncreasingSubsequence.java
+++ Blind_75_LeetCode_Question/LongestIncreasingSubsequence.java
@@ -1,5 +1,29 @@
 /*
+Given an integer array nums, return the length of the longest strictly increasing 
+subsequence
+.
 
+ 
+
+Example 1:
+
+Input: nums = [10,9,2,5,3,7,101,18]
+Output: 4
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+Example 2:
+
+Input: nums = [0,1,0,3,2,3]
+Output: 4
+Example 3:
+
+Input: nums = [7,7,7,7,7,7,7]
+Output: 1
+ 
+
+Constraints:
+
+1 <= nums.length <= 2500
+-104 <= nums[i] <= 104
 */
 class Solution {
     public int lengthOfLIS(int[] nums) {
