c9d3eda1c330a7841f404603dd81b3f91942bceb
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-08-25 03:47:54 +0100

    Longest Increasing Subsequence

---
+++ Blind_75_LeetCode_Question/LongestIncreasingSubsequence.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length; //length of all given array
+        if(n == 1) return 1;
+        int[] arr = new int[n];
+        arr[0] = 1; // insert 1 as the base case
+        int ans = 0;
+        for(int i = 1; i<n; i++){
+            int len = 0;
+            for(int j = 0; j< i; j++){
+                if(nums[j] < nums[i]){
+                    len = Math.max(len, arr[j]);
+                }
+            }arr[i] = 1 +len;
+            ans = Math.max(ans, arr[i]);
+        }return ans;
+    }
+}
