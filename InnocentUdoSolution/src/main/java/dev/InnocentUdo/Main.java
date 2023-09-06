fdfc65ca9263e934335b427f395aa7ba231cf1aa
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-09-06 10:52:08 +0100

    Update

--- InnocentUdoSolution/src/main/java/dev/InnocentUdo/Main.java
+++ InnocentUdoSolution/src/main/java/dev/InnocentUdo/Main.java
@@ -2,6 +2,23 @@ package dev.InnocentUdo;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    int[] nums = {1, 3, 23, 9, 19};
+        int target = 233;
+        int ans = linearSearch(nums, target);
+        System.out.println(ans);
+    }
+
+    static int linearSearch(int[] arr, int target){
+        if (arr.length== 0){
+            return -1;
+        }
+            for (int index = 0; index < arr.length; index++) {
+                int element = arr[index];
+                if (element == target){
+                    return index;
+
+            }
+        }
+        return -1;
     }
 }
