acf0b2d01d7e3b32454906c65166ddba98cd64f5
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-25 12:00:47 +0100

    Linear Search find Minimum number

---
+++ DSA/src/main/java/dev/Innocent/LinearSearch/FIndMin.java
@@ -0,0 +1,33 @@
+package dev.Innocent.LinearSearch;
+
+public class FIndMin {
+    public static void main(String[] args) {
+        int[] arr = {18, 12, 3, 14, 5};
+        System.out.println(max(arr));
+    }
+    static int min(int[] arr){
+        int ans = arr[0];
+        if(arr.length == 0){
+            return -1;
+        }
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i] < ans){
+                ans = arr[i];
+            }
+        }
+        return ans;
+    }
+    
+    static int max(int[] arr){
+//        int ans = arr[0];
+        if(arr.length == 0){
+            return -1;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] > arr[0]){
+                arr[0] = arr[i];
+            }
+        }
+        return arr[0];
+    }
+}
