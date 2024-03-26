657c6cdfbb45abfaf0cbad59e550c79784dc3a0b
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-26 08:43:03 +0100

    Linear Search | Even Digits

--- DSA/src/main/java/dev/Innocent/LinearSearch/EvenDigits.java
+++ DSA/src/main/java/dev/Innocent/LinearSearch/EvenDigits.java
@@ -3,7 +3,7 @@ package dev.Innocent.LinearSearch;
 public class EvenDigits {
     public static void main(String[] args) {
         int[] nums = {12, 345, 2, 6, 7896};
-        System.out.println(findNumbers(nums));
+        System.out.println(findNumber3(nums));
     }
     static int findNumbers(int[] nums){
         int count = 0;
@@ -38,5 +38,26 @@ public class EvenDigits {
             num = num * -1;
         }
         return (int)(Math.log10(num)) + 1;
+    }
+
+    static int findNumber2(int[] nums){
+        int count = 0; // Initialize a counter variable to keep track of the count of numbers with even number of digits
+        for(int num : nums){ // Iterate through each element of the nums array
+            if((int)(Math.log10(num) + 1) % 2 == 0){ // Check if the number of digits in the current number is even
+                count++; // If the number of digits is even, increment the counter
+            }
+        }
+        return count; // Return the count of numbers with even number of digits
+    }
+
+    static int findNumber3(int[] nums){
+        int count = 0; // Initialize a counter variable to keep track of the count of numbers with even number of digits
+        for (int i = 0; i < nums.length; i++) {// Iterate through each element of the nums array using a normal for loop
+            // Check if the current number nums[i] has an even number of digits
+            if(nums[i] >= 10 && nums[i] < 100 || nums[i] >= 1000 && nums[i] < 9999 || nums[i] == 100000){
+                count++; // If the number of digits is even, increment the counter
+            }
+        }
+        return count; // Return the count of numbers with even number of digits
     }
 }
