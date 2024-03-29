9ec7b410419eaf7bd9820df74ae948ab7931fa35
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-29 14:58:59 +0100

    Binary Search | Search In Rotated Sorted Array

---
+++ DSA/src/main/java/dev/Innocent/BinarySearchQuestions/SearchInRotatedSortedArray.java
@@ -0,0 +1,69 @@
+package dev.Innocent.BinarySearchQuestions;
+// https://leetcode.com/problems/search-in-rotated-sorted-array/submissions/1217348738/
+public class SearchInRotatedSortedArray {
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+        System.out.println(findPivot(arr));
+    }
+    static int search(int[] nums, int target) {
+        int pivot = findPivot(nums);
+
+        // if you did not find a pivot, it means the array is not rotated
+        if(pivot == -1){
+            // Just do normal binary search
+            return binarySearch(nums, target, 0, nums.length - 1);
+        }
+
+        // if pivot is found, you have found 2 ascending sorted arrays
+        if(nums[pivot] == target){
+            return pivot;
+        }
+
+        if(target >= nums[0]){
+            return binarySearch(nums, target, 0, pivot - 1);
+        }
+
+        return binarySearch(nums, target, pivot + 1, nums.length - 1);
+
+    }
+    static int binarySearch(int[] arr, int target, int start, int end){
+        while(start <= end){
+            // Find the middle element
+//            int mid = (start + end) / 2; might be possible that(start + end) exceeds the range of int in Java
+            int mid = start + (end - start) / 2;
+            if(target < arr[mid]){
+                end = mid - 1;
+            }
+            else if(target > arr[mid]){
+                start = mid + 1;
+            }
+            else {
+                // answer found
+                return mid;
+            }
+        }
+        return -1;
+    }
+    static int findPivot(int[] arr){
+        int start = 0;
+        int end = arr.length - 1;
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+
+            // 4 cases over here
+            if(mid < end && arr[mid] > arr[mid + 1]){
+                return mid;
+            }
+            if(mid > start && arr[mid] < arr[mid - 1]){
+                return mid -1;
+            }
+            if(arr[mid] <= arr[start]){
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
