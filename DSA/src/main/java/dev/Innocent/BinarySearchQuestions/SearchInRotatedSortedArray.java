7a56f4f3c64007f59d2a8f6345eb5d255f5cb5d4
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-30 15:21:35 +0100

    Binary Search | Search In Rotated Sorted Array

--- DSA/src/main/java/dev/Innocent/BinarySearchQuestions/SearchInRotatedSortedArray.java
+++ DSA/src/main/java/dev/Innocent/BinarySearchQuestions/SearchInRotatedSortedArray.java
@@ -66,4 +66,42 @@ public class SearchInRotatedSortedArray {
         }
         return -1;
     }
+
+    static int findPivotWithDuplicate(int[] arr){
+        int start = 0;
+        int end = arr.length - 1;
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+
+            // 4 cases over here
+            if(mid < end && arr[mid] > arr[mid + 1]){
+                return mid;
+            }
+            if(mid > start && arr[mid] < arr[mid - 1]){
+                return mid -1;
+            }
+
+            // If elements at middle, start, end are equal then just skip the duplicate
+            if(arr[mid] == arr[start] && arr[mid] == arr[end]){
+                // Skip the duplicate
+                //NOTE: what if these elements at start and end where the pivot
+                // Check if start is pivot
+                if(arr[start] > arr[start + 1]){
+                    return start;
+                }start++;
+
+                // Check whether end is pivot
+                if(arr[end] < arr[end - 1]){
+                    return end -1;
+                }end--;
+            }
+            // left side is sorted, so pivot should be in right
+            else if(arr[start] < arr[mid] || (arr[start] == arr[mid] && arr[mid] > arr[end])){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
 }
