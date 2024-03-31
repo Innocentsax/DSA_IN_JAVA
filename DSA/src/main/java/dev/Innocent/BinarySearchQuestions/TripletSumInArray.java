d37534a1d6ac0c4913a6382e47e40a552e106925
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-31 21:37:32 +0100

    Binary Search | TripletSumInArray

--- DSA/src/main/java/dev/Innocent/BinarySearchQuestions/TripletSumInArray.java
+++ DSA/src/main/java/dev/Innocent/BinarySearchQuestions/TripletSumInArray.java
@@ -1,13 +1,18 @@
 package dev.Innocent.BinarySearchQuestions;
+
+import java.util.Arrays;
+
 // Given an array of size n and an integer X. Find if there's a triplet in the array which
 // sums up to the given integer.
 public class TripletSumInArray {
     public static void main(String[] args) {
-        int[] arr = {1, 4, 45, 6, 10, 8};
+        int[] A = {1, 4, 45, 6, 10, 8};
         int n = 6;
         int X = 13;
-        System.out.println(tripletSumInArray(arr, n, X));
+        System.out.println(tripletSumInArray2(A, n, X));
     }
+
+    // First approach
     static boolean tripletSumInArray(int[] A, int n, int X){
         if(n < 3){
             return false;
@@ -18,6 +23,33 @@ public class TripletSumInArray {
                     if(A[i] + A[j] + A[k] == X){
                         return true;
                     }
+                }
+            }
+        }
+        return false;
+    }
+
+    // Second approach
+    static boolean tripletSumInArray2(int[] A, int n, int X){
+        if(n < 3){
+            return false;
+        }
+
+        Arrays.sort(A);
+
+        for (int first = 0; first < n - 1; first++) {
+            int second = first + 1;
+            int third = n - 1;
+
+            while(second < third){
+                if(A[first] + A[second] + A[third] ==  X){
+                    return true;
+                }
+                else if(A[first] + A[second] + A[third] < X){
+                    second++;
+                }
+                else{
+                    third--;
                 }
             }
         }
