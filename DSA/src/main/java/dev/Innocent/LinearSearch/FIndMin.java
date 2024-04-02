05fd9ed97dc31e4ad3c07608e902ca0855883db7
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-02 12:49:12 +0100

    Binary Search | Find max min

--- DSA/src/main/java/dev/Innocent/LinearSearch/FIndMin.java
+++ DSA/src/main/java/dev/Innocent/LinearSearch/FIndMin.java
@@ -7,9 +7,6 @@ public class FIndMin {
     }
     static int min(int[] arr){
         int ans = arr[0];
-        if(arr.length == 0){
-            return -1;
-        }
         for (int i = 1; i < arr.length; i++) {
             if(arr[i] < ans){
                 ans = arr[i];
