4449ef7d1b97154f5d525838f6a8fe34250cc110
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-03 19:09:01 +0100

    Insertion Sort

---
+++ DSA/src/main/java/dev/Innocent/SortingArrays/InsertionSort.java
@@ -0,0 +1,24 @@
+package dev.Innocent.SortingArrays;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {3, 4, 5, 1, 2};
+        insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void insertionSort(int[] arr){
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if(arr[j] < arr[j - 1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                }else{
+                    break;
+                }
+            }
+        }
+    }
+}
