2d143e279f66dc35a7adedd6917a7e0172070b5c
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-04 18:21:56 +0100

    Concatenation Of Array

---
+++ DSA/src/main/java/Assignment/Arrays/ConcatenationOfArray.java
@@ -0,0 +1,18 @@
+package Assignment.Arrays;
+
+import java.util.Arrays;
+
+public class ConcatenationOfArray {
+    public static void main(String[] args) {
+        int[] nums = {1,2,1};
+        System.out.println(Arrays.toString(getConcatenation(nums)));
+    }
+    static int[] getConcatenation(int[] nums) {
+        int[] ans = new int[nums.length * 2];
+        for (int i = nums.length; i < ans.length; i++) {
+            ans[i - nums.length] = nums[i - nums.length];
+            ans[i] = nums[i - nums.length];
+        }
+        return ans;
+    }
+}
