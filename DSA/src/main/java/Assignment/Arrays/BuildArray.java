5994709106932fc271399f7625f0af62213bc4b1
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-04 18:21:38 +0100

    Build an array

---
+++ DSA/src/main/java/Assignment/Arrays/BuildArray.java
@@ -0,0 +1,18 @@
+package Assignment.Arrays;
+
+import java.util.Arrays;
+
+// https://leetcode.com/problems/build-array-from-permutation/
+public class BuildArray {
+    public static void main(String[] args) {
+        int[] nums = {0,2,1,5,3,4};
+        System.out.println(Arrays.toString(buildArray2(nums)));
+    }
+    static int[] buildArray2(int[] nums){
+        int[] ans = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+}
