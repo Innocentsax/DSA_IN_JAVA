35012ffceb5b8a6f804ee03ec787cf9c81c1a86e
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-08 20:30:14 +0100

    Find Duplicate2

---
+++ DSA/src/main/java/dev/Innocent/SortingArrays/Questions/FindDuplicates2.java
@@ -0,0 +1,41 @@
+package dev.Innocent.SortingArrays.Questions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// https://leetcode.com/problems/find-all-duplicates-in-an-array/
+class FindDuplicates2 {
+    public static void main(String[] args) {
+        int[] nums = {4,3,2,7,8,2,3,1};
+        List<Integer> newA = findDuplicates(nums);
+        System.out.println(newA);
+    }
+
+    static List<Integer> findDuplicates(int[] nums) {
+        int i = 0;
+        while(i < nums.length){
+            int correct = nums[i] - 1;
+            if(nums[i] != nums[correct]){
+                swap(nums, i, correct);
+            }else {
+                i++;
+            }
+        }
+
+        // Just find missing number
+        List<Integer> ans = new ArrayList<>();
+        for (int index = 0; index < nums.length; index++) {
+            if(nums[index] != index + 1){
+                ans.add(nums[index]);
+            }
+        }
+        return ans;
+    }
+
+    private static void swap(int[] nums, int i, int correct) {
+        int temp = nums[i];
+        nums[i] = nums[correct];
+        nums[correct] = temp;
+    }
+
+}
