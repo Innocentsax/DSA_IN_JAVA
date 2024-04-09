db88468d4a19a9cf3e32c95c859c58ec0ef3fa3e
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-09 21:08:38 +0100

    Is Palindromic

--- DSA/src/main/java/dev/Innocent/StringsInJava/Palindrome.java
+++ DSA/src/main/java/dev/Innocent/StringsInJava/Palindrome.java
@@ -6,6 +6,10 @@ public class Palindrome {
         System.out.println(isPalindrome(str));
     }
     static boolean isPalindrome(String str){
+        if(str.isEmpty()){
+            return true;
+        }
+        str = str.toLowerCase();
         for (int i = 0; i < str.length()/2; i++) {
             char start = str.charAt(i);
             char end = str.charAt(str.length() -1 -i);
