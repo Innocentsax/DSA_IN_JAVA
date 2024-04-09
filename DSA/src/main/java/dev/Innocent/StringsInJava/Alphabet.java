76f3a9de5b727756768d7b0c7c64d20734c39d41
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-09 21:05:28 +0100

    String Builder e.g

--- DSA/src/main/java/dev/Innocent/StringsInJava/Alphabet.java
+++ DSA/src/main/java/dev/Innocent/StringsInJava/Alphabet.java
@@ -2,9 +2,36 @@ package dev.Innocent.StringsInJava;
 
 public class Alphabet {
     public static void main(String[] args) {
+//        System.out.println(alphabet(""));
+        System.out.println(alphabet2());
+    }
+
+    /*
+    Using String: Remember String are immutable and can not be changed nor modified. hence
+    new series array is been created each time the loop runs and the new value is being added
+    to a copy of the old one. leading to memory wastage.
+    a, ab, abc, abcd, abcde, .... a-z
+
+    Meaning String are immutable
+     */
+    static String alphabet(String series){
         for (int i = 0; i < 26; i++) {
             char alphabet = (char) ('a' + i);
-            System.out.println(alphabet);
+            series = series + alphabet; // series += alphabet
         }
+        return series;
+    }
+
+
+    /*
+    StringBuilder are mutable, hence new object are append to the existing object
+     */
+    static StringBuilder alphabet2(){
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < 26; i++) {
+            char alphabet = (char) ('a' + i);
+            builder.append(alphabet);
+        }
+        return builder;
     }
 }
