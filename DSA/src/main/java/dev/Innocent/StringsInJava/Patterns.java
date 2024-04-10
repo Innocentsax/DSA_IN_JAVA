b52cd4b79653b8ed2aab50ff5d897f51105b3181
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-10 18:31:49 +0100

    Patterns in Java

--- DSA/src/main/java/dev/Innocent/StringsInJava/Patterns.java
+++ DSA/src/main/java/dev/Innocent/StringsInJava/Patterns.java
@@ -113,7 +113,8 @@ public class Patterns {
         n = 2 * n;
         for (int row = 0; row <= n; row++) {
             for (int col = 0; col <= n; col++) {
-                int atEveryIndex = originalN - Math.min(Math.min(row, col), Math.min(n - row, n - col));
+                int atEveryIndex = originalN - Math.min(Math.min(row, col),
+                        Math.min(n - row, n - col));
                 System.out.print(atEveryIndex + " ");
             }
             System.out.println();
