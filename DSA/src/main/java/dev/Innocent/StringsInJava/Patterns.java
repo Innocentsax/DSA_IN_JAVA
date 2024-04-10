22b717f57262dbc6109d57677807a63e9fd73753
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-10 18:31:09 +0100

    Patterns in Java

--- DSA/src/main/java/dev/Innocent/StringsInJava/Patterns.java
+++ DSA/src/main/java/dev/Innocent/StringsInJava/Patterns.java
@@ -2,7 +2,7 @@ package dev.Innocent.StringsInJava;
 
 public class Patterns {
     public static void main(String[] args) {
-        pattern6(4);
+        pattern9(4);
     }
     static void pattern1(int n){
         for (int row = 1; row <= n; row++) {
@@ -72,6 +72,50 @@ public class Patterns {
                 System.out.print("* ");
             }
             // When one row is printed, we need to add a newline
+            System.out.println();
+        }
+    }
+
+    static void pattern7(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int space = 0; space < n-row; space++) {
+                System.out.print("  ");
+            }
+            for (int col = row; col >= 1 ; col--) {
+                System.out.print(col + " ");
+            }
+            for (int col = 2; col <= row ; col++) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern8(int n) {
+        for (int row = 1; row <= 2*n; row++) {
+            int totalNumberCol = row > n ? 2*n - row : row;
+
+            for (int space = 0; space < n-totalNumberCol; space++) {
+                System.out.print("  ");
+            }
+            for (int col = totalNumberCol; col >= 1 ; col--) {
+                System.out.print(col + " ");
+            }
+            for (int col = 2; col <= totalNumberCol ; col++) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern9(int n){
+        int originalN = n;
+        n = 2 * n;
+        for (int row = 0; row <= n; row++) {
+            for (int col = 0; col <= n; col++) {
+                int atEveryIndex = originalN - Math.min(Math.min(row, col), Math.min(n - row, n - col));
+                System.out.print(atEveryIndex + " ");
+            }
             System.out.println();
         }
     }
