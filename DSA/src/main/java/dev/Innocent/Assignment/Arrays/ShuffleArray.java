dea10f32f8528f4cd2c54ce557fb0b385f023233
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-12 16:11:48 +0100

    Shuffle Array

---
+++ DSA/src/main/java/dev/Innocent/Assignment/Arrays/ShuffleArray.java
@@ -0,0 +1,19 @@
+package dev.Innocent.Assignment.Arrays;
+
+import java.util.Arrays;
+
+class ShuffleArray {
+    public static void main(String[] args) {
+        int[] nums = {2,5,1,3,4,7};
+        int n = 3;
+        System.out.println(Arrays.toString(shuffle(nums, n)));
+    }
+    public static int[] shuffle(int[] nums, int n) {
+        int[] ans = new int[2 * n];
+        for(int i = 0; i<n; i++){
+            ans[2*i] = nums[i];
+            ans[2*i+1] = nums[n+i];
+        }
+        return ans;
+    }
+}
