b19a1d7cfcd8386c2b78cfc470ed1cc8432f3c59
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-20 14:48:15 +0100

    Fibonacci

--- DSA/src/main/java/dev/Innocent/Recursion/Fibo.java
+++ DSA/src/main/java/dev/Innocent/Recursion/Fibo.java
@@ -2,7 +2,13 @@ package dev.Innocent.Recursion;
 
 public class Fibo {
     public static void main(String[] args) {
-        System.out.println(fibo(4));
+        System.out.println(fibFormula(50));
+    }
+
+    // Using the Formula
+    static int fibFormula(int n){
+        // Just for demo, use long instead
+        return (int)(Math.pow(((1 + Math.sqrt(5)) / 2), n) / Math.sqrt(5));
     }
 
     static int fibo(int n){
