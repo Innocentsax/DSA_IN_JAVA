4bee17e382ddac6090388436deec500f2d5f74be
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-05-03 10:35:49 +0100

    Smaller than current Number

---
+++ DSA/src/main/java/dev/Innocent/Assignment/Arrays/SmallerThanCurrentNumber.java
@@ -0,0 +1,24 @@
+package dev.Innocent.Assignment.Arrays;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+// https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+class SmallerThanCurrentNumber {
+    public static void main(String[] args) {
+       int[] nums = {8,1,2,2,3};
+        System.out.println(Arrays.toString(smallerNumbersThanCurrent(nums)));
+    }
+    public static int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] result = nums.clone();
+        Arrays.sort(result);
+        Map<Integer, Integer> index = new HashMap<>();
+        for (int i = 0; i < result.length; i++) {
+            index.putIfAbsent(result[i], i);
+        }
+        for (int j = 0; j < nums.length; j++) {
+            result[j] = index.get(nums[j]);
+        }
+        return result;
+    }
+}
