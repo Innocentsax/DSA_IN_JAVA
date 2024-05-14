6248059095f552980d723a7ff3fd2e8a69ebb543
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-05-14 21:48:59 +0100

    Diagonal Sum

---
+++ DSA/src/main/java/dev/Innocent/Assignment/Arrays/DiagonalSum.java
@@ -0,0 +1,30 @@
+package dev.Innocent.Assignment.Arrays;
+
+class DiagonalSum {
+    public static void main(String[] args) {
+
+    }
+    public static int diagonalSum(int[][] mat) {
+        int n = mat.length;
+        int primarySum = 0;
+        int secondarySum = 0;
+
+        // Sum of primary diagonal
+        for (int i = 0; i < n; i++) {
+            primarySum += mat[i][i];
+        }
+
+        // Sum of secondary diagonal (excluding elements of primary diagonal)
+        for (int j = 0; j < n; j++) {
+            // n-1-j gives the index of the element on the secondary diagonal
+            secondarySum += mat[j][n - 1 - j];
+        }
+
+        // If the matrix has odd dimensions, subtract the middle element counted twice
+        if(n % 2 != 0){
+            int mid = n / 2;
+            primarySum -= mat[mid][mid];
+        }
+        return primarySum + secondarySum;
+    }
+}
