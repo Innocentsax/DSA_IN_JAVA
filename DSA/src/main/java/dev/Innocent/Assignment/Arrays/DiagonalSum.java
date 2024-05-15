f88005c96bcf3b4551163ae3872418b3c9e5f28f
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-05-15 09:29:29 +0100

    Diagonal Sum

--- DSA/src/main/java/dev/Innocent/Assignment/Arrays/DiagonalSum.java
+++ DSA/src/main/java/dev/Innocent/Assignment/Arrays/DiagonalSum.java
@@ -2,7 +2,8 @@ package dev.Innocent.Assignment.Arrays;
 
 class DiagonalSum {
     public static void main(String[] args) {
-
+        int[][] mat = {{1,2,3}, {4,5,6}, {7,8,9}};
+        System.out.println(diagonalSum(mat));
     }
     public static int diagonalSum(int[][] mat) {
         int n = mat.length;
