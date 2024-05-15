d4fdb5b7c51330bce82d172d94dfba18eda87aa9
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-05-15 12:07:24 +0100

    Transpose Matrix

---
+++ DSA/src/main/java/dev/Innocent/Assignment/Arrays/Transpose.java
@@ -0,0 +1,28 @@
+package dev.Innocent.Assignment.Arrays;
+
+import java.util.Arrays;
+
+class Transpose {
+    public static void main(String[] args) {
+       int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+        System.out.println(Arrays.deepToString(transpose(matrix)));
+    }
+    public static int[][] transpose(int[][] matrix) {
+        // Get the size of the original matrix
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        // Create a new matrix for the Transpose with Swapped dimension
+        int[][] transposeMatrix = new int[col][row];
+
+        // Loop through each number in the original matrix
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                // Put the number in the right place in the transpose matrix
+                // We swap the row and cols indices
+                transposeMatrix[j][i] = matrix[i][j];
+            }
+        }
+        return transposeMatrix;
+    }
+}
