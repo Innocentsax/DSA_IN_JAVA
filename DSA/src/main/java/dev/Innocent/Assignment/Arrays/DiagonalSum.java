99c4a7d25ebb07a5f79cb2f4b2f64e2cdcb8a0a5
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-05-15 09:46:50 +0100

    Diagonal Sum

--- DSA/src/main/java/dev/Innocent/Assignment/Arrays/DiagonalSum.java
+++ DSA/src/main/java/dev/Innocent/Assignment/Arrays/DiagonalSum.java
@@ -1,5 +1,6 @@
 package dev.Innocent.Assignment.Arrays;
 
+// https://leetcode.com/problems/matrix-diagonal-sum/description/
 class DiagonalSum {
     public static void main(String[] args) {
         int[][] mat = {{1,2,3}, {4,5,6}, {7,8,9}};
