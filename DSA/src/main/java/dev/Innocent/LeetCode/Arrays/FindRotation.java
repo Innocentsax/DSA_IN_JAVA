c57ca6075b1382d7b3b3daeccd674f05c113d374
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-05-19 20:03:59 +0100

    FInd Rotation

---
+++ DSA/src/main/java/dev/Innocent/LeetCode/Arrays/FindRotation.java
@@ -0,0 +1,47 @@
+package dev.Innocent.LeetCode.Arrays;
+
+// https://leetcode.com/problems/determine-whether-matrix-can-be-obtained-by-rotation/
+class FindRotation {
+    public static void main(String[] args) {
+
+    }
+    public boolean findRotation(int[][] mat, int[][] target) {
+        if(areMatricsEqual(mat, target)){
+            return true;
+        }
+        // Rotate up to 3 times (90, 180, 270 degree)
+        for (int i = 0; i < 3; i++) {
+            mat = rotate90DegreeClockwise(mat);
+            if(areMatricsEqual(mat, target)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // This method is to rotate the matrix in 90 degree
+    private int[][] rotate90DegreeClockwise(int[][] mat) {
+        int n = mat.length;
+        int[][] rotated = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                rotated[j][n - 1 - j] = mat[i][j];
+            }
+        }
+        return rotated;
+    }
+
+    // This method is to check if the two matrices are equal
+    private boolean areMatricsEqual(int[][] mat, int[][] target) {
+        int n = mat.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(mat[i][j] == target[i][j]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
