a47f13630f86956c845f89426bab4221304a229d
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-05-20 19:28:19 +0100

    FInd Rotation

--- DSA/src/main/java/dev/Innocent/LeetCode/Arrays/FindRotation.java
+++ DSA/src/main/java/dev/Innocent/LeetCode/Arrays/FindRotation.java
@@ -5,7 +5,7 @@ class FindRotation {
     public static void main(String[] args) {
        int[][] mat = {{0,1},{1,0}};
        int[][] target = {{1,0},{0,1}};
-        System.out.println(findRotation(mat, target));
+        System.out.println(findRotation1(mat, target));
     }
 
     // Don't pass all test
@@ -50,6 +50,25 @@ class FindRotation {
     }
 
     public static boolean findRotation1(int[][] mat, int[][] target){
+        int n = mat.length;
+        int c90 = 0, c180 = 0, c270 = 0, c0 = 0;
 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(target[i][j] == mat[n-j-1][i]){
+                    c90++;
+                }
+                if(target[i][j] == mat[n-i-1][n-j-1]){
+                    c180++;
+                }
+                if(target[i][j] == mat[j][n-i-1]){
+                    c270++;
+                }
+                if(target[i][j] == mat[i][j]){
+                    c0++;
+                }
+            }
+        }
+        return c90 == n * n || c180 == n * n || c270 == n * n || c0 == n * n;
     }
 }
