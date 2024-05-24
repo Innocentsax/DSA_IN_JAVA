009d7dacb3bbd0a000157262ce52af4ae53cdda3
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-05-24 14:23:42 +0100

    Sum Zero

--- DSA/src/main/java/dev/Innocent/LeetCode/Arrays/SumZero.java
+++ DSA/src/main/java/dev/Innocent/LeetCode/Arrays/SumZero.java
@@ -1,11 +1,18 @@
 package dev.Innocent.LeetCode.Arrays;
 
+import java.util.Arrays;
+
 // https://leetcode.com/problems/find-n-unique-integers-sum-up-to-zero/
 class SumZero {
     public static void main(String[] args) {
-
+        int n = 5;
+        System.out.println(Arrays.toString(sumZero(n)));
     }
-    public int[] sumZero(int n) {
-        return new int[n];
+    public static int[] sumZero(int n) {
+        int[] newArr = new int[n];
+        for (int i = 0; i < n; i++) {
+            newArr[i] = i * 2 - n + 1;
+        }
+        return newArr;
     }
 }
