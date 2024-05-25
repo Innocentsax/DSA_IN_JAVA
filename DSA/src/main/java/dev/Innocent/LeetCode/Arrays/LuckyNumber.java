9390d99f9e9019e9c6ff3026c579f6834403f449
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-05-25 20:54:40 +0100

    Lucky Number

---
+++ DSA/src/main/java/dev/Innocent/LeetCode/Arrays/LuckyNumber.java
@@ -0,0 +1,15 @@
+package dev.Innocent.LeetCode.Arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// https://leetcode.com/problems/lucky-numbers-in-a-matrix/
+class LuckyNumber {
+    public static void main(String[] args) {
+
+    }
+    public List<Integer> luckyNumbers (int[][] matrix) {
+        List<Integer> newArr = new ArrayList<>();
+        return newArr;
+    }
+}
