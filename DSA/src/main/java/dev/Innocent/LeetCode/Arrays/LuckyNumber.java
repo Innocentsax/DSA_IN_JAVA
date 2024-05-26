c0f2dc9d130e1b537aca5b956b57b05f9b3f33b4
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-05-26 17:05:08 +0100

    Lucky Number

--- DSA/src/main/java/dev/Innocent/LeetCode/Arrays/LuckyNumber.java
+++ DSA/src/main/java/dev/Innocent/LeetCode/Arrays/LuckyNumber.java
@@ -9,7 +9,30 @@ class LuckyNumber {
 
     }
     public List<Integer> luckyNumbers (int[][] matrix) {
-        List<Integer> newArr = new ArrayList<>();
-        return newArr;
+        List<Integer> luckyNum = new ArrayList<>();
+
+        for (int i = 0; i < matrix.length; i++) {
+            int minNum = Integer.MAX_VALUE;
+            int col = 0;
+
+            for (int j = 0; j < matrix[i].length; j++) {
+                if(minNum > matrix[i][j]){
+                    minNum = matrix[i][j]; // This gives the minimum element in that array
+                    col = j;
+                }
+            }
+
+            boolean isTrue = true;
+            for (int j = 0; j < matrix.length; j++) {
+                if(matrix[i][col] < matrix[j][col]){
+                    isTrue = false;
+                    break;
+                }
+            }
+            if(isTrue){
+                luckyNum.add(matrix[i][col]);
+            }
+        }
+        return luckyNum;
     }
 }
