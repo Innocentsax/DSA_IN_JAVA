bb4a01910436fdcea7659eb162a15bd294d3f67c
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-05-27 08:33:03 +0100

    Matrix Reshape

--- DSA/src/main/java/dev/Innocent/LeetCode/Arrays/MatrixReshape.java
+++ DSA/src/main/java/dev/Innocent/LeetCode/Arrays/MatrixReshape.java
@@ -1,6 +1,9 @@
 package dev.Innocent.LeetCode.Arrays;
 
 class MatrixReshape {
+    public static void main(String[] args) {
+
+    }
     public int[][] matrixReshape(int[][] mat, int r, int c) {
         
     }
