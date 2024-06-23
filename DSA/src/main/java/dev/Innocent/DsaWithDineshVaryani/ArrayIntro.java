82551fc866bf8458f752afad51b3b934d9e0ea3f
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-23 18:37:20 +0100

    Even Integers

--- DSA/src/main/java/dev/Innocent/DsaWithDineshVaryani/ArrayIntro.java
+++ DSA/src/main/java/dev/Innocent/DsaWithDineshVaryani/ArrayIntro.java
@@ -1,11 +1,14 @@
 package dev.Innocent.DsaWithDineshVaryani;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class ArrayIntro {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6};
         System.out.println(Arrays.toString(evenInteger(arr)));
+        System.out.println(removeEven(arr));
     }
 
     // Q1. Given an array of Integers, return an array with integers removed
@@ -25,5 +28,15 @@ public class ArrayIntro {
             }
         }
         return result;
+    }
+
+    public static List<Integer> removeEven(int[] arr){
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] % 2 == 0){
+                ans.add(i);
+            }
+        }
+        return ans;
     }
 }
