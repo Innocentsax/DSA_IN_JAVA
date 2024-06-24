34157cffb345c48d4335b9d6b9f16e8663f7cc72
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-24 17:07:46 +0100

    Min Numbers

--- DSA/src/main/java/dev/Innocent/DsaWithDineshVaryani/MinNumber.java
+++ DSA/src/main/java/dev/Innocent/DsaWithDineshVaryani/MinNumber.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class MinNumber {
     public static void main(String[] args) {
         int[] arr = {2,5,1,9,5,6};
-        System.out.println(minNumber2(arr));
+        System.out.println(minNumber(arr));
     }
 
     private static int minNumber(int[] arr){
