43a4cdc2f7b6898fb717bd5bb15f3a02bf6b7882
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-29 10:52:27 +0100

    Second max Numbers

--- DSA/src/main/java/dev/Innocent/DsaWithDineshVaryani/MissingNumber.java
+++ DSA/src/main/java/dev/Innocent/DsaWithDineshVaryani/MissingNumber.java
@@ -3,7 +3,7 @@ package dev.Innocent.DsaWithDineshVaryani;
 public class MissingNumber {
     public static void main(String[] args) {
         int[] arr = {2, 4, 1, 8, 6, 3, 7};
-        System.out.println(missingNumber(arr));
+        System.out.println(findMissingNumber(arr));
     }
 
     private static int missingNumber(int[] arr){
@@ -11,6 +11,15 @@ public class MissingNumber {
         int sum = n * (n + 1)/2; // Find the sum of n natural numbers
         for(int num : arr){ // Check for every item in the arr and subtract it from the sum
             sum -= num;
+        }
+        return sum;
+    }
+
+    private static int findMissingNumber(int[] arr){
+        int n = arr.length + 1;
+        int sum = n * (n + 1)/2;
+        for (int i = 0; i < arr.length; i++) {
+            sum -= arr[i];
         }
         return sum;
     }
