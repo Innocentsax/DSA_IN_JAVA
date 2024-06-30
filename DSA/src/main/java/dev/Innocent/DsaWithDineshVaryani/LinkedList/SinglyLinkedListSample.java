2a52ad2354cd753c9d3717f7e1c9fefbc35f83ae
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-30 17:11:50 +0100

    LinkedList

---
+++ DSA/src/main/java/dev/Innocent/DsaWithDineshVaryani/LinkedList/SinglyLinkedListSample.java
@@ -0,0 +1,15 @@
+package dev.Innocent.DsaWithDineshVaryani.LinkedList;
+
+public class SinglyLinkedListSample {
+    private ListNode head;
+
+    private static class ListNode{
+        private int data; // Generic Type
+        private ListNode next;
+
+        public ListNode(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+}
