352adedfb2e78a06912fb8532c58d9b6f106ee10
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-07-01 15:30:16 +0100

    LinkedList

--- DSA/src/main/java/dev/Innocent/DsaWithDineshVaryani/LinkedList/SinglyLinkedListSample.java
+++ DSA/src/main/java/dev/Innocent/DsaWithDineshVaryani/LinkedList/SinglyLinkedListSample.java
@@ -12,4 +12,17 @@ public class SinglyLinkedListSample {
             this.next = null;
         }
     }
+
+    public static void main(String[] args) {
+        SinglyLinkedListSample sll = new SinglyLinkedListSample();
+        sll.head = new ListNode(10);
+        ListNode second = new ListNode(1);
+        ListNode third = new ListNode(8);
+        ListNode fourth = new ListNode(11);
+
+        // Now will connect them together to form a chain
+        sll.head.next = second;
+        second.next = third;
+        third.next = fourth;
+    }
 }
