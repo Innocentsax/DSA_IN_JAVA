837c7827d5d8ff454f0a798434b9b3c01b02ddd1
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-09-02 20:08:11 +0800

    Set matrix zero

--- DSA/src/main/java/dev/Innocent/Section5/KeywordsAndExpression/MethodChallenge.java
+++ DSA/src/main/java/dev/Innocent/Section5/KeywordsAndExpression/MethodChallenge.java
@@ -22,10 +22,11 @@ public class MethodChallenge {
     private static int calculateHighScorePosition(int score){
         if(score >= 1000){
             return 1;
-        } else if (score >= 500 && score < 1000) {
+        } else if (score >= 500) {
             return 2;
-        } else if (score >= 100 && score < 500) {
+        } else if (score >= 100) {
             return 3;
-        }else return 4;
+        }
+        return 4;
     }
 }
