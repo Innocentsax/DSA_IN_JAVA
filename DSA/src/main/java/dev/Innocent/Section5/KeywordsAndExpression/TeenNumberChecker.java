b19729fe248428eb761dbd9185c8496e4047568c
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-09-11 22:02:12 +0800

    Let's Begin

--- DSA/src/main/java/dev/Innocent/Section5/KeywordsAndExpression/TeenNumberChecker.java
+++ DSA/src/main/java/dev/Innocent/Section5/KeywordsAndExpression/TeenNumberChecker.java
@@ -2,7 +2,7 @@ package dev.Innocent.Section5.KeywordsAndExpression;
 
 public class TeenNumberChecker {
     public static void main(String[] args) {
-        System.out.println(hasTeen(9, 99, 19));
+        System.out.println(timeConversion(-1));
     }
     public static boolean hasTeen(int a, int b, int c){
         return isTeen(a) || isTeen(b) || isTeen(c);
@@ -24,7 +24,24 @@ public class TeenNumberChecker {
         return convertToCentimeter((feet * 12) + inches);
     }
 
-    public static double convertToCentimeter3(int feet, int inches){
-        return ((feet * 12) + inches) * 2.54;
+    public static String timeConversion(int seconds){
+        if(seconds < 0) return "Invalid input";
+        int minutes = seconds / 60;
+       return timeConversion(seconds, minutes);
+    }
+
+    public static String timeConversion(int seconds, int minutes){
+        if(seconds < 0 || seconds > 59){
+            return "Invalid data";
+        }
+        if(minutes < 0){
+            return "Invalid data";
+        }
+
+        int hours = minutes / 60;
+        int remainingMinutes = minutes % 60;
+        int remainingSeconds = remainingMinutes % 60;
+
+        return hours + " hrs " + remainingMinutes + " Mins " + remainingSeconds + " Secs ";
     }
 }
