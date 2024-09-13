abe67ba5f718c2ffe3b110880d959efe05ed0ecf
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-09-13 22:36:11 +0800

    Let's Begin

---
+++ DSA/src/main/java/dev/Innocent/Section5/KeywordsAndExpression/MinutesToYearsDaysCalculator.java
@@ -0,0 +1,14 @@
+package dev.Innocent.Section5.KeywordsAndExpression;
+
+public class MinutesToYearsDaysCalculator {
+    public static void printYearsAndDays(long minutes){
+        if(minutes<0){
+            System.out.println("Invalid Value");
+        }else{
+        long years = minutes / 525600;
+        long days = (minutes % 525600) / 1440;
+        
+        System.out.println(minutes + " min = " + years + " y and " + days + " d");
+        }
+    }
+}
