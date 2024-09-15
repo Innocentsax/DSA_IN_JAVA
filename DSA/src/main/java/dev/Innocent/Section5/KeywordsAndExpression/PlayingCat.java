3fab1302e891b05f1fa807ee9ac53a0569221c40
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-09-15 23:46:55 +0800

    Let's Begin

--- DSA/src/main/java/dev/Innocent/Section5/KeywordsAndExpression/PlayingCat.java
+++ DSA/src/main/java/dev/Innocent/Section5/KeywordsAndExpression/PlayingCat.java
@@ -1,6 +1,9 @@
 package dev.Innocent.Section5.KeywordsAndExpression;
 
 public class PlayingCat {
+    public static void main(String[] args) {
+        System.out.println(isCatPlaying(true, 34));
+    }
     public static boolean isCatPlaying(boolean summer, int temperature){
         if(!summer && temperature >= 25 && temperature <= 35){
             return true;
