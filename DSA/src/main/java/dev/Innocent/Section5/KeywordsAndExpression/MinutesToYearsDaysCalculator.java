174f678463750c53955efa55ea61a8ecc34683b7
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-09-16 09:44:36 +0800

    Let's Begin

--- DSA/src/main/java/dev/Innocent/Section5/KeywordsAndExpression/MinutesToYearsDaysCalculator.java
+++ DSA/src/main/java/dev/Innocent/Section5/KeywordsAndExpression/MinutesToYearsDaysCalculator.java
@@ -1,6 +1,9 @@
 package dev.Innocent.Section5.KeywordsAndExpression;
 
 public class MinutesToYearsDaysCalculator {
+    public static void main(String[] args) {
+
+    }
     public static void printYearsAndDays(long minutes){
         if(minutes<0){
             System.out.println("Invalid Value");
