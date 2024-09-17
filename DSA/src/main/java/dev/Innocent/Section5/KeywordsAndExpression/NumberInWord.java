5818cbf037d9b82b63d5e61204c27bd542b18297
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-09-17 21:15:40 +0800

    Let's Begin

---
+++ DSA/src/main/java/dev/Innocent/Section5/KeywordsAndExpression/NumberInWord.java
@@ -0,0 +1,19 @@
+package dev.Innocent.Section5.KeywordsAndExpression;
+
+public class NumberInWord {
+    public static void printNumberInWord(int wholeNumber){
+        System.out.println(switch(wholeNumber){
+            case 0 -> "ZERO";
+            case 1 -> "ONE";
+            case 2 -> "TWO";
+            case 3 -> "THREE";
+            case 4 -> "FOUR";
+            case 5 -> "FIVE";
+            case 6 -> "SIX";
+            case 7 -> "SEVEN";
+            case 8 -> "EIGHT";
+            case 9 -> "NINE";
+            default -> "OTHER";
+        });
+    }
+}
