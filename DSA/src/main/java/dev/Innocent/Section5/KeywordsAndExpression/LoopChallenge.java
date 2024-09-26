b6094c433f96324dcc0674a88c00d62cbb8afba7
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-09-26 21:20:34 +0800

    Let's Begin

--- DSA/src/main/java/dev/Innocent/Section5/KeywordsAndExpression/LoopChallenge.java
+++ DSA/src/main/java/dev/Innocent/Section5/KeywordsAndExpression/LoopChallenge.java
@@ -2,17 +2,24 @@ package dev.Innocent.Section5.KeywordsAndExpression;
 
 public class LoopChallenge {
     public static void main(String[] args) {
-        int count = 0;
+        int evenCount = 0;
+        int oddCount = 0;
         int number = 4;
         int finishedNumber = 20;
         while(number <= finishedNumber){
             number++;
             if(!isEvenNumber(number)){
+                oddCount++;
                 continue;
             }
-            count++;
             System.out.println(number);
+            evenCount++;
+            if(evenCount >= 5){
+                break;
+            }
         }
+        System.out.println("This is even Number =" + evenCount);
+        System.out.println("This is odd Number =" + oddCount);
     }
     public static boolean isEvenNumber(int number){
         if(number % 2 == 0){
