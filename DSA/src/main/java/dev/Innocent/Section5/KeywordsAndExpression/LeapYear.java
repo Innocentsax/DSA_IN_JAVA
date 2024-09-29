80d4f99faf2993df47dfb40edcb0be126eee2a4b
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-09-29 23:15:16 +0800

    Let's Begin

--- DSA/src/main/java/dev/Innocent/Section5/KeywordsAndExpression/LeapYear.java
+++ DSA/src/main/java/dev/Innocent/Section5/KeywordsAndExpression/LeapYear.java
@@ -3,6 +3,7 @@ package dev.Innocent.Section5.KeywordsAndExpression;
 public class LeapYear {
     public static void main(String[] args) {
         System.out.println(isLeapYear(2006));
+        System.out.println(isLeapYear(2029));
     }
     public static boolean isLeapYear(int year){
         if(year >= 1 && year <= 9999){
