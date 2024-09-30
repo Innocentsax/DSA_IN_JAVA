e25ffcf280d77c95fc3e345a73516e518e31633a
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-09-30 20:19:42 +0800

    Let's Begin

--- DSA/src/main/java/dev/Innocent/Section5/KeywordsAndExpression/sumDigit.java
+++ DSA/src/main/java/dev/Innocent/Section5/KeywordsAndExpression/sumDigit.java
@@ -1,7 +1,19 @@
 package dev.Innocent.Section5.KeywordsAndExpression;
 
 public class sumDigit {
-    private static void sumDigits(int number){
-
+    public static void main(String[] args) {
+        System.out.println(sumDigits(1234));
+    }
+    private static int sumDigits(int number){
+        int sum = 0;
+        if(number < 0){
+            return -1;
+        }
+        while(number > 9){
+            sum += number % 10;
+            number = number / 10;
+        }
+        sum += number;
+        return sum;
     }
 }
