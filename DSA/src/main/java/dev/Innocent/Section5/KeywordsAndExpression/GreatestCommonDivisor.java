fc3a0a10fbcac9a992efcbb8db06161c8e792068
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-10-05 17:13:50 +0800

    Let's Begin

--- DSA/src/main/java/dev/Innocent/Section5/KeywordsAndExpression/GreatestCommonDivisor.java
+++ DSA/src/main/java/dev/Innocent/Section5/KeywordsAndExpression/GreatestCommonDivisor.java
@@ -1,7 +1,27 @@
 package dev.Innocent.Section5.KeywordsAndExpression;
 
 public class GreatestCommonDivisor {
-    public static void getGreatestCommonDivisor(int first, int second){
+    public static void main(String[] args) {
+        System.out.println(getGreatestCommonDivisor(25, 15));
+    }
+    public static int getGreatestCommonDivisor(int first, int second){
+        if(first < 10 || second < 10){
+            return -1;
+        }
+        // Find the smallest Number
+        int temp = 0;
+        if(first > second){
+            temp = first;
+            first = second;
+            second = temp;
+        }
 
+        int gcd = 0;
+        for (int i = 1; i <= first; i++){
+            if(first % i == 0 && second % i == 0){
+                gcd = i;
+            }
+        }
+        return gcd;
     }
 }
