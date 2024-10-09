a6302a800ad738ab51928dc13b5b7f750b832041
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-10-09 21:09:09 +0800

    Numbers to word

---
+++ DSA/src/main/java/dev/Innocent/Section5/KeywordsAndExpression/LargestPrime.java
@@ -0,0 +1,18 @@
+package dev.Innocent.Section5.KeywordsAndExpression;
+
+public class LargestPrime {
+    public static void main(String[] args) {
+        System.out.println(getLargestPrime(21));
+    }
+    public static int getLargestPrime(int number){
+        if(number < 2){
+            return -1;
+        }
+        for (int i = number / 2; i > 1 ; i--) {
+            if(number % i == 0){
+                number = i;
+            }
+        }
+        return number;
+    }
+}
