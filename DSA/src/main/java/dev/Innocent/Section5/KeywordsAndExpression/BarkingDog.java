e989e961c0faf16489f9a4344e0f327f3e0406a2
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-10-11 22:12:57 +0800

    Numbers to word

--- DSA/src/main/java/dev/Innocent/Section5/KeywordsAndExpression/BarkingDog.java
+++ DSA/src/main/java/dev/Innocent/Section5/KeywordsAndExpression/BarkingDog.java
@@ -1,6 +1,9 @@
 package dev.Innocent.Section5.KeywordsAndExpression;
 
 public class BarkingDog {
+    public static void main(String[] args) {
+        System.out.println(shouldWakeUp(true, 21));
+    }
 
     public static boolean shouldWakeUp(boolean barking, int hourOfDay){
         if(barking && ((hourOfDay < 8 && hourOfDay >= 0) || (hourOfDay > 22 && hourOfDay <= 23))){
