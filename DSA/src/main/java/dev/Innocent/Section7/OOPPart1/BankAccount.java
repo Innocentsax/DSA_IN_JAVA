bb3bbfe44f9285b9cf0f68fc58a7028ba2b4440d
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-10-15 00:02:08 +0800

    Numbers to word

---
+++ DSA/src/main/java/dev/Innocent/Section7/OOPPart1/BankAccount.java
@@ -0,0 +1,63 @@
+package dev.Innocent.Section7.OOPPart1;
+
+public class BankAccount {
+    private int accountNumber;
+    private double accountBalance;
+    private String customerName;
+    private String email;
+    private String phoneNumber;
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getAccountBalance() {
+        return accountBalance;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setAccountBalance(double accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void depositFunds(double depositAmount){
+        accountBalance += depositAmount;
+        System.out.println("Deposit of $" + depositAmount + " made. Your new balance is $" + accountBalance);
+    }
+
+    public void withdrawFunds(double withdrawalAmount){
+        if(accountBalance - withdrawalAmount < 0){
+            System.out.println("insufficient funds! You only have $" + accountBalance + " in your account");
+        }else{
+            accountBalance -= withdrawalAmount;
+            System.out.println("Withdraw of this $" + withdrawalAmount + " processed, Remaining balance  = $" + accountBalance);
+        }
+    }
+}
