adde9ffbc194fabc243979de7b63465e5f039f16
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-10-15 22:23:36 +0800

    Numbers to word

---
+++ DSA/src/main/java/dev/Innocent/Section7/OOPPart1/SimpleCalculator.java
@@ -0,0 +1,42 @@
+package dev.Innocent.Section7.OOPPart1;
+
+public class SimpleCalculator {
+    private double firstNumber;
+    private double secondNumber;
+
+    public double getFirstNumber() {
+        return firstNumber;
+    }
+
+    public void setFirstNumber(double firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public double getSecondNumber() {
+        return secondNumber;
+    }
+
+    public void setSecondNumber(double secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+
+    public double getAdditionResult(){
+        return firstNumber + secondNumber;
+    }
+
+    public double getSubtractionResult(){
+        return firstNumber - secondNumber;
+    }
+
+    public double getMultiplicationResult(){
+        return firstNumber * secondNumber;
+    }
+
+    public double getDivisionResult(){
+        if(secondNumber == 0){
+            return 0.0;
+        }else {
+            return firstNumber / secondNumber;
+        }
+    }
+}
