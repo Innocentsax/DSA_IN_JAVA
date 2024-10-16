bed4b3a686239cd97c2bbbb8103721e02bfba9e9
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-10-16 18:39:59 +0800

    Numbers to word

--- DSA/src/main/java/dev/Innocent/Section7/OOPPart1/Main.java
+++ DSA/src/main/java/dev/Innocent/Section7/OOPPart1/Main.java
@@ -2,13 +2,8 @@ package dev.Innocent.Section7.OOPPart1;
 
 public class Main {
     public static void main(String[] args) {
-        BankAccount bank = new BankAccount();
-        // Use setter
-        bank.setAccountBalance(4000);
-        bank.setAccountNumber(2120100314);
-        bank.setCustomerName("Innocent Udo");
-        bank.setEmail("innocentcharlesudo@gmail.com");
-        bank.setPhoneNumber("+60-179549917");
+        BankAccount bank = new BankAccount(2120100314, 4000,
+                "Innocent Udo", "innocentcharlesudo@gmail.com", "+60-179549917");
 
         bank.depositFunds(2000);
         bank.withdrawFunds(100.00);
