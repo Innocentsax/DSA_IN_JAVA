020a970c04fb49c789f1a364d9f762cacc7c5c01
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-10-17 22:24:12 +0800

    Numbers to word

--- DSA/src/main/java/dev/Innocent/Section7/OOPPart1/Main.java
+++ DSA/src/main/java/dev/Innocent/Section7/OOPPart1/Main.java
@@ -5,6 +5,10 @@ public class Main {
         BankAccount bank = new BankAccount(2120100314, 4000,
                 "Innocent Udo", "innocentcharlesudo@gmail.com", "+60-179549917");
 
+        BankAccount newBank = new BankAccount(1234566, 6000, "Innocent", "08167876543",
+                "oiuytgfrds0987654");
+
+
         bank.depositFunds(2000);
         bank.withdrawFunds(100.00);
         bank.withdrawFunds(200.00);
