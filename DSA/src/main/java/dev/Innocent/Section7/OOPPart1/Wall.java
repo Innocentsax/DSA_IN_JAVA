0d6ec8139725b06dde759119fdd71c7be7cd8d0a
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-10-19 12:34:38 +0800

    Numbers to word

--- DSA/src/main/java/dev/Innocent/Section7/OOPPart1/Wall.java
+++ DSA/src/main/java/dev/Innocent/Section7/OOPPart1/Wall.java
@@ -1,5 +1,34 @@
 package dev.Innocent.Section7.OOPPart1;
 
 public class Wall {
+    private double width;
+    private double height;
 
+    public Wall() {
+    }
+
+    public Wall(double width, double height) {
+        this.width = width < 0 ? 0 : width;
+        this.height = height < 0 ? 0: height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width < 0 ? 0 : width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height < 0 ? 0 : height;
+    }
+
+    public double getArea(){
+        return getWidth() * getHeight();
+    }
 }
