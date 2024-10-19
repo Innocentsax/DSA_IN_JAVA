e5874ef5ebb56a1e1c15eb2df9ec4582427d557b
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-10-19 12:34:35 +0800

    Numbers to word

--- DSA/src/main/java/dev/Innocent/Section7/OOPPart1/Main.java
+++ DSA/src/main/java/dev/Innocent/Section7/OOPPart1/Main.java
@@ -2,16 +2,16 @@ package dev.Innocent.Section7.OOPPart1;
 
 public class Main {
     public static void main(String[] args) {
-//        BankAccount bank = new BankAccount(2120100314, 4000,
-//                "Innocent Udo", "innocentcharlesudo@gmail.com", "+60-179549917");
-//
-//        BankAccount newBank = new BankAccount(1234566, 6000, "Innocent", "08167876543",
-//                "oiuytgfrds0987654");
-//
-//
-//        bank.depositFunds(2000);
-//        bank.withdrawFunds(100.00);
-//        bank.withdrawFunds(200.00);
+        BankAccount bank = new BankAccount(2120100314, 4000,
+                "Innocent Udo", "innocentcharlesudo@gmail.com", "+60-179549917");
+
+        BankAccount newBank = new BankAccount(1234566, 6000, "Innocent", "08167876543",
+                "oiuytgfrds0987654");
+
+
+        bank.depositFunds(2000);
+        bank.withdrawFunds(100.00);
+        bank.withdrawFunds(200.00);
 
         for (int i = 1; i <= 5; i++) {
             LPStudent newStudent = new LPStudent("123er" + i,
@@ -27,5 +27,7 @@ public class Main {
 
             System.out.println(newStudent);
         }
+        Wall newWall = new Wall(5, 4);
+        System.out.println("Area " + newWall.getArea());
     }
 }
