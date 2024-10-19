05c0459aceb97e04b00d9f3c7a3a0f77c7c4280e
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-10-19 11:36:45 +0800

    Numbers to word

--- DSA/src/main/java/dev/Innocent/Section7/OOPPart1/Main.java
+++ DSA/src/main/java/dev/Innocent/Section7/OOPPart1/Main.java
@@ -2,17 +2,30 @@ package dev.Innocent.Section7.OOPPart1;
 
 public class Main {
     public static void main(String[] args) {
-        BankAccount bank = new BankAccount(2120100314, 4000,
-                "Innocent Udo", "innocentcharlesudo@gmail.com", "+60-179549917");
+//        BankAccount bank = new BankAccount(2120100314, 4000,
+//                "Innocent Udo", "innocentcharlesudo@gmail.com", "+60-179549917");
+//
+//        BankAccount newBank = new BankAccount(1234566, 6000, "Innocent", "08167876543",
+//                "oiuytgfrds0987654");
+//
+//
+//        bank.depositFunds(2000);
+//        bank.withdrawFunds(100.00);
+//        bank.withdrawFunds(200.00);
 
-        BankAccount newBank = new BankAccount(1234566, 6000, "Innocent", "08167876543",
-                "oiuytgfrds0987654");
+        for (int i = 1; i <= 5; i++) {
+            LPStudent newStudent = new LPStudent("123er" + i,
+                    switch (i) {
+                        case 1 -> "Mary";
+                        case 2 -> "Henry";
+                        case 3 -> "Bit";
+                        case 4 -> "Epin";
+                        case 5 -> "King";
+                        default -> "Unknown";
+                    },
+                    "1996","I.T hard");
 
-
-        bank.depositFunds(2000);
-        bank.withdrawFunds(100.00);
-        bank.withdrawFunds(200.00);
-
-
+            System.out.println(newStudent);
+        }
     }
 }
