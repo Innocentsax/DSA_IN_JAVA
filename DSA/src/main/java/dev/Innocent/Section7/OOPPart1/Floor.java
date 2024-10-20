77c39d4ba9e9474392754bb52886455a793cdc74
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-10-20 22:44:38 +0800

    Numbers to word

---
+++ DSA/src/main/java/dev/Innocent/Section7/OOPPart1/Floor.java
@@ -0,0 +1,41 @@
+package dev.Innocent.Section7.OOPPart1;
+
+public class Floor {
+    private double width;
+    private double length;
+
+    public Floor(double width, double length) {
+        this.width = width < 0 ? 0 : width;
+        this.length = length < 0 ? 0 : length;
+    }
+
+    public double getArea(){
+        return width * length;
+    }
+}
+
+class Carpet{
+    private double cost;
+
+    public Carpet(double cost) {
+        this.cost = cost < 0 ? 0 : cost;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+}
+
+class Calculator{
+    private Floor floor;
+    private Carpet carpet;
+
+    public Calculator(Floor floor, Carpet carpet) {
+        this.floor = floor;
+        this.carpet = carpet;
+    }
+
+    public double getTotalCost(){
+        return floor.getArea() * carpet.getCost();
+    }
+}
