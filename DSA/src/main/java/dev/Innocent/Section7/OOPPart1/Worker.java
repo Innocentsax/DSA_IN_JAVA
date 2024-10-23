0d876425f521da764a97a6c920047b972cc374af
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-10-23 10:59:42 +0800

    Numbers to word

---
+++ DSA/src/main/java/dev/Innocent/Section7/OOPPart1/Worker.java
@@ -0,0 +1,28 @@
+package dev.Innocent.Section7.OOPPart1;
+
+public class Worker {
+    private String name;
+    private String dateOfBirth;
+    private String endDate;
+
+    public Worker() {
+    }
+
+    public Worker(String name, String dateOfBirth, String endDate) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.endDate = endDate;
+    }
+
+    private int getAge(){
+
+    }
+
+    private double collectPay(){
+
+    }
+
+    private void terminate(String endDate){
+
+    }
+}
