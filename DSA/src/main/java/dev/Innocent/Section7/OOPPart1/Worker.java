e4bdbea4a966592af3d2e7209cf69e89f843a0b8
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-10-24 22:47:05 +0800

    Numbers to word

--- DSA/src/main/java/dev/Innocent/Section7/OOPPart1/Worker.java
+++ DSA/src/main/java/dev/Innocent/Section7/OOPPart1/Worker.java
@@ -3,26 +3,37 @@ package dev.Innocent.Section7.OOPPart1;
 public class Worker {
     private String name;
     private String dateOfBirth;
-    private String endDate;
+    protected String endDate;
 
     public Worker() {
     }
 
-    public Worker(String name, String dateOfBirth, String endDate) {
+    public Worker(String name, String dateOfBirth) {
         this.name = name;
         this.dateOfBirth = dateOfBirth;
-        this.endDate = endDate;
     }
 
     private int getAge(){
+        int currentYear = 2024;
+        int birthYear = Integer.parseInt(dateOfBirth.substring(6));
 
+        return currentYear - birthYear;
     }
 
     private double collectPay(){
-
+        return 0.0;
     }
 
     private void terminate(String endDate){
+        this.endDate = endDate;
+    }
 
+    @Override
+    public String toString() {
+        return "Worker{" +
+                "name='" + name + '\'' +
+                ", dateOfBirth='" + dateOfBirth + '\'' +
+                ", endDate='" + endDate + '\'' +
+                '}';
     }
 }
