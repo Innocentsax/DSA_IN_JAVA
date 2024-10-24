1883d2c3458ee4d2715d26f6b76ae0868866d95a
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-10-24 22:46:58 +0800

    Numbers to word

--- DSA/src/main/java/dev/Innocent/Section7/OOPPart1/Employee.java
+++ DSA/src/main/java/dev/Innocent/Section7/OOPPart1/Employee.java
@@ -4,5 +4,17 @@ public class Employee extends Worker{
     private long employeeId;
     private String hireDate;
 
+    public Employee(String name, String dateOfBirth, long employeeId, String hireDate) {
+        super(name, dateOfBirth);
+        this.employeeId = employeeId;
+        this.hireDate = hireDate;
+    }
 
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId=" + employeeId +
+                ", hireDate='" + hireDate + '\'' +
+                "} " + super.toString();
+    }
 }
