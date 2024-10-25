c14e2af35f91b48aa83d3002c159856d3e566463
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-10-25 13:41:02 +0800

    Numbers to word

---
+++ DSA/src/main/java/dev/Innocent/Section7/OOPPart1/HourlyEmployee.java
@@ -0,0 +1,19 @@
+package dev.Innocent.Section7.OOPPart1;
+
+public class HourlyEmployee extends Employee{
+    private double hourlyPayRate;
+
+    public HourlyEmployee(String name, String dateOfBirth, long employeeId, String hireDate, double hourlyPayRate) {
+        super(name, dateOfBirth, employeeId, hireDate);
+        this.hourlyPayRate = hourlyPayRate;
+    }
+
+    @Override
+    public double collectPay(){
+        return 40 * hourlyPayRate;
+    }
+
+    public double getDoublePay(){
+        return 2 * collectPay();
+    }
+}
