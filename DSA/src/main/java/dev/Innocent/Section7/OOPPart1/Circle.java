5ac53b4be030c1349b84f286f31f9cf776133405
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-10-29 21:30:37 +0800

    Numbers to word

---
+++ DSA/src/main/java/dev/Innocent/Section7/OOPPart1/Circle.java
@@ -0,0 +1,17 @@
+package dev.Innocent.Section7.OOPPart1;
+
+public class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius < 0 ? 0 : radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea(){
+        return radius * radius * Math.PI;
+    }
+}
