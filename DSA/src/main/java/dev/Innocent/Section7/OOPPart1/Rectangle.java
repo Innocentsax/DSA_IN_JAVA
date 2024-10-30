56039427aa73061c7325f440eacb9414ec616ec2
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-10-30 22:36:27 +0800

    Numbers to word

---
+++ DSA/src/main/java/dev/Innocent/Section7/OOPPart1/Rectangle.java
@@ -0,0 +1,23 @@
+package dev.Innocent.Section7.OOPPart1;
+
+public class Rectangle {
+    private double length;
+    private double width;
+
+    public Rectangle(double width, double length) {
+        this.length = length < 0 ? 0 : length;
+        this.width = width < 0 ? 0 : width;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getArea(){
+        return width * length;
+    }
+}
