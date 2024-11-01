17697e8eaf999428dc6061aa2d7c60fa27f6bc49
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-11-01 10:24:24 +0800

    Numbers to word

--- DSA/src/main/java/dev/Innocent/Section7/OOPPart1/SmartKitchen.java
+++ DSA/src/main/java/dev/Innocent/Section7/OOPPart1/SmartKitchen.java
@@ -20,3 +20,33 @@ class CoffeeMaker{
         }
     }
 }
+
+class Refrigerator{
+    private boolean hasWorkToDo;
+
+    public void setHasWorkToDo(boolean hasWorkToDo) {
+        this.hasWorkToDo = hasWorkToDo;
+    }
+
+    public void orderFood(){
+        if(hasWorkToDo){
+            System.out.println("Ordering Food");
+            hasWorkToDo = false;
+        }
+    }
+}
+
+class DishWasher{
+    private boolean hasWorkToDo;
+
+    public void setHasWorkToDo(boolean hasWorkToDo) {
+        this.hasWorkToDo = hasWorkToDo;
+    }
+
+    public void doDishes(){
+        if(hasWorkToDo){
+            System.out.println("Washing Dishes");
+            hasWorkToDo = false;
+        }
+    }
+}
