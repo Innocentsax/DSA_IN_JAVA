b7aaaa37024a56a53d5bae79594123b7d7ead989
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-11-02 21:09:29 +0800

    Numbers to word

---
+++ DSA/src/main/java/dev/Innocent/Section7/OOPPart1/Walls.java
@@ -0,0 +1,13 @@
+package dev.Innocent.Section7.OOPPart1;
+
+public class Walls {
+    private String direction;
+
+    public Walls(String direction) {
+        this.direction = direction;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+}
