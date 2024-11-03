ad8a3da5b1d0ecec07e4be5a340af843abeb2dde
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-11-03 18:00:49 +0800

    Numbers to word

---
+++ DSA/src/main/java/dev/Innocent/Section7/OOPPart1/Ceiling.java
@@ -0,0 +1,18 @@
+package dev.Innocent.Section7.OOPPart1;
+
+public class Ceiling {
+    private int height, paintedColor;
+
+    public Ceiling(int height, int paintedColor) {
+        this.height = height;
+        this.paintedColor = paintedColor;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getPaintedColor() {
+        return paintedColor;
+    }
+}
