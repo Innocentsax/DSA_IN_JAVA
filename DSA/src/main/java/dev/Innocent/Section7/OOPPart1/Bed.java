8f8dec7719f1e6d8d439583893ac9c937cf8e285
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-11-03 18:00:40 +0800

    Numbers to word

---
+++ DSA/src/main/java/dev/Innocent/Section7/OOPPart1/Bed.java
@@ -0,0 +1,38 @@
+package dev.Innocent.Section7.OOPPart1;
+
+public class Bed{
+    private String style;
+    private int pillows, height, sheets, quilt;
+
+    public Bed(String style, int pillows, int height, int sheets, int quilt) {
+        this.style = style;
+        this.pillows = pillows;
+        this.height = height;
+        this.sheets = sheets;
+        this.quilt = quilt;
+    }
+
+    public void make(){
+        System.out.print("Bed -> Making | ");
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public int getPillows() {
+        return pillows;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getSheets() {
+        return sheets;
+    }
+
+    public int getQuilt() {
+        return quilt;
+    }
+}
