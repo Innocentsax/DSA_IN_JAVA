bbb584b5c2f4603c586afcec44b6b785eb37495f
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-11-04 22:36:44 +0800

    Numbers to word

--- DSA/src/main/java/dev/Innocent/Section7/OOPPart1/EnhancedPlayer.java
+++ DSA/src/main/java/dev/Innocent/Section7/OOPPart1/EnhancedPlayer.java
@@ -20,4 +20,23 @@ public class EnhancedPlayer {
         }
         this.weapon = weapon;
     }
+
+    private void loseHealth(int damage){
+        health = health - damage;
+        if(health < 0){
+            System.out.println("Player knocked out of the game");
+        }
+    }
+
+    public int healthRemaining(){
+        return health;
+    }
+
+    public void restoreHealth(int extraHealth){
+        health = health + extraHealth;
+        if(health > 100){
+            System.out.println("Player restored to 100%");
+            health = 100;
+        }
+    }
 }
