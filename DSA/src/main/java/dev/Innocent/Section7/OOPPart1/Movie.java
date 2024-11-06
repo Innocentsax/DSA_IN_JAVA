612030b9fb048dc451d8f4fc04fec9a09aec6029
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-11-06 20:34:24 +0800

    Numbers to word

--- DSA/src/main/java/dev/Innocent/Section7/OOPPart1/Movie.java
+++ DSA/src/main/java/dev/Innocent/Section7/OOPPart1/Movie.java
@@ -11,6 +11,15 @@ public class Movie {
         String instanceType = this.getClass().getSimpleName();
         System.out.println(title + " is a " + instanceType + " film.");
     }
+
+    public static Movie getMovie(String type, String title){
+        return switch (type.toUpperCase().charAt(0)){
+            case 'A' -> new Adventure(title);
+            case 'C' -> new Comedy(title);
+            case 'S' -> new ScienceFiction(title);
+            default -> new Movie(title);
+        };
+    }
 }
 
 class Adventure extends Movie{
