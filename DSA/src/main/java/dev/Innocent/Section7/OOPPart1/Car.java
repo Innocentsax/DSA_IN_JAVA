2e6089c4a20eaa9cd9e432e4b196b69d4836501d
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-11-07 20:46:41 +0800

    Numbers to word

---
+++ DSA/src/main/java/dev/Innocent/Section7/OOPPart1/Car.java
@@ -0,0 +1,17 @@
+package dev.Innocent.Section7.OOPPart1;
+
+public class Car {
+    private String description;
+
+    public void startEngine(){
+
+    }
+
+    public void drive(){
+
+    }
+
+    protected void runEngine(){
+
+    }
+}
