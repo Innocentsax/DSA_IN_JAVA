8997008dd5cb6fb38d24a1e668c591614a4335c3
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-11-08 22:38:52 +0800

    Numbers to word

--- DSA/src/main/java/dev/Innocent/Section7/OOPPart1/Car.java
+++ DSA/src/main/java/dev/Innocent/Section7/OOPPart1/Car.java
@@ -1,26 +1,28 @@
 package dev.Innocent.Section7.OOPPart1;
 
 public class Car {
-    private boolean engine = true;
+    private boolean engine;
     private int cylinders;
     private String name;
-    private int wheel = 4;
+    private int wheels;
 
     public Car(int cylinders, String name) {
         this.cylinders = cylinders;
         this.name = name;
+        this.engine = true;
+        this.wheels = 4;
     }
 
-    public void startEngine(){
-        System.out.println("The car's engine is starting " + name);
+    public String startEngine() {
+        return getClass().getSimpleName() + " -> startEngine()";
     }
 
-    public void accelerate(){
-        System.out.println("Car is accelerating " + name);
+    public String accelerate() {
+        return getClass().getSimpleName() + " -> accelerate()";
     }
 
-    public void brake(){
-        System.out.println("Car is braking " + name);
+    public String brake() {
+        return getClass().getSimpleName() + " -> brake()";
     }
 
     public int getCylinders() {
