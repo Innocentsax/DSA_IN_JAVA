fb8d05709b74cc35e82a2b0f928f114bc590be7a
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-11-09 20:16:55 +0800

    Numbers to word

---
+++ DSA/src/main/java/dev/Innocent/Section7/OOPPart1/Holden.java
@@ -0,0 +1,22 @@
+package dev.Innocent.Section7.OOPPart1;
+
+public class Holden extends Car {
+    public Holden(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return getClass().getSimpleName() + " -> startEngine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return getClass().getSimpleName() + " -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return getClass().getSimpleName() + " -> brake()";
+    }
+}
