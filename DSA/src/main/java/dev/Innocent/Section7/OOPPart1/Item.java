76a688334be23140c54d01bccaa2fc54a1d99209
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-11-10 15:30:21 +0800

    Numbers to word

---
+++ DSA/src/main/java/dev/Innocent/Section7/OOPPart1/Item.java
@@ -0,0 +1,45 @@
+package dev.Innocent.Section7.OOPPart1;
+
+public class Item {
+    private String type;
+    private String name;
+    private double price;
+    private String size = "MEDIUM";
+
+    public Item(String type, String name, double price) {
+        this.type = type.toUpperCase();
+        this.name = name.toUpperCase();
+        this.price = price;
+    }
+
+    public String getName() {
+        if(type.equals("SIDE") || type.equals("DRINK")){
+            return size + " " + name;
+        }
+        return name;
+    }
+
+    public double getBasePrice() {
+        return price;
+    }
+
+    public double getAdjustedPrice(){
+        return switch (size){
+            case "SMALL" -> getBasePrice() - 0.5;
+            case "LARGE" -> getBasePrice() - 1;
+            default -> getBasePrice();
+        };
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public static void printItem(String name, double price){
+        System.out.printf("%20s:%6.2f%n", name, price);
+    }
+
+    public void printItem(){
+        printItem(getName(), getAdjustedPrice());
+    }
+}
