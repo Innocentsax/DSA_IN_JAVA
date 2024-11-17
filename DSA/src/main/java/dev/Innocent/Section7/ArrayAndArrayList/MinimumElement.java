5352789c75f1eadee7c9b38b1636d9f74ea90152
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-11-17 14:01:12 +0800

    Vagrant

--- DSA/src/main/java/dev/Innocent/Section7/ArrayAndArrayList/MinimumElement.java
+++ DSA/src/main/java/dev/Innocent/Section7/ArrayAndArrayList/MinimumElement.java
@@ -37,4 +37,24 @@ public class MinimumElement {
         }
         return min;
     }
+
+    private static void reverse(int[] array){
+        int maxIndex = array.length - 1;
+        int halfLength = array.length / 2;
+
+        for (int i = 0; i < halfLength; i++) {
+            int temp = array[i];
+            array[i] = array[maxIndex - i];
+            array[maxIndex - i] = temp;
+        }
+    }
+
+    private static int[] reverseCopy(int[] array){
+        int[] reversedArray = new int[array.length];
+        int maxIndex = array.length;
+        for(int el : array){
+            reversedArray[maxIndex--] = el;
+        }
+        return reversedArray;
+    }
 }
