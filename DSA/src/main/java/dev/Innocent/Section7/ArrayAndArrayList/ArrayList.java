7550627bc319366400767fc6de88ac78ba482aae
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-11-18 22:49:42 +0800

    Vagrant

---
+++ DSA/src/main/java/dev/Innocent/Section7/ArrayAndArrayList/ArrayList.java
@@ -0,0 +1,11 @@
+package dev.Innocent.Section7.ArrayAndArrayList;
+
+record GroceryItem(String name, String type, int count){
+
+    public GroceryItem(String name) {
+        this(name, "DAIRY", 1);
+    }
+}
+
+public class ArrayList {
+}
