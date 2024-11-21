5e02feb6c8f2023abf0f5aee5a0ce3520fa7abf2
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-11-21 22:19:28 +0800

    ArrayList

---
+++ DSA/src/main/java/dev/Innocent/Section7/ArrayAndArrayList/MobilePhone.java
@@ -0,0 +1,21 @@
+package dev.Innocent.Section7.ArrayAndArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MobilePhone {
+    private String myNumber;
+    private ArrayList<Contact> myContacts;
+
+    public MobilePhone(String myNumber) {
+        this.myNumber = myNumber;
+    }
+
+    public static boolean addNewContact(Contact contact){
+        if()
+    }
+
+    public static int findContact(){
+
+    }
+}
