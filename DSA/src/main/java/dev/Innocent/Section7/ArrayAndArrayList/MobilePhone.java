8709f3f00f611ad11061e92f4f50282b7dd37d80
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-11-22 16:07:47 +0800

    ArrayList

--- DSA/src/main/java/dev/Innocent/Section7/ArrayAndArrayList/MobilePhone.java
+++ DSA/src/main/java/dev/Innocent/Section7/ArrayAndArrayList/MobilePhone.java
@@ -1,21 +1,46 @@
 package dev.Innocent.Section7.ArrayAndArrayList;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MobilePhone {
     private String myNumber;
     private ArrayList<Contact> myContacts;
 
-    public MobilePhone(String myNumber) {
+    public MobilePhone(String myNumber, ArrayList<Contact> myContacts) {
         this.myNumber = myNumber;
+        this.myContacts = myContacts;
     }
 
     public static boolean addNewContact(Contact contact){
         if()
     }
 
-    public static int findContact(){
+    public int findContact(Contact contact){
+        return myContacts.indexOf(contact);
+    }
 
+    private int findContact(String contactName){
+        for (int i = 0; i < myContacts.size(); i++) {
+            if(myContacts.get(i).getName().equalsIgnoreCase(contactName)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private Contact queryContact(String contactName){
+        int position = findContact(contactName);
+        if(position >= 0){
+            return myContacts.get(position);
+        }
+        return null;
+    }
+
+    public void printContact(){
+        System.out.println("Contact List");
+        for (int i = 0; i < myContacts.size(); i++) {
+            Contact contact = myContacts.get(i);
+            System.out.println((i + 1) + ". " + contact.getName() + " -> " + contact.getPhoneNumber());
+        }
     }
 }
