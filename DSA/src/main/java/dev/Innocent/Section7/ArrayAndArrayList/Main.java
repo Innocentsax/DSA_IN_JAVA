6e203028837aac117f44e02d32a0e3bd69dfa463
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-11-23 21:13:38 +0800

    LinkedList Challenge

--- DSA/src/main/java/dev/Innocent/Section7/ArrayAndArrayList/Main.java
+++ DSA/src/main/java/dev/Innocent/Section7/ArrayAndArrayList/Main.java
@@ -1,21 +1,46 @@
 package dev.Innocent.Section7.ArrayAndArrayList;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
 
-        MobilePhone mobilePhone = new MobilePhone("0909090909090");
+        ArrayList<Album> albums = new ArrayList<>();
 
-        Contact contact = Contact.createContact("Innocent", "12345678");
-        Contact contact2 = Contact.createContact("Innocent2", "123456782");
-        Contact contact3 = Contact.createContact("Innocent3", "123456783");
+        Album album = new Album("Stormbringer", "Deep Purple");
+        album.addSong("Stormbringer", 4.6);
+        album.addSong("Love don't mean a thing", 4.22);
+        album.addSong("Holy man", 4.3);
+        album.addSong("Hold on", 5.6);
+        album.addSong("Lady double dealer", 3.21);
+        album.addSong("You can't do it right", 6.23);
+        album.addSong("High ball shooter", 4.27);
+        album.addSong("The gypsy", 4.2);
+        album.addSong("Soldier of fortune", 3.13);
+        albums.add(album);
 
-        mobilePhone.addNewContact(contact);
-        mobilePhone.addNewContact(contact2);
+        album = new Album("For those about to rock", "AC/DC");
+        album.addSong("For those about to rock", 5.44);
+        album.addSong("I put the finger on you", 3.25);
+        album.addSong("Lets go", 3.45);
+        album.addSong("Inject the venom", 3.33);
+        album.addSong("Snowballed", 4.51);
+        album.addSong("Evil walks", 3.45);
+        album.addSong("C.O.D.", 5.25);
+        album.addSong("Breaking the rules", 5.32);
+        album.addSong("Night of the long knives", 5.12);
+        albums.add(album);
 
-        mobilePhone.printContact();
-        mobilePhone.updateContact(contact2, contact3);
-        mobilePhone.printContact();
-        mobilePhone.removeContact(contact2);
-        mobilePhone.printContact();
+        LinkedList<Song> playList = new LinkedList<Song>();
+        albums.get(0).addToPlayList("You can't do it right", playList);
+        albums.get(0).addToPlayList("Holy man", playList);
+        albums.get(0).addToPlayList("Speed king", playList);  // Does not exist
+        albums.get(0).addToPlayList(9, playList);
+        albums.get(1).addToPlayList(3, playList);
+        albums.get(1).addToPlayList(2, playList);
+        albums.get(1).addToPlayList(24, playList);  // There is no track 24
+
+        System.out.println(playList);
     }
 }
