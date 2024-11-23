c8ea62cda2ccae53a65b9184a0cbfea7fed91fbd
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-11-23 17:32:05 +0800

    LinkedList Challenge

--- DSA/src/main/java/dev/Innocent/Section7/ArrayAndArrayList/LinkedListChallenge.java
+++ DSA/src/main/java/dev/Innocent/Section7/ArrayAndArrayList/LinkedListChallenge.java
@@ -1,6 +1,7 @@
 package dev.Innocent.Section7.ArrayAndArrayList;
 
 import java.util.LinkedList;
+import java.util.Scanner;
 
 record Place(String name, int distance){
     @Override
@@ -22,6 +23,36 @@ public class LinkedListChallenge {
 
         placesToVisit.addFirst(new Place("allkin", 0));
         System.out.println(placesToVisit);
+
+        var iterator = placesToVisit.listIterator();
+        Scanner scanner = new Scanner(System.in);
+        boolean quitLoop = false;
+        boolean forward = true;
+
+        printMenu();
+
+        while (!quitLoop){
+            System.out.println("Enter value ");
+            String menuItem = scanner.nextLine().toUpperCase().substring(0,1);
+
+            switch (menuItem){
+                case "F":
+                    System.out.println("User wants to go forward ");
+                    break;
+                case "B":
+                    System.out.println("User wants to go Backward ");
+                    break;
+                case "L":
+                    System.out.println("User wants view the List ");
+                    break;
+                case "M":
+                    printMenu();
+                    break;
+                default:
+                    quitLoop = true;
+                    break;
+            }
+        }
     }
 
     private static void addPlace(LinkedList<Place> list, Place place){
@@ -44,5 +75,15 @@ public class LinkedListChallenge {
             matchedIndex++;
         }
         list.add(place);
+    }
+
+    public static void printMenu(){
+        System.out.println("""
+                Available actions (Select word or letter):
+                (F)orward
+                (B)arkward
+                (L)ist Places
+                (M)enu
+                (Q)uit""");
     }
 }
