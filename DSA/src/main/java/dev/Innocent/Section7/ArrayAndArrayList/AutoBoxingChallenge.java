2f86ef63470f76717405d506b9b288b1c0bc50cc
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-11-24 09:58:09 +0800

    Auto Boxing

--- DSA/src/main/java/dev/Innocent/Section7/ArrayAndArrayList/AutoBoxingChallenge.java
+++ DSA/src/main/java/dev/Innocent/Section7/ArrayAndArrayList/AutoBoxingChallenge.java
@@ -2,9 +2,9 @@ package dev.Innocent.Section7.ArrayAndArrayList;
 
 import java.util.ArrayList;
 
-record Customer(String name, ArrayList<Double> transaction){
+record Customers(String name, ArrayList<Double> transaction){
 
-    public Customer(String name, double initialDeposit){
+    public Customers(String name, double initialDeposit){
         this(name.toUpperCase(), new ArrayList<Double>(500));
         transaction.add(initialDeposit);
     }
@@ -12,10 +12,10 @@ record Customer(String name, ArrayList<Double> transaction){
 
 public class AutoBoxingChallenge {
     public static void main(String[] args) {
-        Customer bob = new Customer("Bob S", 10000);
+        Customers bob = new Customers("Bob S", 10000);
         System.out.println(bob);
 
-        Bank bank = new Bank("UOB Bank");
+        Banks bank = new Banks("UOB Bank");
         bank.addNewCustomer("Innocent Udo", 1500000);
         System.out.println(bank);
 
@@ -25,11 +25,11 @@ public class AutoBoxingChallenge {
     }
 }
 
-class Bank{
+class Banks {
     private String name;
-    private ArrayList<Customer> customers = new ArrayList<>(5000);
+    private ArrayList<Customers> customers = new ArrayList<>(5000);
 
-    public Bank(String name) {
+    public Banks(String name) {
         this.name = name;
     }
 
@@ -41,7 +41,7 @@ class Bank{
                 '}';
     }
 
-    private Customer getCustomer(String customerName) {
+    private Customers getCustomer(String customerName) {
         for(var customer : customers){
             if(customer.name().equalsIgnoreCase(customerName)){
                 return customer;
@@ -53,21 +53,21 @@ class Bank{
 
     public void addNewCustomer(String customerName, double initialDeposit){
         if(getCustomer(customerName) == null){
-            Customer customer = new Customer(customerName, initialDeposit);
+            Customers customer = new Customers(customerName, initialDeposit);
             customers.add(customer);
             System.out.println("New Customer added: " + customer);
         }
     }
 
     public void addTransaction(String name, double transactionAmount){
-        Customer customer = getCustomer(name);
+        Customers customer = getCustomer(name);
         if(customer != null){
             customer.transaction().add(transactionAmount);
         }
     }
 
     public void printStatement(String customerName){
-        Customer customer = getCustomer(customerName);
+        Customers customer = getCustomer(customerName);
         if(customer == null){
             return;
         }
