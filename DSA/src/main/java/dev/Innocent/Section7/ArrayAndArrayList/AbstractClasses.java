33cf1deada76e87a684c4b719d51ebfddc78df67
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-11-25 23:08:34 +0800

    Auto Boxing

---
+++ DSA/src/main/java/dev/Innocent/Section7/ArrayAndArrayList/AbstractClasses.java
@@ -0,0 +1,39 @@
+package dev.Innocent.Section7.ArrayAndArrayList;
+
+public class AbstractClasses {
+
+}
+
+abstract class Animal{
+    protected String type;
+    private String size;
+    private double weight;
+
+    public Animal(String type, String size, double weight) {
+        this.type = type;
+        this.size = size;
+        this.weight = weight;
+    }
+
+    public abstract void move(String speed);
+
+    public abstract void makeNoise();
+
+}
+
+class Dog extends Animal{
+
+    public Dog(String type, String size, double weight) {
+        super(type, size, weight);
+    }
+
+    @Override
+    public void move(String speed) {
+
+    }
+
+    @Override
+    public void makeNoise() {
+
+    }
+}
