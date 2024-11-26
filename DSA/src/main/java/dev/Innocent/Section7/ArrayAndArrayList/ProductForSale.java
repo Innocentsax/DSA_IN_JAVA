96e254d9d0b33387bfc2176869d832a0062c58bb
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-11-26 21:23:43 +0800

    Auto Boxing

--- DSA/src/main/java/dev/Innocent/Section7/ArrayAndArrayList/ProductForSale.java
+++ DSA/src/main/java/dev/Innocent/Section7/ArrayAndArrayList/ProductForSale.java
@@ -2,4 +2,22 @@ package dev.Innocent.Section7.ArrayAndArrayList;
 
 public abstract class ProductForSale {
     protected String type;
+    protected double price;
+    protected String description;
+
+    public ProductForSale(String type, double price, String description) {
+        this.type = type;
+        this.price = price;
+        this.description = description;
+    }
+
+    public double getSalesPrice(int qty) {
+        return qty * price;
+    }
+
+    public void printPricedItem(int qty){
+        System.out.printf("%2d qty at $%8.2f each, %-15s %-35s %n", qty, price, type, description);
+    }
+
+    public abstract void showDetails();
 }
