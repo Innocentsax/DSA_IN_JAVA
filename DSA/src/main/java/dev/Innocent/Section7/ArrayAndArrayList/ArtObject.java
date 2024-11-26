2635ae983b48920ab9ee945b00211d428346e4cd
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-11-26 21:23:38 +0800

    Auto Boxing

---
+++ DSA/src/main/java/dev/Innocent/Section7/ArrayAndArrayList/ArtObject.java
@@ -0,0 +1,15 @@
+package dev.Innocent.Section7.ArrayAndArrayList;
+
+public class ArtObject extends ProductForSale{
+
+    public ArtObject(String type, double price, String description) {
+        super(type, price, description);
+    }
+
+    @Override
+    public void showDetails() {
+        System.out.println("This " + type + " is a beautiful reproduction");
+        System.out.printf("The price of the piece is $%6.2f %n", price);
+        System.out.println(description);
+    }
+}
