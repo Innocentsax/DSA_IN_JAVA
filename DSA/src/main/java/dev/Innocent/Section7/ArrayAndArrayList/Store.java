3b40f4f210be2aad4f969acafb7e52bf0faa25c8
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-11-27 18:49:25 +0800

    Auto Boxing

--- DSA/src/main/java/dev/Innocent/Section7/ArrayAndArrayList/Store.java
+++ DSA/src/main/java/dev/Innocent/Section7/ArrayAndArrayList/Store.java
@@ -1,7 +1,20 @@
 package dev.Innocent.Section7.ArrayAndArrayList;
 
-public class Store {
-    public static void main(String[] args) {
+import java.util.ArrayList;
 
+public class Store {
+    private static ArrayList<ProductForSale> productForSales = new ArrayList<>();
+    public static void main(String[] args) {
+        productForSales.add(new ArtObject("kilishi", 249, "wewe"));
+        productForSales.add(new ArtObject("Suya", 3434, "Iban"));
+
+        listProduct();
+    }
+
+    public static void listProduct(){
+        for(var product : productForSales){
+            System.out.println("-".repeat(30));
+            product.showDetails();
+        }
     }
 }
