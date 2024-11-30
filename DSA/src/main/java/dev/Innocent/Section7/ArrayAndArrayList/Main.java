f40da8694d6cb65ccd10df8a32f50f4ca6e8893a
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-12-01 00:32:20 +0800

    Abstract / Interface

--- DSA/src/main/java/dev/Innocent/Section7/ArrayAndArrayList/Main.java
+++ DSA/src/main/java/dev/Innocent/Section7/ArrayAndArrayList/Main.java
@@ -1,46 +1,60 @@
 package dev.Innocent.Section7.ArrayAndArrayList;
 
+
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        ArrayList<Album> albums = new ArrayList<>();
+        Bird bird = new Bird();
+        Animals animals = bird;
+        FlightEnabled flier = bird;
+        Trackable tracked = bird;
 
-        Album album = new Album("Stormbringer", "Deep Purple");
-        album.addSong("Stormbringer", 4.6);
-        album.addSong("Love don't mean a thing", 4.22);
-        album.addSong("Holy man", 4.3);
-        album.addSong("Hold on", 5.6);
-        album.addSong("Lady double dealer", 3.21);
-        album.addSong("You can't do it right", 6.23);
-        album.addSong("High ball shooter", 4.27);
-        album.addSong("The gypsy", 4.2);
-        album.addSong("Soldier of fortune", 3.13);
-        albums.add(album);
+        animals.move();
+        inFlight(flier);
+        inFlight(new Jet());
 
-        album = new Album("For those about to rock", "AC/DC");
-        album.addSong("For those about to rock", 5.44);
-        album.addSong("I put the finger on you", 3.25);
-        album.addSong("Lets go", 3.45);
-        album.addSong("Inject the venom", 3.33);
-        album.addSong("Snowballed", 4.51);
-        album.addSong("Evil walks", 3.45);
-        album.addSong("C.O.D.", 5.25);
-        album.addSong("Breaking the rules", 5.32);
-        album.addSong("Night of the long knives", 5.12);
-        albums.add(album);
+        Trackable truck = new Truck();
+        truck.track();
 
-        LinkedList<Song> playList = new LinkedList<Song>();
-        albums.get(0).addToPlayList("You can't do it right", playList);
-        albums.get(0).addToPlayList("Holy man", playList);
-        albums.get(0).addToPlayList("Speed king", playList);  // Does not exist
-        albums.get(0).addToPlayList(9, playList);
-        albums.get(1).addToPlayList(3, playList);
-        albums.get(1).addToPlayList(2, playList);
-        albums.get(1).addToPlayList(24, playList);  // There is no track 24
+        double kmsTraveled = 100;
+        double milesTraveled = kmsTraveled * FlightEnabled.KM_TO_MILE;
+        System.out.printf("The truck traveled %.2f km or %.2f miles%n", kmsTraveled, milesTraveled);
 
-        System.out.println(playList);
+        List<FlightEnabled> flies = new ArrayList<>();
+        flies.add(bird);
+
+        triggerFlier(flies);
+        flyFlier(flies);
+        landFlier(flies);
+    }
+
+    private static void inFlight(FlightEnabled flies){
+        flies.takeOff();
+        flies.fly();
+        if(flies instanceof Trackable tracked){
+            tracked.track();
+        }
+        flies.land();
+    }
+
+    private static void triggerFlier(List<FlightEnabled> fliers){
+        for(var m : fliers){
+            m.takeOff();
+        }
+    }
+
+    private static void flyFlier(List<FlightEnabled> fliers){
+        for(var m : fliers){
+            m.fly();
+        }
+    }
+
+    private static void landFlier(List<FlightEnabled> fliers){
+        for(var m : fliers){
+            m.land();
+        }
     }
 }
