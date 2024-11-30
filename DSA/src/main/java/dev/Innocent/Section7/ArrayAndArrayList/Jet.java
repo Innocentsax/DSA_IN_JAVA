2efbc335575e93d30cda18ce2122dde44df0fc9a
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-12-01 00:32:17 +0800

    Abstract / Interface

---
+++ DSA/src/main/java/dev/Innocent/Section7/ArrayAndArrayList/Jet.java
@@ -0,0 +1,29 @@
+package dev.Innocent.Section7.ArrayAndArrayList;
+
+public class Jet implements FlightEnabled, Trackable{
+    @Override
+    public void takeOff() {
+        System.out.println(getClass().getSimpleName() + " is taking off");
+    }
+
+    @Override
+    public void land() {
+        System.out.println(getClass().getSimpleName() + " is landing");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println(getClass().getSimpleName() + " is flying");
+    }
+
+    @Override
+    public void track() {
+        System.out.println(getClass().getSimpleName() + " 's coordinates recorded");
+    }
+
+    @Override
+    public FlightStage transition(FlightStage stage) {
+        System.out.println(getClass().getSimpleName() + " transitioning");
+        return FlightEnabled.super.transition(stage);
+    }
+}
