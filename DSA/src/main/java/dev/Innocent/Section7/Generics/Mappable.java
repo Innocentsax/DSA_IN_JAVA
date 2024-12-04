efffa8651e6416391c611aa578d384b691efc7e2
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-12-04 20:15:40 +0800

    Generics

--- DSA/src/main/java/dev/Innocent/Section7/Generics/Mappable.java
+++ DSA/src/main/java/dev/Innocent/Section7/Generics/Mappable.java
@@ -31,3 +31,26 @@ abstract class Point implements Mappable{
         return Arrays.toString(location);
     }
 }
+
+abstract class Line implements Mappable{
+    private double[][] locations;
+
+    public Line (String... locations){
+        this.locations = new double[locations.length][];
+        int index = 0;
+        for(var l : locations){
+            this.locations[index++] = Mappable.stringToLatLon(l);
+        }
+    }
+
+    @Override
+    public void render() {
+        System.out.println("Render " + this + " as LINE (" + locations() + ")");
+    }
+
+    private String locations() {
+        return Arrays.deepToString(locations);
+    }
+}
+
+
