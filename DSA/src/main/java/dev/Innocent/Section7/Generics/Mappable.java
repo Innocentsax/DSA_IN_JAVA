9a32c89958e698ac2dc17880a265d424bf6ec9bb
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-12-04 19:32:57 +0800

    Generics

--- DSA/src/main/java/dev/Innocent/Section7/Generics/Mappable.java
+++ DSA/src/main/java/dev/Innocent/Section7/Generics/Mappable.java
@@ -1,10 +1,33 @@
 package dev.Innocent.Section7.Generics;
 
+import java.util.Arrays;
+
 public interface Mappable {
     void render();
 
 
     static double[] stringToLatLon(String location){
-        return
+        var splits = location.split(",");
+        double lat = Double.valueOf(splits[0]);
+        double lng = Double.valueOf(splits[1]);
+        return new double[]{lat, lng};
+    }
+
+}
+
+abstract class Point implements Mappable{
+    private double[] location = new double[2];
+
+    public Point(String location){
+        this.location = Mappable.stringToLatLon(location);
+    }
+
+    @Override
+    public void render() {
+        System.out.println("Render " + this + " as POINT (" + location() + ")");
+    }
+
+    private String location() {
+        return Arrays.toString(location);
     }
 }
