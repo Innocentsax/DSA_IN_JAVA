28694c6129da0179ada7936cad56d1b562f2e9cc
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-12-05 21:27:36 +0800

    Generics

---
+++ DSA/src/main/java/dev/Innocent/Section7/Generics/Park.java
@@ -0,0 +1,15 @@
+package dev.Innocent.Section7.Generics;
+
+public class Park extends Point{
+    private String name;
+
+    public Park(String name, String location) {
+        super(location);
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name + " National Park";
+    }
+}
