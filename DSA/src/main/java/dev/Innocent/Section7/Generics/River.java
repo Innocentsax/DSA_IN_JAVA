21306b26ce7647818303b1a53e01990942d7235b
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-12-05 21:27:40 +0800

    Generics

---
+++ DSA/src/main/java/dev/Innocent/Section7/Generics/River.java
@@ -0,0 +1,15 @@
+package dev.Innocent.Section7.Generics;
+
+public class River extends Line{
+    private String name;
+
+    public River(String name, String... locations) {
+        super(locations);
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name + " River";
+    }
+}
