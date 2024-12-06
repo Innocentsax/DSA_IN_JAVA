abd294c93b609a44f49c183c975e8a1e5089ece0
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-12-06 22:36:19 +0800

    Generics

--- DSA/src/main/java/dev/Innocent/Section7/Generics/LPAStudent.java
+++ DSA/src/main/java/dev/Innocent/Section7/Generics/LPAStudent.java
@@ -15,4 +15,12 @@ public class LPAStudent extends Student{
     public double getPercentComplete() {
         return percentComplete;
     }
+
+    @Override
+    public boolean matchFieldValue(String fieldName, String value) {
+        if(fieldName.equalsIgnoreCase("percentComplete")){
+            return percentComplete <= Integer.parseInt(value);
+        }
+        return super.matchFieldValue(fieldName, value);
+    }
 }
