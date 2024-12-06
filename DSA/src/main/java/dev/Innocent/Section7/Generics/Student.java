06206a7a8a1f0d5162dbfe83263bd2e12cd56959
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-12-06 22:36:33 +0800

    Generics

--- DSA/src/main/java/dev/Innocent/Section7/Generics/Student.java
+++ DSA/src/main/java/dev/Innocent/Section7/Generics/Student.java
@@ -2,7 +2,9 @@ package dev.Innocent.Section7.Generics;
 
 import java.util.Random;
 
-public class Student {
+public class Student implements QueryItem{
+    private static int LAST_ID = 10_000;
+    private int studentId;
     private String name;
     private String course;
     private int yearStarted;
@@ -12,6 +14,7 @@ public class Student {
     private static String[] courses = {"C++", "Java", "Python"};
 
     public Student(){
+        studentId = LAST_ID++;
         int lastNameIndex = random.nextInt(65, 91);
         name = firstNames[random.nextInt(5)] + " " + (char) lastNameIndex;
         course = courses[random.nextInt(3)];
@@ -20,10 +23,21 @@ public class Student {
 
     @Override
     public String toString() {
-        return "%-15s %-15s %d".formatted(name, course, yearStarted);
+        return "%d %-15s %-15s %d".formatted(studentId, name, course, yearStarted);
     }
 
     public int getYearStarted() {
         return yearStarted;
+    }
+
+    @Override
+    public boolean matchFieldValue(String fieldName, String value) {
+        String fName = fieldName.toUpperCase();
+        return switch (fName){
+            case "NAME" -> name.equalsIgnoreCase(value);
+            case "COURSE" -> course.equalsIgnoreCase(value);
+            case "YEARSTARTED" -> yearStarted == (Integer.parseInt(value));
+            default -> false;
+        };
     }
 }
