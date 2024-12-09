259ca167ae5b7b3b3cf9f372114501bfcd229bc6
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-12-09 08:01:48 +0800

    Nested Classes

--- DSA/src/main/java/dev/Innocent/Section7/Generics/Employee.java
+++ DSA/src/main/java/dev/Innocent/Section7/Generics/Employee.java
@@ -1,6 +1,30 @@
 package dev.Innocent.Section7.Generics;
 
+import java.util.Comparator;
+
 public class Employee {
+
+    public static class EmployeeComparator <T extends Employee> implements Comparator<Employee> {
+
+        private String sortType;
+
+        public EmployeeComparator() {
+            this("name");
+        }
+
+        public EmployeeComparator(String sortType) {
+            this.sortType = sortType;
+        }
+
+        @Override
+        public int compare(Employee o1, Employee o2) {
+            if(sortType.equalsIgnoreCase("yearStarted")){
+                return o1.yearStarted - o2.yearStarted;
+            }
+            return o1.name.compareTo(o2.name);
+        }
+    }
+
     private int employeeId;
     private String name;
     private int yearStarted;
@@ -20,6 +44,6 @@ public class Employee {
 
     @Override
     public String toString() {
-        return super.toString();
+        return "%d %-8s %d".formatted(employeeId, name, yearStarted);
     }
 }
