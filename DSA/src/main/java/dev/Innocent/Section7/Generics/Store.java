319ada1aa3f0aeb8320f5cc011a3238fb1ea5fdc
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-12-10 20:40:51 +0800

    Nested Classes

---
+++ DSA/src/main/java/dev/Innocent/Section7/Generics/Store.java
@@ -0,0 +1,8 @@
+package dev.Innocent.Section7.Generics;
+
+public class Store {
+    public static void main(String[] args) {
+        Meal regularMeal = new Meal();
+        System.out.println(regularMeal);
+    }
+}
