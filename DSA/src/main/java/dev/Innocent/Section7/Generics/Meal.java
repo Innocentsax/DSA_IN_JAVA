8767397c67a60625ab00da10b67f7c3368a75626
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-12-11 22:36:01 +0800

    Nested Classes

--- DSA/src/main/java/dev/Innocent/Section7/Generics/Meal.java
+++ DSA/src/main/java/dev/Innocent/Section7/Generics/Meal.java
@@ -1,8 +1,11 @@
 package dev.Innocent.Section7.Generics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Meal {
     private double base = 5.0;
-    private Item burger;
+    private Burger burger;
     private Item drink;
     private Item side;
     private double conversionRate;
@@ -13,14 +16,14 @@ public class Meal {
 
     public Meal(double conversionRate){
         this.conversionRate = conversionRate;
-        burger = new Item("regular", "burger");
+        burger = new Burger("regular");
         drink = new Item("coke", "drink", 1.5);
         System.out.println(drink.name);
         side = new Item("fries", "side", 2.0);
     }
 
     public double getTotal(){
-        double total = burger.price + drink.price + side.price;
+        double total = burger.getPrice() + drink.price + side.price;
         return Item.getPrice(total, conversionRate);
     }
 
@@ -51,6 +54,20 @@ public class Meal {
 
         private static double getPrice(double price, double rate){
             return price * rate;
+        }
+    }
+
+    private class Burger extends Item{
+
+
+        private List<Item> topping = new ArrayList<>();
+
+        Burger(String name){
+            super(name, "burger", 5.0);
+        }
+
+        public double getPrice(){
+            return super.price;
         }
     }
 }
