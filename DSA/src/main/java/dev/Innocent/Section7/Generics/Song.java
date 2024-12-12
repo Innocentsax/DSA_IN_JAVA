45b611ef5e44d61d9e3c179c32dc5077e5d10622
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-12-12 23:23:47 +0800

    PlayList InnerClass

--- DSA/src/main/java/dev/Innocent/Section7/Generics/Song.java
+++ DSA/src/main/java/dev/Innocent/Section7/Generics/Song.java
@@ -1,5 +1,20 @@
 package dev.Innocent.Section7.Generics;
 
 public class Song {
+    private String title;
+    private double duration;
 
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return title + ": " + duration;
+    }
 }
