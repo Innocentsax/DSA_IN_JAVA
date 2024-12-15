93256446934875eea1fc9896e701bdb1381ea90c
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-12-15 22:29:57 +0800

    Lambda

--- DSA/src/main/java/dev/Innocent/Section7/Generics/Person.java
+++ DSA/src/main/java/dev/Innocent/Section7/Generics/Person.java
@@ -1,10 +1,29 @@
 package dev.Innocent.Section7.Generics;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class Person {
 
-
+    record Persons(String firstName, String lastName){
+        @Override
+        public String toString() {
+            return firstName + " " + lastName;
+        }
+    }
 
     public static void main(String[] args) {
+        List<Persons> people = new ArrayList<>(Arrays.asList(
+                new Person.Persons("Innocent", "Udo"),
+                new Persons("David", "Koz"),
+                new Persons("Normal", "guy"),
+                new Persons("Ufan", "Uwem")
+        ));
 
+        // Using anonymous class
+        people.sort((o1, o2) -> o1.lastName.compareTo(o2.firstName));
+        System.out.println(people);
     }
 }
