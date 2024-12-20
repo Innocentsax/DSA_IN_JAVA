08629be49c1e6e8d667a5048568f36c9db82b36b
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-12-20 23:19:23 +0800

    Consumer Lambda

---
+++ DSA/src/main/java/dev/Innocent/Section7/Generics/ConsumerLambda.java
@@ -0,0 +1,14 @@
+package dev.Innocent.Section7.Generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConsumerLambda {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>(List.of(
+                "alpha", "bravo", "charlie", "delta"
+        ));
+
+
+    }
+}
