e129f29fed85b495648f3acf1fe263523b646449
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-12-21 13:09:58 +0800

    Consumer Lambda

--- DSA/src/main/java/dev/Innocent/Section7/Generics/ConsumerLambda.java
+++ DSA/src/main/java/dev/Innocent/Section7/Generics/ConsumerLambda.java
@@ -1,7 +1,9 @@
 package dev.Innocent.Section7.Generics;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.BiConsumer;
 import java.util.function.BinaryOperator;
 
 public class ConsumerLambda {
@@ -26,11 +28,25 @@ public class ConsumerLambda {
 
         int result = calculator((var a, var b) -> (a + b), 5, 2);
 
+        System.out.println("-------------");
+
+        var coords = Arrays.asList(
+                new double[]{47.2160, -95.2348},
+                new double[]{29.1566, -89.2495},
+                new double[]{35.1556, -90.0659}
+        );
+        coords.forEach(s -> System.out.println(Arrays.toString(s)));
+
+        
     }
 
     public static <T> T calculator(BinaryOperator<T> function, T value1, T value2){
         T result = function.apply(value1, value2);
         System.out.println("Result of Operation: " + result);
         return result;
+    }
+
+    public static <T> void processPoint(T t1, T t2, BiConsumer<T,T> consumer){
+        consumer.accept(t1, t2);
     }
 }
