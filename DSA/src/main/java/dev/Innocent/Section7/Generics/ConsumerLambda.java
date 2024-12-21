ec4488fecfe36960912cbcf1d1bf27d72c0d9c9d
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-12-21 11:13:41 +0800

    Consumer Lambda

--- DSA/src/main/java/dev/Innocent/Section7/Generics/ConsumerLambda.java
+++ DSA/src/main/java/dev/Innocent/Section7/Generics/ConsumerLambda.java
@@ -2,6 +2,7 @@ package dev.Innocent.Section7.Generics;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BinaryOperator;
 
 public class ConsumerLambda {
     public static void main(String[] args) {
@@ -22,5 +23,14 @@ public class ConsumerLambda {
             char first = s.charAt(0);
             System.out.println(prefix + " " + s + " means " + first);
         });
+
+        int result = calculator((var a, var b) -> (a + b), 5, 2);
+
+    }
+
+    public static <T> T calculator(BinaryOperator<T> function, T value1, T value2){
+        T result = function.apply(value1, value2);
+        System.out.println("Result of Operation: " + result);
+        return result;
     }
 }
