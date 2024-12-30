e3ef35c4836d67d1367df71ffee923a75c4b141c
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-12-30 19:14:44 +0800

    Collection

--- DSA/src/main/java/dev/Innocent/Section7/Collection/CollectionMethods.java
+++ DSA/src/main/java/dev/Innocent/Section7/Collection/CollectionMethods.java
@@ -2,6 +2,7 @@ package dev.Innocent.Section7.Collection;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 record Card(Suit suit, String face, int rank){
@@ -74,5 +75,10 @@ public class CollectionMethods {
     public static void main(String[] args) {
         List<Card> deck = Card.getStandardDeck();
         Card.printDeck(deck);
+
+        Card[] cardArray = new Card[13];
+        Card aceOfHearts = Card.getFaceCard(Card.Suit.HEART, 'A');
+        Arrays.fill(cardArray, aceOfHearts);
+        Card.printDeck(Arrays.asList(cardArray), "Aces of Hearts", 1);
     }
 }
