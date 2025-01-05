d69d2ff1333bd524e099af841e364d297d5c6903
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-01-05 20:48:07 +0800

    Collection

--- DSA/src/main/java/dev/Innocent/Section7/Collection/Ranking.java
+++ DSA/src/main/java/dev/Innocent/Section7/Collection/Ranking.java
@@ -1,5 +1,11 @@
 package dev.Innocent.Section7.Collection;
 
 public enum Ranking {
+    NONE, ONE_PAIR, TWO_PAIRS, THREE_OF_A_KIND, FULL_HOUSE, FOUR_OF_A_KIND;
 
+
+    @Override
+    public String toString() {
+        return this.name().replace('_', ' ');
+    }
 }
