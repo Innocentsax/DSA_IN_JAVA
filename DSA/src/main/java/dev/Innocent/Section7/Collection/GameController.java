38d156bc38201106da35051537ed47752f60afd2
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-01-07 23:09:05 +0800

    Collection

--- DSA/src/main/java/dev/Innocent/Section7/Collection/GameController.java
+++ DSA/src/main/java/dev/Innocent/Section7/Collection/GameController.java
@@ -3,6 +3,7 @@ package dev.Innocent.Section7.Collection;
 public class GameController {
 
     public static void main(String[] args) {
-
+        PokerGame fiveCardDraw = new PokerGame(4, 5);
+        fiveCardDraw.startPlay();
     }
 }
