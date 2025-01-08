1f1e18827b148da91c5bc5c319f9adbfc03bc15e
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-01-08 19:48:31 +0800

    Hash codes in Java

--- DSA/src/main/java/dev/Innocent/Section7/Collection/Hashing.java
+++ DSA/src/main/java/dev/Innocent/Section7/Collection/Hashing.java
@@ -1,5 +1,10 @@
 package dev.Innocent.Section7.Collection;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Hashing {
     public static void main(String[] args) {
         String aText = "Hello";
@@ -8,6 +13,12 @@ public class Hashing {
         String dText = "He".concat("llo");
         String eText = "hello";
 
+        List<String> hellos = Arrays.asList(aText, bText, cText, dText, eText);
+        hellos.forEach(s -> System.out.println(s + ": " + s.hashCode()));
 
+        Set<String> mySet = new HashSet<>(hellos);
+
+        System.out.println("mySet = " + mySet);
+        System.out.println("# of elements = " + mySet.size());
     }
 }
