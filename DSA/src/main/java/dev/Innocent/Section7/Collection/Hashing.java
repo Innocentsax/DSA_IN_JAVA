f2bebcd1595caa5e7c94ab81cfb6278d8143e1c5
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-01-08 19:39:44 +0800

    Hash codes in Java

---
+++ DSA/src/main/java/dev/Innocent/Section7/Collection/Hashing.java
@@ -0,0 +1,13 @@
+package dev.Innocent.Section7.Collection;
+
+public class Hashing {
+    public static void main(String[] args) {
+        String aText = "Hello";
+        String bText = "Hello";
+        String cText = String.join("l", "He", "lo");
+        String dText = "He".concat("llo");
+        String eText = "hello";
+
+
+    }
+}
