19b7ec1ec0b1cb04eff2008980e73b9825c1d664
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-01-10 21:10:33 +0800

    Hash codes in Java

--- DSA/src/main/java/dev/Innocent/Section7/Collection/SetsAndMaps.java
+++ DSA/src/main/java/dev/Innocent/Section7/Collection/SetsAndMaps.java
@@ -1,7 +1,9 @@
 package dev.Innocent.Section7.Collection;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class SetsAndMaps {
     public static void main(String[] args) {
@@ -10,6 +12,19 @@ public class SetsAndMaps {
 
         printData("Phone List", phones);
         printData("Email List", emails);
+
+        Set<Contact> emailContacts = new HashSet<>(emails);
+        Set<Contact> phoneContact = new HashSet<>(phones);
+
+        printData("Phone List", phoneContact);
+        printData("Email List", emailContacts);
+
+        int index = emails.indexOf(new Contact("Robin Hood"));
+        Contact robinHood = emails.get(index);
+        robinHood.addEmail("Sherwood Forest");
+        robinHood.addEmail("Sherwood Forest");
+        robinHood.replaceEmailIfExists("RHood@sherwoodforest.com", "RHood@sherwoodforest.org");
+        System.out.println(robinHood);
     }
 
     public static void printData(String header, Collection<Contact> contacts){
