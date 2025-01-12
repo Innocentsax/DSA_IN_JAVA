74c713934030646b1e556be967d2c06d3583a5d2
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-01-13 02:29:09 +0800

    Hash codes in Java

--- DSA/src/main/java/dev/Innocent/Section7/Collection/Task.java
+++ DSA/src/main/java/dev/Innocent/Section7/Collection/Task.java
@@ -1,5 +1,105 @@
 package dev.Innocent.Section7.Collection;
 
-public class Task {
+enum Priority {
+    HIGH, MEDIUM, LOW
+}
 
+enum Status {
+    IN_QUEUE, ASSIGNED, IN_PROGRESS
+}
+
+public class Task implements Comparable<Task>{
+    private String project;
+    private String description;
+    private String assignee;
+    private Priority priority;
+    private Status status;
+
+    public Task(String project, String description, String assignee, Priority priority, Status status) {
+        this.project = project;
+        this.description = description;
+        this.assignee = assignee;
+        this.priority = priority;
+        this.status = status;
+    }
+
+    public Task(String project, String description, String assignee, Priority priority) {
+        this(project, description, assignee, priority,
+                assignee == null ? Status.IN_QUEUE : Status.ASSIGNED);
+    }
+
+    public Task(String project, String description, Priority priority) {
+        this(project, description, null, priority);
+    }
+
+    public String getProject() {
+        return project;
+    }
+
+    public void setProject(String project) {
+        this.project = project;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAssignee() {
+        return assignee;
+    }
+
+    public void setAssignee(String assignee) {
+        this.assignee = assignee;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "%-20s %-25s %-10s %s".formatted(project, description, priority,
+                assignee, status);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Task task = (Task) o;
+        return getProject().equals(task.getProject()) && getDescription().equals(task.getDescription());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getProject().hashCode();
+        result = 31 * result + getDescription().hashCode();
+        return result;
+    }
+
+    @Override
+    public int compareTo(Task o) {
+        int result = this.project.compareTo(o.project);
+        if(result == 0){
+            result = this.description.compareTo(o.description);
+        }
+        return result;
+    }
 }
