5359643347a38fd9e398a7b21b1724aa2cd96f30
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-01-13 19:04:35 +0800

    Hash codes in Java

--- DSA/src/main/java/dev/Innocent/Section7/Collection/SetOperationChallenge.java
+++ DSA/src/main/java/dev/Innocent/Section7/Collection/SetOperationChallenge.java
@@ -4,21 +4,22 @@ import java.util.*;
 
 public class SetOperationChallenge {
     public static void main(String[] args) {
-        Set<Task> task = TaskData.getTasks("all");
-        sortAndPrint("All Tasks", task);
+
+        Set<Task> tasks = TaskData.getTasks("all");
+        sortAndPrint("All Tasks", tasks);
 
         Comparator<Task> sortByPriority = Comparator.comparing(Task::getPriority);
-
-        Set<Task> annsTask = TaskData.getTasks("Ann");
-        sortAndPrint("Ann's Tasks", annsTask, sortByPriority);
+        Set<Task> annsTasks = TaskData.getTasks("Ann");
+        sortAndPrint("Ann's Tasks", annsTasks, sortByPriority);
     }
 
-    private static void sortAndPrint(String header, Collection<Task> collection){
+    private static void sortAndPrint(String header, Collection<Task> collection) {
         sortAndPrint(header, collection, null);
     }
 
     private static void sortAndPrint(String header, Collection<Task> collection,
-                                     Comparator<Task> sorter){
+                                     Comparator<Task> sorter) {
+
         String lineSeparator = "_".repeat(90);
         System.out.println(lineSeparator);
         System.out.println(header);
@@ -27,5 +28,19 @@ public class SetOperationChallenge {
         List<Task> list = new ArrayList<>(collection);
         list.sort(sorter);
         list.forEach(System.out::println);
+    }
+
+    private static Set<Task> getUnion(List<Set<Task>> sets){
+        Set<Task> union = new HashSet<>();
+        for(var taskSet : sets){
+            union.addAll(taskSet);
+        }
+        return union;
+    }
+
+    private static Set<Task> getIntersect(Set<Task> a, Set<Task> b){
+        Set<Task> intersect = new HashSet<>(a);
+        intersect.retainAll(b);
+        return intersect;
     }
 }
