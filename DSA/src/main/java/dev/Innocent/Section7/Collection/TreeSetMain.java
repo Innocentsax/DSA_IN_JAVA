bb8098b638fb4c4cb6da0f0ba4c504b2d8b41595
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-01-14 21:26:46 +0800

    Hash codes in Java

---
+++ DSA/src/main/java/dev/Innocent/Section7/Collection/TreeSetMain.java
@@ -0,0 +1,46 @@
+package dev.Innocent.Section7.Collection;
+
+import java.util.*;
+
+public class TreeSetMain {
+    public static void main(String[] args) {
+        List<Contact> phones = ContactData.getData("phone");
+        List<Contact> email = ContactData.getData("email");
+
+        Comparator<Contact> mySort = Comparator.comparing(Contact::getName);
+        NavigableSet<Contact> sorted = new TreeSet<>(mySort);
+        sorted.addAll(phones);
+        sorted.forEach(System.out::println);
+
+        NavigableSet<String> justNames = new TreeSet<>();
+        phones.forEach(c -> justNames.add(c.getName()));
+        System.out.println(justNames);
+
+        NavigableSet<Contact> fullSet = new TreeSet<>(sorted);
+        fullSet.addAll(email);
+        fullSet.forEach(System.out::println);
+
+        List<Contact> fullList = new ArrayList<>(phones);
+        fullList.addAll(email);
+        fullList.sort(sorted.comparator());
+        System.out.println("-----------------------------------------");
+        fullList.forEach(System.out::println);
+
+        Contact min = Collections.min(fullSet, fullSet.comparator());
+        Contact max = Collections.max(fullSet, fullSet.comparator());
+
+        Contact first = fullSet.first();
+        Contact last = fullSet.last();
+
+        System.out.println("-------------------------------------------");
+        System.out.printf("Min = %s, first=%s %n", min.getName(), first.getName());
+        System.out.printf("Min = %s, last=%s %n", max.getName(), last.getName());
+        System.out.println("--------------------------------------------");
+
+        NavigableSet<Contact> copiedSet = new TreeSet<>(fullSet);
+        System.out.println("First element = " + copiedSet.pollFirst());
+        System.out.println("Last element = " + copiedSet.pollLast());
+        copiedSet.forEach(System.out::println);
+        System.out.println("-------------------------------------------");
+    }
+}
