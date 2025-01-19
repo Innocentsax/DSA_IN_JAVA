ef5e7f1be44ca62aae05d5417c90191ee45d0802
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-01-19 19:36:28 +0800

    TreeSet in Java

--- DSA/src/main/java/dev/Innocent/Section7/Collection/BookingAgent.java
+++ DSA/src/main/java/dev/Innocent/Section7/Collection/BookingAgent.java
@@ -2,6 +2,9 @@ package dev.Innocent.Section7.Collection;
 
 public class BookingAgent {
     public static void main(String[] args) {
-
+        int rows = 10;
+        int totalSeats = 100;
+        Theatre rodgersNYC = new Theatre("Richard Rodgers", rows, totalSeats);
+        rodgersNYC.printSeatMap();
     }
 }
