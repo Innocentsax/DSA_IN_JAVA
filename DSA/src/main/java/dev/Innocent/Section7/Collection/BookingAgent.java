09a938f7dd7dad68e8f16b301d4f0d47599047b7
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-01-19 20:16:47 +0800

    TreeSet in Java

--- DSA/src/main/java/dev/Innocent/Section7/Collection/BookingAgent.java
+++ DSA/src/main/java/dev/Innocent/Section7/Collection/BookingAgent.java
@@ -6,5 +6,22 @@ public class BookingAgent {
         int totalSeats = 100;
         Theatre rodgersNYC = new Theatre("Richard Rodgers", rows, totalSeats);
         rodgersNYC.printSeatMap();
+
+        bookSeat(rodgersNYC, 'A', 3);
+        bookSeat(rodgersNYC, 'A', 3);
+
+        bookSeat(rodgersNYC, 'B', 1);
+        bookSeat(rodgersNYC, 'M', 11);
+
+    }
+
+    private static void bookSeat(Theatre theatre, char row, int seatNo){
+        String seat = theatre.reserveSeat(row, seatNo);
+        if(seat != null){
+            System.out.println("Congratulation! Your reserved seat is " + seat);
+            theatre.printSeatMap();
+        }else {
+            System.out.println("Sorry! Unable to reserve " + row + seatNo);
+        }
     }
 }
