b07a4d9a06bfac245dcadb9c45f4a8289dd2499e
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-01-23 22:31:16 +0800

    TreeSet in Java

--- DSA/src/main/java/dev/Innocent/Section7/Collection/MapMain.java
+++ DSA/src/main/java/dev/Innocent/Section7/Collection/MapMain.java
@@ -1,7 +1,9 @@
 package dev.Innocent.Section7.Collection;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MapMain {
     public static void main(String[] args) {
@@ -12,5 +14,20 @@ public class MapMain {
         fullList.addAll(emails);
         fullList.forEach(System.out::println);
         System.out.println("-------------------------------------------");
+
+        Map<String, Contact> contacts = new HashMap<>();
+        for (Contact contact : fullList){
+            contacts.put(contact.getName(), contact);
+        }
+        contacts.forEach((k, v) -> System.out.println("key = " + k + ", value = " + v));
+
+        System.out.println("-------------------------------------------");
+        System.out.println(contacts.get("Charlie Brown"));
+
+        Contact defaultContact = new Contact("Chuck Brown");
+        System.out.println(contacts.getOrDefault("Chuck Brown", defaultContact));
+
+        System.out.println("-------------------------------------------");
+
     }
 }
