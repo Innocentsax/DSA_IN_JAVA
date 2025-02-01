dff6df3db465509522c97eabbc3dd52ad51819ee
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-02-01 10:52:33 +0800

    Map in Java

--- DSA/src/main/java/dev/Innocent/Section7/Collection/Student.java
+++ DSA/src/main/java/dev/Innocent/Section7/Collection/Student.java
@@ -1,6 +1,8 @@
 package dev.Innocent.Section7.Collection;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 record Course(String courseId, String name, String subject){}
 
@@ -11,4 +13,19 @@ record Purchase(String courseId, int studentId, double price, int yr, int dayOfY
 }
 
 public class Student {
+    private static int lastId = 1;
+    private String name;
+    private int id;
+    private List<Course> courseList;
+
+    public Student(String name, List<Course> courseList) {
+        this.name = name;
+        this.courseList = courseList;
+        id = lastId++;
+    }
+
+    public Student(String name, Course course) {
+        this(name, new ArrayList<>(List.of(course)));
+    }
+
 }
