8cf8d471a86a39365717ce44eda62269e54ee961
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-02-01 16:27:50 +0800

    Map in Java

--- DSA/src/main/java/dev/Innocent/Section7/Collection/Student.java
+++ DSA/src/main/java/dev/Innocent/Section7/Collection/Student.java
@@ -59,10 +59,28 @@ public class Student {
         addPurchase("Joe Jones", jmc, 149.99);
         addPurchase("Bill Brown", python, 119.99);
 
+        addPurchase("Chuck Cheese", python, 119.99);
+        addPurchase("Davey Jones", jmc, 139.99);
+        addPurchase("Eva East", python, 139.99);
+        addPurchase("Fred Forker", jmc, 139.99);
+        addPurchase("Greg Brady", python, 129.99);
+
+
         purchases.forEach((key, value) -> System.out.println(key + ": " + value));
         System.out.println("-------------------------------------");
         students.forEach((key, value) -> System.out.println(key + ": " + value));
 
+
+        NavigableMap<LocalDate, List<Purchase>> datedPurchases = new TreeMap<>();
+
+        for(Purchase p : purchases.values()){
+            datedPurchases.compute(p.purchaseDate(), (pdate, plist) -> {
+                List<Purchase> list = (plist == null) ? new ArrayList<>() : plist;
+                list.add(p);
+                return list;
+            });
+        }
+        datedPurchases.forEach((key, value) -> System.out.println(key + ": " + value));
     }
 
     private static void addPurchase(String name, Course course, double price){
@@ -74,7 +92,7 @@ public class Student {
             existingStudent.addCourse(course);
         }
 
-        int day = purchases.size() + 1;
+        int day = new Random().nextInt(1, 15);
         String key = course.courseId() + "_" + existingStudent.getId();
         int year = LocalDate.now().getYear();
         Purchase purchase = new Purchase(course.courseId(), existingStudent.getId(),
