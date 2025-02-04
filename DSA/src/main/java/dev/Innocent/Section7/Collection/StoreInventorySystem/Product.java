571d8f2a22c09ca0d2712d0546641e4032967608
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-02-04 21:22:29 +0800

    Map in Java

---
+++ DSA/src/main/java/dev/Innocent/Section7/Collection/StoreInventorySystem/Product.java
@@ -0,0 +1,8 @@
+package dev.Innocent.Section7.Collection.StoreInventorySystem;
+
+public record Product(
+        String sku,
+        String name,
+        String mfgr,
+        Category category) {
+}
