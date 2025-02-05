284ddc987a6d7eca59e11b66ac887075513fa729
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-02-05 18:21:11 +0800

    Map in Java

--- DSA/src/main/java/dev/Innocent/Section7/Collection/StoreInventorySystem/Store.java
+++ DSA/src/main/java/dev/Innocent/Section7/Collection/StoreInventorySystem/Store.java
@@ -1,4 +1,38 @@
 package dev.Innocent.Section7.Collection.StoreInventorySystem;
 
+import java.util.*;
+
 public class Store {
+    private static Random random = new Random();
+    private Map<String, InventoryItem> inventory;
+    private NavigableSet<Cart> carts = new TreeSet<>(Comparator.comparing(Cart::getId));
+    private Map<Category, Map<String, InventoryItem>> aisleInventory;
+
+    public static void main(String[] args) {
+
+    }
+
+    private void manageStoreCarts(){
+
+    }
+
+    private boolean checkOutCart(Cart cart){
+        return false;
+    }
+
+    private void abandonCarts(){
+
+    }
+
+    private void listProductsByCategory(){
+
+    }
+
+    private void stockStore(){
+
+    }
+
+    private void stockAisles(){
+
+    }
 }
