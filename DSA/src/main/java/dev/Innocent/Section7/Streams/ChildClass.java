085ad34913f622bc263d939e675f117934269eda
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-02-09 19:11:16 +0800

    Store Inventary

--- DSA/src/main/java/dev/Innocent/Section7/Streams/ChildClass.java
+++ DSA/src/main/java/dev/Innocent/Section7/Streams/ChildClass.java
@@ -1,4 +1,19 @@
 package dev.Innocent.Section7.Streams;
 
 public class ChildClass extends BassClass{
+    @Override
+    protected void optionalMethod() {
+        System.out.println("[Child:optionalMethod] EXTRA Stuff Here");
+        super.optionalMethod();
+    }
+
+//    @Override
+//    public void recommendedMethod() {
+//        System.out.println("[Child:recommendedMethod]: I'll do things my way");
+//        optionalMethod();
+//    }
+
+    private void mandatoryMethod(){
+        System.out.println("[Child:mandatoryMethod]: My own important stuff");
+    }
 }
