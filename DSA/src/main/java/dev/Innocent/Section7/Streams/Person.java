8027786ab598fe0f27974ae6e1cd06c009a7b027
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-02-15 09:25:58 +0800

    Stream

--- DSA/src/main/java/dev/Innocent/Section7/Streams/Person.java
+++ DSA/src/main/java/dev/Innocent/Section7/Streams/Person.java
@@ -44,17 +44,4 @@ public class Person {
         return name + ", dob = " + dob + ", kids = " + kidString;
     }
 
-    public static void main(String[] args) {
-        Person jane = new Person();
-        jane.setName("Jane");
-        Person jim = new Person();
-        jim.setName("Jim");
-        Person joe = new Person();
-        joe.setName("Joe");
-        Person john = new Person();
-        john.setName("John");
-        john.setDob("05/05/1900");
-        john.setKids(new Person[]{jane, jim, joe});
-        System.out.println(john);
-    }
 }
