681c3d8cfbf76503fd5502eabefcf94620e78367
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-02-18 21:24:09 +0800

    Stream

--- DSA/src/main/java/dev/Innocent/Section7/Streams/Bank.java
+++ DSA/src/main/java/dev/Innocent/Section7/Streams/Bank.java
@@ -1,4 +1,45 @@
 package dev.Innocent.Section7.Streams;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Bank {
+    public final int routingNumber;
+    private long lastTransactionId = 1;
+    private final Map<String, BankCustomer> customers;
+
+    public Bank(int routingNumber) {
+        this.routingNumber = routingNumber;
+        customers = new HashMap<>();
+    }
+
+    public BankCustomer getCustomer(String id){
+        BankCustomer customer = customers.get(id);
+        return customer;
+    }
+
+    public void addCustomer(String name, double checkingInitialDeposit,
+                            double savingsInitialDeposit){
+        BankCustomer customer = new BankCustomer(name, checkingInitialDeposit,
+                savingsInitialDeposit);
+        customers.put(customer.getCustomerId(), customer);
+    }
+
+    public boolean doTransaction(String id, BankAccount.AccountType accountType, double amount){
+        BankCustomer customer = customers.get(id);
+        if(customer != null){
+            BankAccount account = customer.getAccount(accountType);
+            if(account != null){
+                if((account.getBalance() + amount) < 0){
+                    System.out.println("Insufficient funds");
+                }else {
+                    account.commitTransaction(routingNumber, lastTransactionId++, id, amount);
+                    return true;
+                }
+            }
+        } else{
+            System.out.println("Invalid customer id");
+        }
+        return false;
+    }
 }
