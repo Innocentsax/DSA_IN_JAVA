0b78d233706fce4c9d062cbce0c8ea51bcc65a5f
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-02-22 14:57:56 +0800

    Stream

--- DSA/src/main/java/dev/Innocent/Section7/Streams/Parent.java
+++ DSA/src/main/java/dev/Innocent/Section7/Streams/Parent.java
@@ -1,8 +1,32 @@
 package dev.Innocent.Section7.Streams;
 
 public class Parent {
+    private String name;
+    private String dob;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    @Override
+    public String toString() {
+        return "name = '" + name + '\'' + ", dob = '" + dob + '\'';
+    }
 
     public static void main(String[] args) {
         Parent parent = new Parent();
+        Child child = new Child();
     }
 }
