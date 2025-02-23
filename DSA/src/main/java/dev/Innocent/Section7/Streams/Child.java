442344958df45af7edaa957f10b552b523bea721
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-02-23 20:52:09 +0800

    Stream

--- DSA/src/main/java/dev/Innocent/Section7/Streams/Child.java
+++ DSA/src/main/java/dev/Innocent/Section7/Streams/Child.java
@@ -1,4 +1,26 @@
 package dev.Innocent.Section7.Streams;
 
+import java.util.Random;
+
 public class Child extends Parent {
+
+    private final int birthOrder = getBirthOrder();
+    private final String birthOrderString;
+
+    {
+        if(siblings == 0){
+            birthOrderString = "Only";
+        } else if (birthOrder == 1) {
+            birthOrderString = "Last";
+        } else if (birthOrder == (siblings + 1)) {
+            birthOrderString = "Last";
+        }else {
+            birthOrderString = "Middle";
+        }
+    }
+
+    private final int getBirthOrder(){
+        if(siblings == 0) return 1;
+        return new Random().nextInt(1, siblings + 2);
+    }
 }
