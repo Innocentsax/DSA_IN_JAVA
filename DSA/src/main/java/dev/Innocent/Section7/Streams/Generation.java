ae1e24f66dc9fdafa7601831206dc34a9e3abb6a
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-02-24 19:50:37 +0800

    Stream

---
+++ DSA/src/main/java/dev/Innocent/Section7/Streams/Generation.java
@@ -0,0 +1,14 @@
+package dev.Innocent.Section7.Streams;
+
+public enum Generation {
+    GEN_Z,
+    MILLENNIAL,
+    GEN_X,
+    BABY_BOOMER,
+    SILENT_GENERATION,
+    GREATEST_GENERATION;
+
+    Generation() {
+        System.out.println(this);
+    }
+}
