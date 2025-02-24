78cd362b535f25fc8c17ae51f4f9f1af479d5d86
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-02-24 21:48:21 +0800

    Stream

--- DSA/src/main/java/dev/Innocent/Section7/Streams/Generation.java
+++ DSA/src/main/java/dev/Innocent/Section7/Streams/Generation.java
@@ -1,14 +1,30 @@
 package dev.Innocent.Section7.Streams;
 
+import java.time.LocalDate;
+
 public enum Generation {
     GEN_Z,
-    MILLENNIAL,
-    GEN_X,
-    BABY_BOOMER,
-    SILENT_GENERATION,
-    GREATEST_GENERATION;
+    MILLENNIAL(1981, 2000),
+    GEN_X(1965, 1980),
+    BABY_BOOMER(1946, 1980),
+    SILENT_GENERATION(1927, 1945),
+    GREATEST_GENERATION(1901, 1926);
+
+    private final int startYear;
+    private final int endYear;
 
     Generation() {
+        this(2001, LocalDate.now().getYear());
+    }
+
+    Generation(int startYear, int endYear) {
+        this.startYear = startYear;
+        this.endYear = endYear;
         System.out.println(this);
+    }
+
+    @Override
+    public String toString() {
+        return this.name() + " " + startYear + " - " + endYear;
     }
 }
