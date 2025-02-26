14b7c27f4af02c48974069ae23a9b6b7b75c1bdf
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-02-26 22:23:11 +0800

    Stream

--- DSA/src/main/java/dev/Innocent/Section7/Streams/GameConsole/Main.java
+++ DSA/src/main/java/dev/Innocent/Section7/Streams/GameConsole/Main.java
@@ -2,6 +2,9 @@ package dev.Innocent.Section7.Streams.GameConsole;
 
 public class Main {
     public static void main(String[] args) {
+        var console = new GameConsole<>(new ShooterGame("The Shootout Game"));
 
+        int playerIndex = console.addPlayer();
+        console.playGame(playerIndex);
     }
 }
