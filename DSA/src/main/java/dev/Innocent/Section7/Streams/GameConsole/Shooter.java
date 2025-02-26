5375b9c279050ce6534d2ac750b161a7f110f9a7
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-02-26 22:23:17 +0800

    Stream

---
+++ DSA/src/main/java/dev/Innocent/Section7/Streams/GameConsole/Shooter.java
@@ -0,0 +1,13 @@
+package dev.Innocent.Section7.Streams.GameConsole;
+
+public record Shooter(String name) implements Player {
+    boolean findPrize(){
+        System.out.println("Prize found, score should be adjusted.");
+        return false;
+    }
+
+    boolean useWeapon(String weapon){
+        System.out.println("You Shot your " + weapon);
+        return false;
+    }
+}
