1932e36d6148c2a076f0c928c33add72976b8804
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-02-27 21:28:39 +0800

    Stream

--- DSA/src/main/java/dev/Innocent/Section7/Streams/GameConsole/Weapon.java
+++ DSA/src/main/java/dev/Innocent/Section7/Streams/GameConsole/Weapon.java
@@ -1,4 +1,33 @@
 package dev.Innocent.Section7.Streams.GameConsole;
 
 public enum Weapon {
+    KNIFE(0, 10),
+    AXE(0, 30),
+    MACHETE(1, 40),
+    PISTOL(1, 50);
+
+    private final int minLevel;
+    private final int hitPoints;
+
+    Weapon(int minLevel, int hitPoints) {
+        this.minLevel = minLevel;
+        this.hitPoints = hitPoints;
+    }
+
+    public int getMinLevel() {
+        return minLevel;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public static Weapon getWeaponByChar(char firstInitial){
+        for(var w : Weapon.values()){
+            if(w.name().charAt(0) == firstInitial){
+                return w;
+            }
+        }
+        return values()[0];
+    }
 }
