bc8186ffafff76084f741d590f6c3ae7664cea34
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-02-28 20:49:51 +0800

    Stream

--- DSA/src/main/java/dev/Innocent/Section7/Streams/GameConsole/Main.java
+++ DSA/src/main/java/dev/Innocent/Section7/Streams/GameConsole/Main.java
@@ -2,9 +2,19 @@ package dev.Innocent.Section7.Streams.GameConsole;
 
 public class Main {
     public static void main(String[] args) {
-        var console = new GameConsole<>(new ShooterGame("The Shootout Game"));
+//        var console = new GameConsole<>(new ShooterGame("The Shootout Game"));
+//
+//        int playerIndex = console.addPlayer();
+//        console.playGame(playerIndex);
 
-        int playerIndex = console.addPlayer();
-        console.playGame(playerIndex);
+        Weapon weapon = Weapon.getWeaponByChar('P');
+        System.out.println("weapon = " + weapon + ", hitPoints" +
+                weapon.getHitPoints() + ", minLevel = " + weapon.getMinLevel());
+
+        var list = Weapon.getWeaponsByLevel(1);
+        list.forEach(System.out::println);
+
+        Pirate Inno = new Pirate("Innocent");
+        System.out.println(Inno);
     }
 }
