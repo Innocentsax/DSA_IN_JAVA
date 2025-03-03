205f3b1c0d8eef4779dbda3f95e0e72ff0f48f19
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-03-03 20:45:14 +0800

    Stream

--- DSA/src/main/java/dev/Innocent/Section7/Streams/GameConsole/Pirate.java
+++ DSA/src/main/java/dev/Innocent/Section7/Streams/GameConsole/Pirate.java
@@ -53,11 +53,13 @@ public class Pirate implements Player{
 
     boolean useWeapon(){
         System.out.println("Used the " + currentWeapon);
-        return false;
+        return visitNextTown();
     }
 
     boolean visitTown(){
-        String town = "My Town, somewhere";
+        List<String> levelTowns = PirateGame.getTowns(value("level"));
+        if(levelTowns == null) return true;
+        String town = levelTowns.get(value("townIndex"));
         if(town != null){
             townsVisited.add(town);
             return false;
@@ -77,5 +79,22 @@ public class Pirate implements Player{
         Arrays.setAll(simpleNames, i -> townsVisited.get(i).split(",")[0]);
         return "----> " + current + "\nPirate " + name + " " + gameData + "\n\ttownsVisited="
                 + Arrays.toString(simpleNames);
+    }
+
+    private boolean visitNextTown(){
+        int townIndex = value("townIndex");
+        var towns = PirateGame.getTowns(value("level"));
+        if(towns == null) return true;
+        if(townIndex >= (towns.size() - 1)){
+            System.out.println("Leveling up! Bonus: 500 points");
+            adjustValue("score", 500);
+            adjustValue("level", 1);
+            setValue("townIndex", 0);
+        }else {
+            System.out.println("Sailing to next town! Bonus: 50 points");
+            adjustValue("townIndex", 1);
+            adjustValue("score", 50);
+        }
+        return visitTown();
     }
 }
