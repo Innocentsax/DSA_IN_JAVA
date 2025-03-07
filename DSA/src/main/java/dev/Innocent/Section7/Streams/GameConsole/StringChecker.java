0ace04426ca409ac419408df907f1053130c3c3e
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-03-08 00:57:55 +0800

    Sealed Class

---
+++ DSA/src/main/java/dev/Innocent/Section7/Streams/GameConsole/StringChecker.java
@@ -0,0 +1,10 @@
+package dev.Innocent.Section7.Streams.GameConsole;
+
+import java.util.function.Predicate;
+
+public final class StringChecker implements SealedInterface{
+    @Override
+    public boolean testData(Predicate<String> p, String... String) {
+        return false;
+    }
+}
