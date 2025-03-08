92e4d4097c28edb1fdc6ceaf44ed4236143ddd8e
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-03-08 21:45:53 +0800

    Sealed Class

---
+++ DSA/src/main/java/dev/Innocent/Section7/Streams/GameConsole/GameConsoleThatWorks/out/production/GameConsole/dev/lpa/game/pirate/Feature.java
@@ -0,0 +1,22 @@
+package dev.Innocent.Section7.Streams.GameConsole.GameConsoleThatWorks.out.production.GameConsole.dev.lpa.game.pirate;
+
+public enum Feature {
+
+    ALLIGATOR(-45),
+    ALOE(5),
+    JELLYFISH (-10),
+    PINEAPPLE(10),
+    SNAKE(-25),
+    SPRING(25),
+    SUN_POISON(-15);
+
+    private final int healthPoints;
+
+    Feature(int healthPoints) {
+        this.healthPoints = healthPoints;
+    }
+
+    public int getHealthPoints() {
+        return healthPoints;
+    }
+}
