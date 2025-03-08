373846e218b788357afb20572476dd547c05b1b9
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-03-08 21:45:38 +0800

    Sealed Class

---
+++ DSA/src/main/java/dev/Innocent/Section7/Streams/GameConsole/GameConsoleThatWorks/out/production/GameConsole/dev/lpa/game/sealed/BetterInterface.java
@@ -0,0 +1,4 @@
+package dev.Innocent.Section7.Streams.GameConsole.GameConsoleThatWorks.out.production.GameConsole.dev.lpa.game.sealed;
+
+public non-sealed interface BetterInterface extends SealedInterface {
+}
