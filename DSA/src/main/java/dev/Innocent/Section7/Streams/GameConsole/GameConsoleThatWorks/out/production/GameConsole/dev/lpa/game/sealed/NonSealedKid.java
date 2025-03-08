e68f2a722592f7ae2a9f891342a66ba47ce848be
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-03-08 21:47:08 +0800

    Sealed Class

---
+++ DSA/src/main/java/dev/Innocent/Section7/Streams/GameConsole/GameConsoleThatWorks/out/production/GameConsole/dev/lpa/game/sealed/NonSealedKid.java
@@ -0,0 +1,4 @@
+package dev.Innocent.Section7.Streams.GameConsole.GameConsoleThatWorks.out.production.GameConsole.dev.lpa.game.sealed;
+
+public non-sealed class NonSealedKid extends SpecialAbstractClass {
+}
