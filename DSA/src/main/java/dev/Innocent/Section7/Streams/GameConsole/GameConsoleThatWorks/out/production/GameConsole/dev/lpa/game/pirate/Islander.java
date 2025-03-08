5b67a50510f2815b0bd2de0e95ceae7cc6ca7c70
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-03-08 21:46:15 +0800

    Sealed Class

---
+++ DSA/src/main/java/dev/Innocent/Section7/Streams/GameConsole/GameConsoleThatWorks/out/production/GameConsole/dev/lpa/game/pirate/Islander.java
@@ -0,0 +1,9 @@
+package dev.Innocent.Section7.Streams.GameConsole.GameConsoleThatWorks.out.production.GameConsole.dev.lpa.game.pirate;
+
+public final class Islander extends Combatant {
+
+    public Islander(String name, Weapon weapon) {
+        super(name);
+        setCurrentWeapon(weapon);
+    }
+}
