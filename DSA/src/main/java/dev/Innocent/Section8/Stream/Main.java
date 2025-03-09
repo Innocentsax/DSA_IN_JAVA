6a6ee01f87e896e61b23b4d4687ce0fb1817124e
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-03-09 18:08:06 +0800

    Sealed Class

---
+++ DSA/src/main/java/dev/Innocent/Section8/Stream/Main.java
@@ -0,0 +1,57 @@
+package dev.Innocent.Section8.Stream;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> bingoPool = new ArrayList<>(75);
+
+        int start = 1;
+        for(char c : "BINGO".toCharArray()){
+            for(int i = start; i < (start + 15); i++){
+                bingoPool.add("" + c + i);
+                System.out.println("" + c + i);
+            }
+            start += 15;
+        }
+
+        Collections.shuffle(bingoPool);
+        for(int i = 0; i < 15; i++){
+            System.out.println(bingoPool.get(i));
+        }
+        System.out.println("------------------------------------");
+
+//        List<String> firstOnes = bingoPool.subList(0, 15);
+        List<String> firstOnes = new ArrayList<>(bingoPool.subList(0, 15));
+        firstOnes.sort(Comparator.naturalOrder());
+        firstOnes.replaceAll(s -> {
+            if(s.indexOf('G') == 0 || s.indexOf("O") == 0){
+                String updated = s.charAt(0) + "-" + s.substring(1);
+                System.out.print(updated + " ");
+                return updated;
+            }
+            return s;
+        });
+        System.out.println("\n------------------------------------------");
+
+        for(int i = 0; i < 15; i++){
+            System.out.println(bingoPool.get(i));
+        }
+        System.out.println("------------------------------------");
+
+        var tempStream = bingoPool.stream()
+                .limit(15)
+                .filter(s -> s.indexOf('G') == 0 || s.indexOf("O") == 0)
+                .map(s -> s.charAt(0) + "-" + s.substring(1))
+                .sorted();
+//                .forEach(s -> System.out.print(s + " "));
+
+
+        tempStream.forEach(s -> System.out.println(s + " "));
+        System.out.println("------------------------------------");
+    }
+
+}
