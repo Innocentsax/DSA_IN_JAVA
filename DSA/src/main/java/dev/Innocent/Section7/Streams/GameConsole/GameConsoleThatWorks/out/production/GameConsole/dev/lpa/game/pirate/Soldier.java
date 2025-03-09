eb653feae13bfb1b9ee289c30ce39046a62e2107
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-03-09 18:08:23 +0800

    Sealed Class

---
+++ DSA/src/main/java/dev/Innocent/Section7/Streams/GameConsole/GameConsoleThatWorks/out/production/GameConsole/dev/lpa/game/pirate/Soldier.java
@@ -0,0 +1,9 @@
+package dev.Innocent.Section7.Streams.GameConsole.GameConsoleThatWorks.out.production.GameConsole.dev.lpa.game.pirate;
+
+public final class Soldier extends Combatant {
+
+    public Soldier(String name, Weapon weapon) {
+        super(name);
+        setCurrentWeapon(weapon);
+    }
+}
