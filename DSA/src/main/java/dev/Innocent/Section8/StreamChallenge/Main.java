971ca05a4ed8f50f11d80e5f9b4111a38032d5ce
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-03-17 20:07:35 +0800

    Stream

--- DSA/src/main/java/dev/Innocent/Section8/StreamChallenge/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/StreamChallenge/Main.java
@@ -11,14 +11,14 @@ public class Main {
         int seed = 1;
         var streamB = Stream.iterate(seed, i -> i <= 15, i -> i + 1)
                 .map(i -> "B" + i);
-        streamB.forEach(System.out::println);
+//        streamB.forEach(System.out::println);
 
         System.out.println("-----------------------------------------------");
 
         seed += 15;
         var streamI = Stream.iterate(seed, i -> i + 1)
                 .limit(15).map(i -> "I" + i);
-        streamI.forEach(System.out::println);
+//        streamI.forEach(System.out::println);
 
         System.out.println("-----------------------------------------------");
 
@@ -27,16 +27,27 @@ public class Main {
         String[] oLabels = new String[15];
         Arrays.setAll(oLabels, i -> "N" + (nSeed + i));
         var streamN = Arrays.stream(oLabels);
-        streamN.forEach(System.out::println);
+//        streamN.forEach(System.out::println);
 
         System.out.println("-----------------------------------------------");
+
+        seed += 15;
+        int finalSeed = seed;
+        var streamG = Stream.generate(Main::getCounter)
+                .limit(15)
+                .map(i -> "O" + (finalSeed + i));
 
         seed += 15;
         int rSeed = seed;
         var streamO = Stream.generate(Main::getCounter)
                 .limit(15)
                 .map(i -> "O" + (rSeed + i));
-        streamO.forEach(System.out::println);
+//        streamO.forEach(System.out::println);
+
+        var streamBI = Stream.concat(streamB, streamI);
+        var streamNG = Stream.concat(streamN, streamG);
+        var streamBING = Stream.concat(streamNG, streamBI);
+        Stream.concat(streamBING, streamO).forEach(System.out::println);
     }
 
     public static int getCounter(){
