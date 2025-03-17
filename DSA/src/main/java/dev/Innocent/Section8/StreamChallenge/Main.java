09d22c4a0aeec2a0f0405d6b60dbf2ad94973b49
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-03-17 19:35:44 +0800

    Stream

--- DSA/src/main/java/dev/Innocent/Section8/StreamChallenge/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/StreamChallenge/Main.java
@@ -1,5 +1,6 @@
 package dev.Innocent.Section8.StreamChallenge;
 
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class Main {
@@ -9,5 +10,21 @@ public class Main {
         var streamB = Stream.iterate(seed, i -> i <= 15, i -> i + 1)
                 .map(i -> "B" + i);
         streamB.forEach(System.out::println);
+
+        System.out.println("-----------------------------------------------");
+
+        seed += 15;
+        var streamI = Stream.iterate(seed, i -> i + 1)
+                .limit(15).map(i -> "I" + i);
+        streamI.forEach(System.out::println);
+
+        System.out.println("-----------------------------------------------");
+
+        seed += 15;
+        int nSeed = seed;
+        String[] oLabels = new String[15];
+        Arrays.setAll(oLabels, i -> "N" + (nSeed + i));
+        var streamN = Arrays.stream(oLabels);
+        streamN.forEach(System.out::println);
     }
 }
