deb3897a555fd49afd8cc44a895d5e7c31126756
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-03-18 09:42:17 +0800

    Stream

---
+++ DSA/src/main/java/dev/Innocent/Section8/StreamIntermidiate/Seat.java
@@ -0,0 +1,13 @@
+package dev.Innocent.Section8.StreamIntermidiate;
+
+public record Seat(char rowMaker, int seatNumber, double price) {
+    public Seat(char rowMaker, int seatNumber) {
+        this(rowMaker, seatNumber,
+                rowMaker > 'C' && (seatNumber <= 2 || seatNumber >= 9) ? 50 : 75);
+    }
+
+    @Override
+    public String toString() {
+        return "%c%03d %.0f".formatted(rowMaker, seatNumber, price);
+    }
+}
