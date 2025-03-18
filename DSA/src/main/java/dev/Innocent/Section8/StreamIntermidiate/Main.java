d4e4d8db9e03f21761e4baebce55a5c2b92320b6
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-03-18 10:33:43 +0800

    Stream

--- DSA/src/main/java/dev/Innocent/Section8/StreamIntermidiate/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/StreamIntermidiate/Main.java
@@ -1,5 +1,6 @@
 package dev.Innocent.Section8.StreamIntermidiate;
 
+import java.util.Comparator;
 import java.util.Random;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -29,7 +30,14 @@ public class Main {
         int seatsInRow = 10;
         var stream = Stream.iterate(0, i -> i < maxSeats, i -> i + 1)
                 .map(i -> new Seat((char) ('A' + i / seatsInRow),
-                        i % seatsInRow + 1));
+                        i % seatsInRow + 1))
+                .skip(5)
+                .limit(10)
+                .peek(s -> System.out.println("---> " + s))
+                .sorted(Comparator.comparing(Seat::price)
+                                .thenComparing(Seat::toString));
+//                        .mapToDouble(Seat::price).boxed()
+//                                .map("%.2f"::formatted);
         stream.forEach(System.out::println);
     }
 }
