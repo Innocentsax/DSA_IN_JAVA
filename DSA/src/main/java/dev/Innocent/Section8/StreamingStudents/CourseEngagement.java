2317cd06a1ea7401c048078e34a443671e6933c8
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-03-19 22:30:33 +0800

    Stream

---
+++ DSA/src/main/java/dev/Innocent/Section8/StreamingStudents/CourseEngagement.java
@@ -0,0 +1,52 @@
+package dev.Innocent.Section8.StreamingStudents;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class CourseEngagement {
+    private final Course course;
+    private final LocalDate enrollmentDate;
+    private String engagementType;
+    private int lastLecture;
+    private LocalDate lastActivityDate;
+
+    public CourseEngagement(Course course, LocalDate enrollmentDate, String engagementType) {
+        this.course = course;
+        this.enrollmentDate = this.lastActivityDate = enrollmentDate;
+        this.engagementType = engagementType;
+    }
+
+    public String getCourseCode() {
+        return course.courseCode();
+    }
+
+    public int getEnrollmentYear() {
+        return enrollmentDate.getYear();
+    }
+
+    public String getEngagementType() {
+        return engagementType;
+    }
+
+    public int getLastLecture() {
+        return lastLecture;
+    }
+
+    public int getLastActivityYear() {
+        return lastActivityDate.getYear();
+    }
+
+    public String getLastActivityMonth(){
+        return "%tb".formatted(lastActivityDate);
+    }
+
+    public double getPercentComplete(){
+        return lastLecture * 100.0 / course.lectureCount();
+    }
+
+    public int getMonthsSinceActive(){
+        LocalDate now = LocalDate.now();
+        var months = Period.between(lastActivityDate, now).toTotalMonths();
+        return (int) months;
+    }
+}
