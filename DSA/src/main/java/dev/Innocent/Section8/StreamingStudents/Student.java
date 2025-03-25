90fe01ce3cfc59070ed31e634f2472bc90252e34
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-03-25 21:35:02 +0800

    Stream

--- DSA/src/main/java/dev/Innocent/Section8/StreamingStudents/Student.java
+++ DSA/src/main/java/dev/Innocent/Section8/StreamingStudents/Student.java
@@ -1,5 +1,13 @@
 package dev.Innocent.Section8.StreamingStudents;
 
+import java.util.Random;
+
 public class Student {
     private static long lastStudentId = 1;
+    private final static Random random = new Random();
+    private final long studentId;
+    private final String countryCode;
+    private final int yearEnrolled;
+    private final int ageEnrolled;
+    private final String gender;
 }
