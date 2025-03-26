f52b315db50d76e5068c85aad74484eac395d71b
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-03-26 20:37:50 +0800

    Stream

--- DSA/src/main/java/dev/Innocent/Section8/StreamingStudents/Student.java
+++ DSA/src/main/java/dev/Innocent/Section8/StreamingStudents/Student.java
@@ -1,5 +1,8 @@
 package dev.Innocent.Section8.StreamingStudents;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class Student {
@@ -10,4 +13,16 @@ public class Student {
     private final int yearEnrolled;
     private final int ageEnrolled;
     private final String gender;
+    private final boolean programmingExperience;
+    private final Map<String, CourseEngagement> engagementMap = new HashMap<>();
+
+    public Student(String countryCode, int yearEnrolled, int ageEnrolled,
+                   String gender, boolean programmingExperience, Course... courses) {
+        studentId = lastStudentId++;
+        this.countryCode = countryCode;
+        this.yearEnrolled = yearEnrolled;
+        this.ageEnrolled = ageEnrolled;
+        this.gender = gender;
+        this.programmingExperience = programmingExperience;
+    }
 }
