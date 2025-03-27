f85cb0b46599689c921a34c2ee1cfbaff97a1097
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-03-27 21:48:27 +0800

    Stream

--- DSA/src/main/java/dev/Innocent/Section8/StreamingStudents/CourseEngagement.java
+++ DSA/src/main/java/dev/Innocent/Section8/StreamingStudents/CourseEngagement.java
@@ -4,13 +4,15 @@ import java.time.LocalDate;
 import java.time.Period;
 
 public class CourseEngagement {
+
     private final Course course;
     private final LocalDate enrollmentDate;
     private String engagementType;
     private int lastLecture;
     private LocalDate lastActivityDate;
 
-    public CourseEngagement(Course course, LocalDate enrollmentDate, String engagementType) {
+    public CourseEngagement(Course course, LocalDate enrollmentDate,
+                            String engagementType) {
         this.course = course;
         this.enrollmentDate = this.lastActivityDate = enrollmentDate;
         this.engagementType = engagementType;
@@ -36,21 +38,23 @@ public class CourseEngagement {
         return lastActivityDate.getYear();
     }
 
-    public String getLastActivityMonth(){
+    public String getLastActivityMonth() {
         return "%tb".formatted(lastActivityDate);
     }
 
-    public double getPercentComplete(){
+    public double getPercentComplete() {
         return lastLecture * 100.0 / course.lectureCount();
     }
 
-    public int getMonthsSinceActive(){
+    public int getMonthsSinceActive() {
+
         LocalDate now = LocalDate.now();
         var months = Period.between(lastActivityDate, now).toTotalMonths();
         return (int) months;
     }
 
-    void watchLecture(int lectureNumber, LocalDate currentDate){
+    void watchLecture(int lectureNumber, LocalDate currentDate) {
+
         lastLecture = Math.max(lectureNumber, lastLecture);
         lastActivityDate = currentDate;
         engagementType = "Lecture " + lastLecture;
@@ -59,7 +63,8 @@ public class CourseEngagement {
     @Override
     public String toString() {
         return "%s: %s %d %s [%d]".formatted(course.courseCode(),
-                getLastActivityMonth(), getLastActivityYear(),
-                engagementType, getMonthsSinceActive());
+                getLastActivityMonth(), getLastActivityYear(), engagementType,
+                getMonthsSinceActive());
     }
 }
+
