78fdb817866741a5147356dcd84e1ec583bc7ac6
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-03-27 21:48:24 +0800

    Stream

--- DSA/src/main/java/dev/Innocent/Section8/StreamingStudents/Course.java
+++ DSA/src/main/java/dev/Innocent/Section8/StreamingStudents/Course.java
@@ -3,13 +3,13 @@ package dev.Innocent.Section8.StreamingStudents;
 public record Course(String courseCode, String title, int lectureCount) {
 
     public Course {
-        if(lectureCount <= 0){
+        if (lectureCount <= 0) {
             lectureCount = 1;
         }
     }
 
     public Course(String courseCode, String title) {
-        this(courseCode, title, 0);
+        this(courseCode, title, 40);
     }
 
     @Override
