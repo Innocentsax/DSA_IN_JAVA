0bbf7eb0a00da6d31b34d959f47eb69702f7a9b2
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-03-28 13:55:48 +0800

    Stream

--- DSA/src/main/java/dev/Innocent/Section8/StreamingStudents/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/StreamingStudents/Main.java
@@ -1,5 +1,8 @@
 package dev.Innocent.Section8.StreamingStudents;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class Main {
@@ -16,9 +19,39 @@ public class Main {
 //        tim.watchLecture("PYMC", 7, 7, 2020);
 //        System.out.println(tim);
 
-        Stream.generate(() -> Student.getRandomStudent(jmc, pymc))
-                .limit(10)
-                .forEach(System.out::println);
+//        Stream.generate(() -> Student.getRandomStudent(jmc, pymc))
+//                .limit(10)
+//                .forEach(System.out::println);
+//        var maleStudent = Stream.generate(() -> Student.getRandomStudent(jmc, pymc))
+//                .limit(1000);
+//        maleStudent = maleStudent.filter(s -> s.getGender().equals("M"));
 
+        Student[] students = new Student[1000];
+        Arrays.setAll(students, (i) -> Student.getRandomStudent(jmc, pymc));
+
+        var maleStudent = Arrays.stream(students)
+                        .filter(s -> s.getGender().equals("M"));
+
+        System.out.println("# of male students " + maleStudent.count());
+
+        for(String gender : List.of("M", "F", "U")){
+            var myStudent = Arrays.stream(students)
+                    .filter(s -> s.getGender().equals(gender));
+            System.out.println("# of " + gender + " students " + myStudent.count());
+        }
+
+        List<Predicate<Student>> list = List.of(
+                (s) -> s.getAge() < 30,
+                (Student s) -> s.getAge() >= 30 && s.getAge() < 60
+        );
+
+        long total = 0;
+        for(int i = 0; i < list.size(); i++){
+            var myStudent = Arrays.stream(students).filter(list.get(i));
+            long cnt = myStudent.count();
+            total += cnt;
+            System.out.printf("# of students (%s) = %d%n", i == 0 ? "< 30" : ">= 30 & 60", cnt);
+        }
+        System.out.println("# of students >= 60 = " + (students.length - total));
     }
 }
