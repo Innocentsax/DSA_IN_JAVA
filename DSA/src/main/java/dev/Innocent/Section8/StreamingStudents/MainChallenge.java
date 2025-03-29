cf76a8e5530f2fdc7c53253ee4021eceede44474
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-03-30 00:08:57 +0800

    Stream

---
+++ DSA/src/main/java/dev/Innocent/Section8/StreamingStudents/MainChallenge.java
@@ -0,0 +1,10 @@
+package dev.Innocent.Section8.StreamingStudents;
+
+public class MainChallenge {
+    public static void main(String[] args) {
+        Course pymc= new Course("PYMC", "Python Masterclass");
+        Course jmc= new Course("JMC", "Java Masterclass");
+
+
+    }
+}
