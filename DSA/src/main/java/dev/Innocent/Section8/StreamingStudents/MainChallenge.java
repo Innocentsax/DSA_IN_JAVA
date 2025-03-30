390776a7e8a57d5ae9fa47f4f4cba51a53d41b9a
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-03-30 16:53:02 +0800

    Stream

--- DSA/src/main/java/dev/Innocent/Section8/StreamingStudents/MainChallenge.java
+++ DSA/src/main/java/dev/Innocent/Section8/StreamingStudents/MainChallenge.java
@@ -1,10 +1,23 @@
 package dev.Innocent.Section8.StreamingStudents;
 
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class MainChallenge {
     public static void main(String[] args) {
-        Course pymc= new Course("PYMC", "Python Masterclass");
-        Course jmc= new Course("JMC", "Java Masterclass");
+        Course pymc= new Course("PYMC", "Python Masterclass", 50);
+        Course jmc= new Course("JMC", "Java Masterclass", 100);
+        Course jgames = new Course("JGAME", "Creating games in Java");
 
+        List<Student> student = Stream
+                .iterate(1, s -> s <= 5000, s -> s + 1)
+                .map(s -> Student.getRandomStudent(jmc, pymc))
+                .toList();
 
+        List<Student> student2 = IntStream
+                .rangeClosed(1, 5000)
+                .mapToObj(s -> Student.getRandomStudent(jmc, pymc))
+                .toList();
     }
 }
