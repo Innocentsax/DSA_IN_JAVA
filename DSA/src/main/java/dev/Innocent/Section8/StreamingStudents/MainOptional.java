9f346cc38004c0236c3a6fb196bca807b360b94f
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-03-31 15:35:24 +0800

    Stream

---
+++ DSA/src/main/java/dev/Innocent/Section8/StreamingStudents/MainOptional.java
@@ -0,0 +1,41 @@
+package dev.Innocent.Section8.StreamingStudents;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class MainOptional {
+    public static void main(String[] args) {
+        Course pymc= new Course("PYMC", "Python Masterclass");
+        Course jmc= new Course("JMC", "Java Masterclass");
+
+        List<Student> students =
+                Stream.generate(() -> Student.getRandomStudent(jmc, pymc))
+                        .limit(1000)
+                        .collect(Collectors.toList());
+
+        Optional<Student> o1 = getStudent(new ArrayList<>(), "first");
+        System.out.println("Empty = " + o1.isEmpty() + ", Present = " + o1.isPresent());
+        System.out.println(o1);
+
+        students.add(0, null);
+
+        Optional<Student> o2 = getStudent(students, "first");
+        System.out.println("Empty = " + o2.isEmpty() + ", Present = " + o2.isPresent());
+        System.out.println(o2);
+    }
+
+    private static Optional<Student> getStudent(List<Student> list, String type){
+        if(list == null || list.size() == 0){
+            return Optional.empty();
+        } else if (type.equals("first")) {
+            return Optional.ofNullable(list.get(0));
+        } else if (type.equals("last")) {
+            return Optional.ofNullable(list.get(list.size() - 1));
+        }
+        return Optional.ofNullable(list.get(new Random().nextInt(list.size())));
+    }
+}
