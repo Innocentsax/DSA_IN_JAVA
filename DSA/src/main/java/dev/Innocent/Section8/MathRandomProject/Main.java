ae9011d18c5fd4628c4c0419737bafd6d49e153b
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-04-03 22:54:38 +0800

    Stream

--- DSA/src/main/java/dev/Innocent/Section8/MathRandomProject/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/MathRandomProject/Main.java
@@ -2,6 +2,32 @@ package dev.Innocent.Section8.MathRandomProject;
 
 public class Main {
     public static void main(String[] args) {
+//        int maxMinusFive = Integer.MAX_VALUE - 5;
+//        for(int j = 0, id = maxMinusFive; j < 10; id++, j++){
+//            System.out.printf("Assigning id %,d%n", id);
+//        }
 
+        System.out.println(Math.abs(-50));
+        System.out.println(Math.abs(Integer.MIN_VALUE));
+//        System.out.println(Math.absExact(Integer.MIN_VALUE));
+        System.out.println(Math.abs((long) Integer.MIN_VALUE));
+
+        System.out.println("Max = " + Math.max(10, -10));
+        System.out.println("Min = " + Math.min(10.0000002, 10.001f));
+
+        System.out.println("Round Down = " + Math.round(10.2));
+        System.out.println("Round Up = " + Math.round(10.8));
+        System.out.println("Round ? = " + Math.round(10.5));
+
+        System.out.println("Floor = " + Math.floor(10.8));
+        System.out.println("Ceil = " + Math.ceil(10.2));
+
+        System.out.println("Square root of 100 = " + Math.sqrt(100));
+        System.out.println("2 to the third power (2*2*2) = " + Math.pow(2, 3));
+        System.out.println("10 to the fifth power (10*10*10*10*10) = " + Math.pow(10, 5));
+
+        for(int i = 0; i < 10; i++){
+            System.out.println(Math.random());
+        }
     }
 }
