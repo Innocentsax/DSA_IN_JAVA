2500a9f63ecf9c23a4cdb29be8f29fbb494aa4e8
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-04-05 13:35:38 +0800

    Java Core Math

---
+++ DSA/src/main/java/dev/Innocent/Section8/MathRandomProject/RandomChallenge.java
@@ -0,0 +1,13 @@
+package dev.Innocent.Section8.MathRandomProject;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class RandomChallenge {
+    private static final Random random = new Random();
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+    }
+}
