0acba585665fee0e1ebb44b28b6866ca8c4c4e3c
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-04-05 17:37:00 +0800

    Java Core Math

--- DSA/src/main/java/dev/Innocent/Section8/MathRandomProject/RandomChallenge.java
+++ DSA/src/main/java/dev/Innocent/Section8/MathRandomProject/RandomChallenge.java
@@ -1,5 +1,7 @@
 package dev.Innocent.Section8.MathRandomProject;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -8,6 +10,17 @@ public class RandomChallenge {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        List<Integer> currentDice = new ArrayList<>();
+    }
 
+    private static void rollDice(List<Integer> currentDice){
+        int randomCount = 5 - currentDice.size();
+        var newDice = random.ints(randomCount, 1, 7)
+                .sorted()
+                .boxed()
+                .toList();
+        currentDice.addAll(newDice);
+
+        System.out.println("You're dice are: " + currentDice);
     }
 }
