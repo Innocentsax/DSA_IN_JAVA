88539e35ded93b6525bbb28c91c34950dfe8f513
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-04-08 21:24:21 +0800

    Dice Game

--- DSA/src/main/java/dev/Innocent/Section8/MathRandomProject/games/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/MathRandomProject/games/Main.java
@@ -1,6 +1,8 @@
 package dev.Innocent.Section8.MathRandomProject.games;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 import java.util.Arrays;
 
 public class Main {
@@ -34,7 +36,7 @@ public class Main {
                     bd, bd.unscaledValue(), bd.scale(), bd.precision());
         }
 
-        double[] doubles = {15.456, 8, 10000.000001, .123};
+        double[] doubles = {15.456, 8, 10000.000001, .125};
         Arrays.setAll(bds, i -> BigDecimal.valueOf(doubles[i]));
         System.out.println("---------------------------");
         System.out.printf("%-14s %-15s %-8s %s%n", "Value", "Unscaled Value", "Scale",
@@ -42,18 +44,34 @@ public class Main {
         for (var bd : bds) {
             System.out.printf("%-15s %-15d %-8d %d %n",
                     bd, bd.unscaledValue(), bd.scale(), bd.precision());
+            bd = bd.setScale(2, RoundingMode.HALF_UP);
+            System.out.printf("%-15s %-15d %-8d %d %n",
+                    bd, bd.unscaledValue(), bd.scale(), bd.precision());
         }
 
-//        BigDecimal test1 = new BigDecimal("1.1111122222333334444455555");
-//        BigDecimal test2 = BigDecimal.valueOf(1.1111122222333334444455555);
-//
-//        System.out.println("---------------------------");
-//        System.out.printf("%-30s %-30s %-8s %s%n", "Value", "Unscaled Value", "Scale",
-//                "Precision");
-//        System.out.printf("%-30s %-30d %-8d %d %n",
-//                test1, test1.unscaledValue(), test1.scale(), test1.precision());
-//        System.out.printf("%-30s %-30d %-8d %d %n",
-//                test2, test2.unscaledValue(), test2.scale(), test2.precision());
+        BigDecimal policyPayout = new BigDecimal("100000000.00");
+        System.out.printf("%-15s %-15d %-8d %d %n",
+                policyPayout, policyPayout.unscaledValue(), policyPayout.scale(),
+                policyPayout.precision());
 
+        BigDecimal percent = BigDecimal.ONE.divide(BigDecimal.valueOf(beneficiaries),
+                new MathContext(60, RoundingMode.UP));
+        System.out.println(percent);
+
+        BigDecimal checkAmount = policyPayout.multiply(percent);
+        System.out.printf("%.2f%n", checkAmount);
+        checkAmount = checkAmount.setScale(2, RoundingMode.HALF_UP);
+        System.out.printf("%-15s %-15d %-8d %d %n",
+                checkAmount, checkAmount.unscaledValue(), checkAmount.scale(),
+                checkAmount.precision());
+
+        BigDecimal totalChecksAmount = checkAmount.multiply(
+                BigDecimal.valueOf(beneficiaries));
+        System.out.printf("Combined: %.2f%n", totalChecksAmount);
+        System.out.println("Remaining = " + policyPayout.subtract(totalChecksAmount));
+
+        System.out.printf("%-15s %-15d %-8d %d %n",
+                totalChecksAmount, totalChecksAmount.unscaledValue(),
+                totalChecksAmount.scale(), totalChecksAmount.precision());
     }
 }
