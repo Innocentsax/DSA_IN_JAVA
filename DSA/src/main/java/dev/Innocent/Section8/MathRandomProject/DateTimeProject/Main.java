ca2b799c79c3cfebf3317ebf5253698befff65d7
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-04-10 21:21:39 +0800

    Date and Time

--- DSA/src/main/java/dev/Innocent/Section8/MathRandomProject/DateTimeProject/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/MathRandomProject/DateTimeProject/Main.java
@@ -3,35 +3,61 @@ package dev.Innocent.Section8.MathRandomProject.DateTimeProject;
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
 
 public class Main {
     public static void main(String[] args) {
+
         LocalDate today = LocalDate.now();
         System.out.println(today);
 
-        LocalDate five5 = LocalDate.of(2025, 4, 5);
-        System.out.println(five5);
+        LocalDate Five5 = LocalDate.of(2022, 5, 5);
+        System.out.println(Five5);
 
-        LocalDate May5th = LocalDate.of(2025, Month.APRIL, 5);
+        LocalDate May5th = LocalDate.of(2022, Month.MAY, 5);
         System.out.println(May5th);
 
-        LocalDate dat125 = LocalDate.ofYearDay(2025, 125);
-        System.out.println(dat125);
+        LocalDate Day125 = LocalDate.ofYearDay(2022, 125);
+        System.out.println(Day125);
 
-        LocalDate april = LocalDate.parse("2025-04-08");
-        System.out.println(april);
+        LocalDate May5 = LocalDate.parse("2022-05-05");
+        System.out.println(May5);
 
-        System.out.println(april.getYear());
-        System.out.println(april.getMonth());
+        System.out.println(May5.getYear());
+        System.out.println(May5.getMonth());
 
-        System.out.println(april.getMonthValue());
-        System.out.println(april.getDayOfMonth());
-        System.out.println(april.getDayOfWeek());
-        System.out.println(april.getDayOfYear());
+        System.out.println(May5.getMonthValue());
 
-        System.out.println(april.get(ChronoField.YEAR));
-        System.out.println(april.get(ChronoField.MONTH_OF_YEAR));
-        System.out.println(april.get(ChronoField.DAY_OF_MONTH));
-        System.out.println(april.get(ChronoField.DAY_OF_YEAR));
+        System.out.println(May5.getDayOfMonth());
+        System.out.println(May5.getDayOfWeek());
+        System.out.println(May5.getDayOfYear());
+
+        System.out.println(May5.get(ChronoField.YEAR));
+        System.out.println(May5.get(ChronoField.MONTH_OF_YEAR));
+        System.out.println(May5.get(ChronoField.DAY_OF_MONTH));
+        System.out.println(May5.get(ChronoField.DAY_OF_YEAR));
+
+        System.out.println(May5.withYear(2000));
+        System.out.println(May5.withMonth(3));
+        System.out.println(May5.withDayOfMonth(4));
+        System.out.println(May5.withDayOfYear(126));
+        System.out.println(May5);
+        System.out.println(May5.with(ChronoField.DAY_OF_YEAR, 126));
+        System.out.println(May5.plusYears(1));
+        System.out.println(May5.plusMonths(12));
+        System.out.println(May5.plusDays(365));
+        System.out.println(May5.plusWeeks(52));
+        System.out.println(May5.plus(365, ChronoUnit.DAYS));
+
+        System.out.println("May5 > today? " + May5.isAfter(today));
+        System.out.println("today > May5? " + May5.isBefore(today));
+        System.out.println("May5 > today? " + May5.compareTo(today));
+        System.out.println("today > May5? " + today.compareTo(May5));
+
+        System.out.println("today = now ? " + today.compareTo(LocalDate.now()));
+        System.out.println("today = now ? " + today.equals(LocalDate.now()));
+
+        System.out.println(today.isLeapYear());
+        System.out.println(May5.minusYears(2).isLeapYear());
     }
 }
