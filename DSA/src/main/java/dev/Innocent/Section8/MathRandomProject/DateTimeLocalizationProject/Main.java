95b4e7042def73e5fc29762319ef1fce83f69814
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-04-15 06:55:32 +0800

    Localization of Meeting in dif timezones

--- DSA/src/main/java/dev/Innocent/Section8/MathRandomProject/DateTimeLocalizationProject/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/MathRandomProject/DateTimeLocalizationProject/Main.java
@@ -1,6 +1,21 @@
 package dev.Innocent.Section8.MathRandomProject.DateTimeLocalizationProject;
 
+import java.time.ZoneId;
+import java.util.Locale;
+
 public class Main {
+
+    private record Employee(String name, Locale locale, ZoneId zone){
+
+        public Employee(String name, String locale, String zone) {
+            this(name, Locale.forLanguageTag(locale), ZoneId.of(zone));
+        }
+
+        public Employee(String name, Locale locale, String zone) {
+            this(name, locale, ZoneId.of(zone));
+        }
+    }
+
     public static void main(String[] args) {
 
     }
