db9e98e97ce1637177f08f481c6ca3c90d096ac7
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-04-20 17:23:01 +0800

    Localization of Meeting in dif timezones

--- DSA/src/main/java/dev/Innocent/Section8/RegularExpression/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/RegularExpression/Main.java
@@ -10,6 +10,20 @@ public class Main {
 
         String helloWorld3 = Main.format("%s %s", "Hello", "World");
         System.out.println("Using Main.format: " + helloWorld3);
+
+        String testString = "Anyone can Learn abc's, 123's, and any regular expression";
+        String replacement = "(-)";
+
+        String[] patterns = {
+                "[a-zA-Z]*$",
+                "^[a-zA-Z]{3}",
+                "[aA]ny\\b"
+        };
+
+        for (String pattern : patterns) {
+            String output = testString.replaceFirst(pattern, replacement);
+            System.out.println("Pattern: " + pattern + " => " + output);
+        }
     }
 
     private static String format(String regexp, String... args) {
