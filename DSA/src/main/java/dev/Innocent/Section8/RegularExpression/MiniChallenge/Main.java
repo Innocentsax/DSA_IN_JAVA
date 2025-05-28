720e944551a20758cbb77f8fafc1d2158af8981f
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-05-28 22:34:28 +0800

    Localization of Meeting in dif timezones

--- DSA/src/main/java/dev/Innocent/Section8/RegularExpression/MiniChallenge/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/RegularExpression/MiniChallenge/Main.java
@@ -1,9 +1,19 @@
 package dev.Innocent.Section8.RegularExpression.MiniChallenge;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         String sentence = "Hello, World";
         boolean matches = sentence.matches("Hello, World");
         System.out.println(matches);
+
+        String challenge2 = "[A-Z].*\\.";
+        for(String s : List.of("The bike is red.", "I am a new student.",
+                "hello world.", "How are you")){
+            boolean matched = s.matches(challenge2);
+            System.out.println(matched + ": " + s);
+        }
+
     }
 }
