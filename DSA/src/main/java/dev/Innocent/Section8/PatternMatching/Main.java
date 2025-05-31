6d084d0faceaf7ae7f60cb469d5a107eba804fdb
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-05-31 18:09:59 +0800

    Localization of Meeting in dif timezones

--- DSA/src/main/java/dev/Innocent/Section8/PatternMatching/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/PatternMatching/Main.java
@@ -4,11 +4,11 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        String sentence = "I like motorcycles.";
+        String sentence = "I like B.M.W motorcycles.";
         boolean matched = Pattern.matches("[A-Z].*[.]", sentence);
         System.out.println(matched + ": " + sentence);
 
-        Pattern firstPattern  = Pattern.compile("[A-Z].*[.]");
+        Pattern firstPattern  = Pattern.compile("[A-Z].*?[.]");
         var matcher = firstPattern.matcher(sentence);
         System.out.println(matcher.matches() + ": " +sentence);
         System.out.println("sentence.length: " + sentence.length());
@@ -16,5 +16,11 @@ public class Main {
 
         System.out.println(matcher.lookingAt() + ": " + sentence);
         System.out.println("Matched Ending Index: " + matcher.end());
+        System.out.println("Matched Ending Index: " + sentence.substring(0, matcher.end()));
+
+        matcher.reset();
+        System.out.println(matcher.lookingAt() + ": " + sentence);
+        System.out.println("Matched Ending Index: " + matcher.end());
+        System.out.println("Matched Ending Index: " + sentence.substring(matcher.start(), matcher.end()));
     }
 }
