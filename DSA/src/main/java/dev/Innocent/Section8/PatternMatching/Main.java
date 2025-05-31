7c4819733ca504c0cc67bfddbc257c875ee4ab9a
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-05-31 17:49:14 +0800

    Localization of Meeting in dif timezones

--- DSA/src/main/java/dev/Innocent/Section8/PatternMatching/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/PatternMatching/Main.java
@@ -11,5 +11,10 @@ public class Main {
         Pattern firstPattern  = Pattern.compile("[A-Z].*[.]");
         var matcher = firstPattern.matcher(sentence);
         System.out.println(matcher.matches() + ": " +sentence);
+        System.out.println("sentence.length: " + sentence.length());
+        System.out.println("Matched Ending Index: " + matcher.end());
+
+        System.out.println(matcher.lookingAt() + ": " + sentence);
+        System.out.println("Matched Ending Index: " + matcher.end());
     }
 }
