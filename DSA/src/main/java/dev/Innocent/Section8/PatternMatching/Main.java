7d78db1b64e874ae169736cd8b561f8d583f1427
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-06-01 19:04:52 +0800

    Localization of Meeting in dif timezones

--- DSA/src/main/java/dev/Innocent/Section8/PatternMatching/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/PatternMatching/Main.java
@@ -1,26 +1,49 @@
 package dev.Innocent.Section8.PatternMatching;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        String sentence = "I like B.M.W motorcycles.";
+        String sentence = "I like B.M.W. motorcycles.";
         boolean matched = Pattern.matches("[A-Z].*[.]", sentence);
         System.out.println(matched + ": " + sentence);
 
-        Pattern firstPattern  = Pattern.compile("[A-Z].*?[.]");
+        Pattern firstPattern = Pattern.compile("[A-Z].*?[.]");
         var matcher = firstPattern.matcher(sentence);
-        System.out.println(matcher.matches() + ": " +sentence);
+        System.out.println(matcher.matches() + ": " + sentence);
         System.out.println("sentence.length: " + sentence.length());
         System.out.println("Matched Ending Index: " + matcher.end());
 
         System.out.println(matcher.lookingAt() + ": " + sentence);
         System.out.println("Matched Ending Index: " + matcher.end());
-        System.out.println("Matched Ending Index: " + sentence.substring(0, matcher.end()));
+        System.out.println("Matched on : " +
+                sentence.substring(0, matcher.end()));
 
         matcher.reset();
-        System.out.println(matcher.lookingAt() + ": " + sentence);
+        System.out.println(matcher.find() + ": " + sentence);
         System.out.println("Matched Ending Index: " + matcher.end());
-        System.out.println("Matched Ending Index: " + sentence.substring(matcher.start(), matcher.end()));
+        System.out.println("Matched on : " +
+                sentence.substring(matcher.start(), matcher.end()));
+        System.out.println("Matched on : " + matcher.group());
+
+        String htmlSnippet = """
+                <H1>My Heading</H1>
+                <h2>Sub-heading</h2>
+                <p>This is a paragraph about something.</p>
+                <p>This is another paragraph about something else.</p>
+                <h3>Summary</h3>
+                """;
+
+        Pattern htmlPattern = Pattern.compile("<[hH](?<level>\\d)>(.*)</[hH]\\d>");
+        Matcher htmlMatcher = htmlPattern.matcher(htmlSnippet);
+
+        while (htmlMatcher.find()) {
+//            System.out.println("group: " + htmlMatcher.group());
+//            System.out.println("group0: " + htmlMatcher.group(0));
+            System.out.println(htmlMatcher.group("level") + " " +
+                    htmlMatcher.group(2));
+            System.out.println("index = " + htmlMatcher.start("level"));
+        }
     }
 }
