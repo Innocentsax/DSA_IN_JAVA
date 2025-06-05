608aa0c16ecc722e9933b9a71af1d702edde63d6
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-06-05 19:46:22 +0800

    Localization of Meeting in dif timezones

--- DSA/src/main/java/dev/Innocent/Section8/FileOperation/FileException/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/FileOperation/FileException/Main.java
@@ -1,6 +1,8 @@
 package dev.Innocent.Section8.FileOperation.FileException;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,13 +11,12 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        String filename = "testing.csv";
-
-        testFile(filename);
+        System.out.println("Current working Directory CWD: " + new File("").getAbsolutePath());
+        String filename = "files/testing.csv";
         File file = new File(filename);
+
         if(!file.exists()){
             System.out.println("I can't run unless this file exists");
-            System.out.println("Quitting Application, go figure it out");
             return;
         }
         System.out.println("I'm good to go");
@@ -25,15 +26,39 @@ public class Main {
 
     private static void testFile(String filename){
         Path path = Paths.get(filename);
-
+        FileReader reader = null;
         // The IOException is a special kind of exception, called a Checked Exception
         try {
-            List<String> lines = Files.readAllLines(path);
+//            List<String> lines = Files.readAllLines(path);
+            reader = new FileReader(filename);
         } catch (IOException e) {
             throw new RuntimeException(e);
         } finally {
+            if(reader != null){
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
             System.out.println("Maybe I'd log something either way...");
         }
         System.out.println("File exists and able to use as a resource");
+    }
+
+    private static void testFile2(String filename){
+        try (FileReader reader = new FileReader(filename)) {
+        } catch (FileNotFoundException e) {
+            System.out.println("File '" + filename + "' does not exist");
+            throw new RuntimeException(e);
+        } catch (NullPointerException | IllegalArgumentException badData) {
+            System.out.println("User has added bad data "  + badData.getMessage());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+            System.out.println("Something unrelated and unexpected happened");
+        }finally {
+            System.out.println("Maybe I'd log something either way...");
+        }
     }
 }
