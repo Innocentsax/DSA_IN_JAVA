0e177416f889124418078f9c794e77ee5d1f8bef
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-06-06 22:59:03 +0800

    Localization of Meeting in dif timezones

--- DSA/src/main/java/dev/Innocent/Section8/FileOperation/FileAndPath/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/FileOperation/FileAndPath/Main.java
@@ -1,10 +1,11 @@
 package dev.Innocent.Section8.FileOperation.FileAndPath;
 
 import java.io.File;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-
+        useFile("testfile.txt");
     }
 
     private static void useFile(String fileName){
@@ -17,6 +18,18 @@ public class Main {
         if(fileExists){
             System.out.println("Deleting File: " + fileName);
             fileExists = !file.delete();
+        }
+
+        if(!fileExists){
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                System.out.println("Something went wrong");
+            }
+            System.out.println("Create File: " + fileName);
+            if(file.canWrite()){
+                System.out.println("Would write to file here");
+            }
         }
     }
 }
