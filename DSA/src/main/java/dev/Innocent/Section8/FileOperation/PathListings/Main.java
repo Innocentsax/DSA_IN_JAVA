c9f370517b33ce7d42b565b795e48b0807d8aa25
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-06-11 22:37:43 +0800

    File

--- DSA/src/main/java/dev/Innocent/Section8/FileOperation/PathListings/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/FileOperation/PathListings/Main.java
@@ -4,10 +4,27 @@ import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) {
-
+        Path path = Path.of("files/testing.txt");
+        printPathInfo(path);
     }
 
     private static void printPathInfo(Path path){
+        System.out.println("Path: " + path);
+        System.out.println("fileName = " + path.getFileName());
+        System.out.println("parent = " + path.getParent());
+        Path absolutePath = path.toAbsolutePath();
+        System.out.println("Absolute Path: = " + absolutePath);
+        System.out.println("Absolute Path Root: = " + absolutePath.getRoot());
+        System.out.println("Root = " + path.getRoot());
+        System.out.println("isAbsolute = " + path.isAbsolute());
+        System.out.println("--------------------------------------");
 
+        System.out.println(absolutePath.getRoot());
+        int i = 1;
+        var it = path.toAbsolutePath().iterator();
+        while(it.hasNext()){
+            System.out.println(".".repeat(i++) + " " + it.next());
+        }
+        System.out.println("-------------------------------------------");
     }
 }
