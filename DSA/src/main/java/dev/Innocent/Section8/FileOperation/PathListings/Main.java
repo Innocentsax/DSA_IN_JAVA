426a87a9127efb4ea26f3d2a0dab027811d711cf
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-06-11 22:00:13 +0800

    File

---
+++ DSA/src/main/java/dev/Innocent/Section8/FileOperation/PathListings/Main.java
@@ -0,0 +1,13 @@
+package dev.Innocent.Section8.FileOperation.PathListings;
+
+import java.nio.file.Path;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    private static void printPathInfo(Path path){
+
+    }
+}
