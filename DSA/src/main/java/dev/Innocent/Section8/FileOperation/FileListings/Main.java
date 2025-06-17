ecd7fc7aecccb835c333ebd8c6b7d7e6bd90e290
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-06-17 22:52:59 +0800

    File

--- DSA/src/main/java/dev/Innocent/Section8/FileOperation/FileListings/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/FileOperation/FileListings/Main.java
@@ -3,6 +3,8 @@ package dev.Innocent.Section8.FileOperation.FileListings;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.attribute.FileTime;
+import java.sql.SQLOutput;
 import java.util.stream.Stream;
 
 public class Main {
@@ -14,6 +16,17 @@ public class Main {
             paths.forEach(System.out::println);
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private static String listDir(Path path){
+        try {
+            boolean isDir = Files.isDirectory(path);
+            FileTime dateField = Files.getLastModifiedTime(path);
+            return "%s %-15s %s".formatted(dateField, (isDir ? "<DIR>" : ""), path);
+        } catch (IOException e){
+            System.out.println("Whoops! Something went wrong with " + path);
+            return path.toString();
         }
     }
 }
