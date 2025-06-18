2666b8f0fdbb456744de6a3700ae28d286977308
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-06-18 22:43:48 +0800

    File

--- DSA/src/main/java/dev/Innocent/Section8/FileOperation/FileListings/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/FileOperation/FileListings/Main.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.attribute.FileTime;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.stream.Stream;
 
 public class Main {
@@ -12,7 +14,9 @@ public class Main {
         System.out.println("cwd =  " + path.toAbsolutePath());
 
         try (Stream<Path> paths = Files.list(path)) {
-            paths.forEach(System.out::println);
+            paths
+                    .map(Main::listDir)
+                    .forEach(System.out::println);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -22,7 +26,9 @@ public class Main {
         try {
             boolean isDir = Files.isDirectory(path);
             FileTime dateField = Files.getLastModifiedTime(path);
-            return "%s %-15s %s".formatted(dateField, (isDir ? "<DIR>" : ""), path);
+            LocalDateTime modDT = LocalDateTime.ofInstant(dateField.toInstant(), ZoneId.systemDefault());
+            return "%tD %tT %-5s %12s %s".formatted(modDT, modDT, (isDir ? "<DIR>" : ""),
+                    (isDir ? "" : Files.size(path)),path);
         } catch (IOException e){
             System.out.println("Whoops! Something went wrong with " + path);
             return path.toString();
