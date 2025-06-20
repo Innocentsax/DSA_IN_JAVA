24068613d76d32ee13dee899c15e7bb9a9c77621
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-06-20 08:42:08 +0800

    File

--- DSA/src/main/java/dev/Innocent/Section8/FileOperation/FileListings/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/FileOperation/FileListings/Main.java
@@ -25,9 +25,38 @@ public class Main {
 
         try (Stream<Path> paths = Files.walk(path, 2)) {
             paths
+                    .filter(Files::isRegularFile)
                     .map(Main::listDir)
                     .forEach(System.out::println);
         } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("--------------------------------------------");
+
+        try (Stream<Path> paths = Files.find(path, Integer.MAX_VALUE,
+                (p, attr) -> attr.isRegularFile() && attr.size() > 300)) {
+            paths
+                    .map(Main::listDir)
+                    .forEach(System.out::println);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        path = path.resolve(".idea");
+        System.out.println("=======================Directory Stream======================");
+        try(var dirs = Files.newDirectoryStream(path, "*.xml")){
+            dirs.forEach(d -> System.out.println(Main.listDir(d)));
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("=======================Directory Stream======================");
+        try(var dirs = Files.newDirectoryStream(path,
+                p -> p.getFileName().toString().endsWith(".xml")
+                        && Files.isRegularFile(p) && Files.size(p) > 1000)){
+            dirs.forEach(d -> System.out.println(Main.listDir(d)));
+        }catch (IOException e){
             throw new RuntimeException(e);
         }
     }
