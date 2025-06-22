97062212fecbeb5813b31032a8af1213ff1ebb46
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-06-22 22:13:10 +0800

    File

--- DSA/src/main/java/dev/Innocent/Section8/FileOperation/FileWalker/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/FileOperation/FileWalker/Main.java
@@ -4,12 +4,14 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
         Path startingPath = Path.of(".");
-        FileVisitor<Path> statsVisitor = new StatsVisitor();
+        FileVisitor<Path> statsVisitor = new StatsVisitor(1);
         try {
             Files.walkFileTree(startingPath, statsVisitor);
         } catch (IOException e) {
@@ -17,12 +19,21 @@ public class Main {
         }
     }
 
-    private static class StatsVisitor extends SimpleFileVisitor<Path>{
+    private static class StatsVisitor extends SimpleFileVisitor<Path> {
+        private Path initialPath = null;
+        private final Map<Path, Long> folderSizes = new LinkedHashMap<>();
+        private int initialCount;
+        private int printLevel;
+
+        public StatsVisitor(int printLevel) {
+            this.printLevel = printLevel;
+        }
 
         @Override
         public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
             Objects.requireNonNull(file);
             Objects.requireNonNull(attrs);
+            folderSizes.merge(file.getFileName(), 0L, (o, n) -> o += attrs.size());
             return FileVisitResult.CONTINUE;
         }
 
@@ -30,6 +41,16 @@ public class Main {
         public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
             Objects.requireNonNull(dir);
             Objects.requireNonNull(attrs);
+            if (initialPath == null) {
+                initialPath = dir;
+                initialCount = dir.getNameCount();
+            } else {
+                int relativeLevel = dir.getNameCount() - initialCount;
+                if (relativeLevel == 1) {
+                    folderSizes.clear();
+                }
+                folderSizes.put(dir, 0L);
+            }
             return FileVisitResult.CONTINUE;
         }
 
@@ -38,6 +59,23 @@ public class Main {
             Objects.requireNonNull(dir);
 //            if (exc != null)
 //                throw exc;
+            if (dir.equals(initialPath)) {
+                return FileVisitResult.TERMINATE;
+            }
+
+            int relativeLevel = dir.getNameCount() - initialCount;
+            if (relativeLevel == 1) {
+                folderSizes.forEach((key, value) -> {
+                    int level = key.getNameCount() - initialCount;
+                    if(level < printLevel){
+                        System.out.printf("%s[%s] - %,d bytes %n",
+                                "\t".repeat(level), key.getFileName(), value);
+                    }
+                });
+            } else {
+                long folderSize = folderSizes.get(dir);
+                folderSizes.merge(dir.getParent(), 0L, (o, n) -> o += folderSize);
+            }
             return FileVisitResult.CONTINUE;
         }
     }
