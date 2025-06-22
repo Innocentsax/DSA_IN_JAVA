e3b9025eae1a22020058f0005fff9e107357b534
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-06-22 21:02:47 +0800

    File

--- DSA/src/main/java/dev/Innocent/Section8/FileOperation/FileWalker/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/FileOperation/FileWalker/Main.java
@@ -2,10 +2,9 @@ package dev.Innocent.Section8.FileOperation.FileWalker;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.FileVisitor;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
@@ -20,5 +19,26 @@ public class Main {
 
     private static class StatsVisitor extends SimpleFileVisitor<Path>{
 
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            Objects.requireNonNull(file);
+            Objects.requireNonNull(attrs);
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+            Objects.requireNonNull(dir);
+            Objects.requireNonNull(attrs);
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+            Objects.requireNonNull(dir);
+//            if (exc != null)
+//                throw exc;
+            return FileVisitResult.CONTINUE;
+        }
     }
 }
