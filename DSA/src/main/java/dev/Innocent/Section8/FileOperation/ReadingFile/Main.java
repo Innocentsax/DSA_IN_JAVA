b24b16f19527e4449c185b674439985e8c9d4684
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-06-27 12:42:01 +0800

    File

--- DSA/src/main/java/dev/Innocent/Section8/FileOperation/ReadingFile/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/FileOperation/ReadingFile/Main.java
@@ -1,7 +1,17 @@
 package dev.Innocent.Section8.FileOperation.ReadingFile;
 
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-
+        try(FileReader reader = new FileReader("file.txt")){
+            int data;
+            while((data = reader.read()) != -1){
+                System.out.println(data);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
