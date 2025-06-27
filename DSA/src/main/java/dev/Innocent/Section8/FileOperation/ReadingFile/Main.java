ed28b6d2ca7481ed9664524513a8d86f8efd908a
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-06-27 13:49:36 +0800

    File

--- DSA/src/main/java/dev/Innocent/Section8/FileOperation/ReadingFile/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/FileOperation/ReadingFile/Main.java
@@ -1,16 +1,26 @@
 package dev.Innocent.Section8.FileOperation.ReadingFile;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
         try(FileReader reader = new FileReader("file.txt")){
+            char[] block = new char[1000];
             int data;
-            while((data = reader.read()) != -1){
-                System.out.println(data);
+            while((data = reader.read(block)) != -1){
+                String content = new String(block, 0, data);
+                System.out.printf("----> [%d chars] %s%n", data, content);
             }
         }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        System.out.println("-----------------------------------------");
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader("file.txt"))){
+            bufferedReader.lines().forEach(System.out::println);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
