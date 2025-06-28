9b80c9ad57b7ccfc51abedccc7a5b0267a721c4c
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-06-28 18:58:06 +0800

    File

--- DSA/src/main/java/dev/Innocent/Section8/FileOperation/ScannerProject/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/FileOperation/ScannerProject/Main.java
@@ -1,7 +1,17 @@
 package dev.Innocent.Section8.FileOperation.ScannerProject;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        try(Scanner scanner = new Scanner(new File("file.txt"))){
+            while(scanner.hasNextLine()){
+                System.out.println(scanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
