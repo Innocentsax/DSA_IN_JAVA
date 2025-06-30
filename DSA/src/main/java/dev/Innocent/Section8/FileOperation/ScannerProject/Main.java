60706f0f16a69bf27f36ba98a060f7faa789a44d
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-06-30 23:04:16 +0800

    File

--- DSA/src/main/java/dev/Innocent/Section8/FileOperation/ScannerProject/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/FileOperation/ScannerProject/Main.java
@@ -2,12 +2,15 @@ package dev.Innocent.Section8.FileOperation.ScannerProject;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.regex.MatchResult;
 
 public class Main {
     public static void main(String[] args) {
-        try(Scanner scanner = new Scanner(new File("file.txt"))){
+        try(Scanner scanner = new Scanner(Path.of("fixedWidth.txt"))){
 //            while(scanner.hasNextLine()){
 //                System.out.println(scanner.nextLine());
 //            }
@@ -15,12 +18,21 @@ public class Main {
 //            scanner.useDelimiter("$");
 //            scanner.tokens().forEach(System.out::println);
 
-            scanner.findAll("[A-Za-z]{10,}")
-                    .map(MatchResult::group)
+//            scanner.findAll("[A-Za-z]{10,}")
+//                    .map(MatchResult::group)
+//                    .distinct()
+//                    .sorted()
+//                    .forEach(System.out::println);
+
+            var results = scanner.findAll(
+                    "(.{15})(.{3})(.{12})(.{8})(.{2}).*")
+                    .skip(1)
+                    .map(m -> m.group(3).trim())
                     .distinct()
                     .sorted()
-                    .forEach(System.out::println);
-        } catch (FileNotFoundException e) {
+                    .toArray(String[]::new);
+            System.out.println(Arrays.toString(results));
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
