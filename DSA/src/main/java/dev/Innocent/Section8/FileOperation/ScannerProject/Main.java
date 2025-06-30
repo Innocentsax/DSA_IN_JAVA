87e0ff00421e60e8a7c309259d7a15f095ed4e29
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-06-30 21:28:05 +0800

    File

--- DSA/src/main/java/dev/Innocent/Section8/FileOperation/ScannerProject/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/FileOperation/ScannerProject/Main.java
@@ -3,6 +3,7 @@ package dev.Innocent.Section8.FileOperation.ScannerProject;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.regex.MatchResult;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,9 +11,15 @@ public class Main {
 //            while(scanner.hasNextLine()){
 //                System.out.println(scanner.nextLine());
 //            }
-            System.out.println(scanner.delimiter());
-            scanner.useDelimiter("$");
-            scanner.tokens().forEach(System.out::println);
+//            System.out.println(scanner.delimiter());
+//            scanner.useDelimiter("$");
+//            scanner.tokens().forEach(System.out::println);
+
+            scanner.findAll("[A-Za-z]{10,}")
+                    .map(MatchResult::group)
+                    .distinct()
+                    .sorted()
+                    .forEach(System.out::println);
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
