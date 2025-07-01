378fc88024ff3329ddf1a13dc5bf68bf130d427b
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-07-01 21:51:41 +0800

    File

--- DSA/src/main/java/dev/Innocent/Section8/FileOperation/ReadingWithNIO2/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/FileOperation/ReadingWithNIO2/Main.java
@@ -1,11 +1,21 @@
 package dev.Innocent.Section8.FileOperation.ReadingWithNIO2;
 
 import java.awt.desktop.SystemEventListener;
+import java.io.IOException;
 import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println(System.getProperty("file.encoding"));
         System.out.println(Charset.defaultCharset());
+
+        Path path = Path.of("fixedWidth.txt");
+        try{
+            System.out.println(new String(Files.readAllBytes(path)));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
