984680db8acb2c4641734e5b5d80fde93f441851
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-07-05 19:01:12 +0800

    File

--- DSA/src/main/java/dev/Innocent/Section8/FileOperation/ReadingWithNIO2/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/FileOperation/ReadingWithNIO2/Main.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,13 +15,20 @@ public class Main {
         System.out.println(Charset.defaultCharset());
 
         System.out.println(Charset.defaultCharset());
-        System.out.println(Charset.defaultCharset());
 
         Path path = Path.of("fixedWidth.txt");
         try{
             System.out.println(new String(Files.readAllBytes(path)));
             System.out.println("-----------------------------------------");
             System.out.println(Files.readString(path));
+
+            Pattern p = Pattern.compile("(.{15})(.{3})(.{12})(.{8})(.{2}).*");
+            Set<String> values = new TreeSet<>();
+            Files.readAllLines(path).forEach(s -> {
+                if(!s.startsWith("Name")){
+
+                }
+            });
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
