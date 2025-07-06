8baf9662387887d8dbaf695051fc14ac24b0a063
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-07-06 16:13:26 +0800

    File

--- DSA/src/main/java/dev/Innocent/Section8/FileOperation/ReadingFilesChallenge/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/FileOperation/ReadingFilesChallenge/Main.java
@@ -2,6 +2,7 @@ package dev.Innocent.Section8.FileOperation.ReadingFilesChallenge;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.Arrays;
 import java.util.regex.Pattern;
 
 public class Main {
@@ -10,7 +11,10 @@ public class Main {
 //            System.out.printf("%,d lines in file%n", br.lines().count());
             Pattern pattern = Pattern.compile("\\p{javaWhitespace}+");
             System.out.printf("%,d words in file%n", br.lines()
-                    .flatMap(pattern::splitAsStream).count());
+//                    .flatMap(pattern::splitAsStream).count());
+                    .flatMap(l -> Arrays.stream(l.split(pattern.toString())))
+                    .count()
+            );
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
