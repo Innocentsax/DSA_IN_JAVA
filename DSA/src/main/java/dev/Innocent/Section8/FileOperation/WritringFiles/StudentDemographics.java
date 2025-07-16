fae5c8d212ff9191bfd7e23904522406bcbd3529
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-07-16 22:05:15 +0800

    Writing into a file

--- DSA/src/main/java/dev/Innocent/Section8/FileOperation/WritringFiles/StudentDemographics.java
+++ DSA/src/main/java/dev/Innocent/Section8/FileOperation/WritringFiles/StudentDemographics.java
@@ -1,5 +1,7 @@
 package dev.Innocent.Section8.FileOperation.WritringFiles;
 
+import java.util.StringJoiner;
+
 public record StudentDemographics(String countryCode, int enrolledMonth,
                                   int enrolledYear, int ageAtEnrollment, String gender,
                                   boolean previousProgrammingExperience ) {
@@ -9,5 +11,16 @@ public record StudentDemographics(String countryCode, int enrolledMonth,
         return "%s,%d,%d,%d,%s,%b".formatted(countryCode,
                 enrolledMonth,enrolledYear, ageAtEnrollment,gender,
                 previousProgrammingExperience);
+    }
+
+    public String JSON() {
+        return new StringJoiner(", ", "{", "}")
+                .add("\"countryCode\":\"" + countryCode + "\"")
+                .add("\"enrolledMonth\":" + enrolledMonth)
+                .add("\"enrolledYear\":" + enrolledYear)
+                .add("\"ageAtEnrollment\":" + ageAtEnrollment)
+                .add("\"gender\":\"" + gender + "\"")
+                .add("\"previousProgrammingExperience\":" + previousProgrammingExperience)
+                .toString();
     }
 }
