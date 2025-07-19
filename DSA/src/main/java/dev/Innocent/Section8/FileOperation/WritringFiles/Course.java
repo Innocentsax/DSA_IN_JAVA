85457a1474f19424a8af87a911fef150df39a9f7
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-07-19 21:04:36 +0800

    Writing into a file

--- DSA/src/main/java/dev/Innocent/Section8/FileOperation/WritringFiles/Course.java
+++ DSA/src/main/java/dev/Innocent/Section8/FileOperation/WritringFiles/Course.java
@@ -1,5 +1,7 @@
 package dev.Innocent.Section8.FileOperation.WritringFiles;
 
+import java.util.StringJoiner;
+
 public record Course(String courseCode, String title) {
 
     public int getLectureCount() {
@@ -9,5 +11,12 @@ public record Course(String courseCode, String title) {
     @Override
     public String toString() {
         return "%s %s".formatted(courseCode, title);
+    }
+
+    public String JSON() {
+        return new StringJoiner(", ", "{", "}")
+                .add("\"courseCode\":\"" + courseCode + "\"")
+                .add("\"title\":\"" + title + "\"")
+                .toString();
     }
 }
