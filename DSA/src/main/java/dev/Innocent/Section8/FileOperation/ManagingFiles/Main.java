b21179e017ba308db9cd8c8f1e5b6c088eeaf723
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-07-21 23:25:57 +0800

    Writing into a file

--- DSA/src/main/java/dev/Innocent/Section8/FileOperation/ManagingFiles/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/FileOperation/ManagingFiles/Main.java
@@ -1,7 +1,17 @@
 package dev.Innocent.Section8.FileOperation.ManagingFiles;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
+        File oldFile = new File("students.json");
+        File newFile = new File("student-activity.json");
 
+        if(oldFile.exists()){
+            oldFile.renameTo(newFile);
+            System.out.println("FIle renamed successfully");
+        } else {
+            System.out.println("File does not exist");
+        }
     }
 }
