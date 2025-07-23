739a1745b564a8a2aa04b5fad2b5a7d2b2c287bc
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-07-23 22:31:39 +0800

    Writing into a file

--- DSA/src/main/java/dev/Innocent/Section8/FileOperation/ManagingFiles/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/FileOperation/ManagingFiles/Main.java
@@ -7,25 +7,35 @@ import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) {
-        File oldFile = new File("students.json");
-        File newFile = new File("student-activity.json");
+//        File oldFile = new File("students.json");
+//        File newFile = new File("student-activity.json");
+//
+//        if(oldFile.exists()){
+//            oldFile.renameTo(newFile);
+//            System.out.println("FIle renamed successfully");
+//        } else {
+//            System.out.println("File does not exist");
+//        }
+//
+//        Path oldPath = oldFile.toPath();
+//        Path newPath = newFile.toPath();
+//
+//        try{
+//            Files.move(newPath, oldPath);
+//            System.out.println("Path renamed successfully");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
-        if(oldFile.exists()){
-            oldFile.renameTo(newFile);
-            System.out.println("FIle renamed successfully");
-        } else {
-            System.out.println("File does not exist");
-        }
-
-
-        Path oldPath = oldFile.toPath();
-        Path newPath = newFile.toPath();
+        Path oldPath = Path.of("students.json");
+        Path newPath = Path.of("file/student-activity.json");
 
         try{
-            Files.move(newPath, oldPath);
-            System.out.println("Path renamed successfully");
+            Files.createDirectories(newPath.subpath(0, newPath.getNameCount() - 1));
+            Files.move(oldPath, newPath);
+            System.out.println(oldPath + " moved (renamed to) --> " + newPath);
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
     }
 }
