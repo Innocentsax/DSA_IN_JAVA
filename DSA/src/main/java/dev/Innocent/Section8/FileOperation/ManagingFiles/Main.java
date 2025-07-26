f83f8df5205ba7ca95c2f155cf8384eb97a0653b
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-07-26 09:58:20 +0800

    Writing into a file

--- DSA/src/main/java/dev/Innocent/Section8/FileOperation/ManagingFiles/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/FileOperation/ManagingFiles/Main.java
@@ -66,6 +66,39 @@ public class Main {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+        Path jsonPath = Path.of("USPopulationByState.txt");
+        try(var reader = new InputStreamReader(uri.toURL().openStream());
+        var writer = Files.newBufferedWriter(jsonPath)){
+            reader.transferTo(writer);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try(var reader = new InputStreamReader(uri.toURL().openStream());
+            PrintWriter writer = new PrintWriter("USPopulationByState.csv")){
+            reader.transferTo(new Writer() {
+                @Override
+                public void write(char[] cbuf, int off, int len) throws IOException {
+                    String jsonString = new String(cbuf, off, len).trim();
+                    jsonString = jsonString.replace('[', ' ').trim();
+                    jsonString = jsonString.replaceAll("\\]", "");
+                    writer.write(jsonString);
+                }
+
+                @Override
+                public void flush() throws IOException {
+                    writer.flush();
+                }
+
+                @Override
+                public void close() throws IOException {
+                    writer.close();
+                }
+            });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static void recurseCopy(Path source, Path target) throws IOException {
