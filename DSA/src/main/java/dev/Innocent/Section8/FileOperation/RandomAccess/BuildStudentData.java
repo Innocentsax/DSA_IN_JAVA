475897e74f5d9dc3b592daa3fe363a288c27466e
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-07-29 08:17:24 +0800

    Writing into a file

--- DSA/src/main/java/dev/Innocent/Section8/FileOperation/RandomAccess/BuildStudentData.java
+++ DSA/src/main/java/dev/Innocent/Section8/FileOperation/RandomAccess/BuildStudentData.java
@@ -1,10 +1,13 @@
 package dev.Innocent.Section8.FileOperation.RandomAccess;
 
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class BuildStudentData {
 
@@ -16,7 +19,40 @@ public class BuildStudentData {
         try{
             Files.deleteIfExists(Path.of(dataFile));
             String data = Files.readString(studentJson);
-            data = data.replaceFirst("^(\\[)", "");
+            data = data.replaceFirst("^(\\[)", "")
+                    .replaceFirst("(\\])$", "");
+            var records = data.split(System.lineSeparator());
+            System.out.println("# of records = " + records.length);
+
+
+            long startingPos = 4 + (16L * records.length);
+
+            Pattern idPattern = Pattern.compile("studentId\":([0-9]+)");
+            try(RandomAccessFile ra = new RandomAccessFile(dataFile, "rw")){
+                ra.seek(startingPos);
+                for(String record : records){
+                    Matcher m = idPattern.matcher(record);
+                    if(m.find()){
+                        long id = Long.parseLong(m.group(1));
+                        indexedIds.put(id, ra.getFilePointer());
+                        ra.writeUTF(record);
+                    }
+                }
+                writeIndex(ra, indexedIds);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void writeIndex(RandomAccessFile ra, Map<Long, Long> indexMap){
+        try{
+            ra.seek(0);
+            ra.writeInt(indexMap.size());
+            for (var studentIdx : indexMap.entrySet()){
+                ra.writeLong(studentIdx.getKey());
+                ra.writeLong(studentIdx.getValue());
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
