4ca4c6441225e335e8b12051ba9ad974428d3ca7
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-07-30 22:32:05 +0800

    Writing into a file

--- DSA/src/main/java/dev/Innocent/Section8/FileOperation/RandomAccess/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/FileOperation/RandomAccess/Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
 
@@ -11,7 +12,26 @@ public class Main {
     private static int recordsInFile = 0;
 
     public static void main(String[] args) {
-        BuildStudentData.build("studentDate");
+//        BuildStudentData.build("studentDate");
+
+        try(RandomAccessFile ra = new RandomAccessFile("studentData.dat", "r")){
+            loadIndex(ra, 0);
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter a Student Id or 0 to quit");
+
+            while(scanner.hasNext()){
+                long studentId = Long.parseLong(scanner.nextLine());
+                if(studentId < 1){
+                    break;
+                }
+                ra.seek(indexedId.get(studentId));
+                String targetedRecord = ra.readUTF();
+                System.out.println(targetedRecord);
+                System.out.println("Enter another Student Id or 0 to quit");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static void loadIndex(RandomAccessFile ra, int indexPosition){
