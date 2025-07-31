d5d16fae0b5ea7151fbbf92df689ddfd42d676ee
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-07-31 08:19:02 +0800

    Writing into a file

--- DSA/src/main/java/dev/Innocent/Section8/FileOperation/RandomAccess/BuildStudentData.java
+++ DSA/src/main/java/dev/Innocent/Section8/FileOperation/RandomAccess/BuildStudentData.java
@@ -11,7 +11,7 @@ import java.util.regex.Pattern;
 
 public class BuildStudentData {
 
-    public static void build(String datFileName){
+    public static void build(String datFileName, boolean separateIndex){
         Path studentJson = Path.of("students.json");
         String dataFile = datFileName + ".dat";
         Map<Long, Long> indexedIds = new LinkedHashMap<>();
@@ -25,7 +25,7 @@ public class BuildStudentData {
             System.out.println("# of records = " + records.length);
 
 
-            long startingPos = 4 + (16L * records.length);
+            long startingPos =separateIndex ? 0 : 4 + (16L * records.length);
 
             Pattern idPattern = Pattern.compile("studentId\":([0-9]+)");
             try(RandomAccessFile ra = new RandomAccessFile(dataFile, "rw")){
@@ -38,7 +38,8 @@ public class BuildStudentData {
                         ra.writeUTF(record);
                     }
                 }
-                writeIndex(ra, indexedIds);
+                writeIndex((separateIndex) ? new RandomAccessFile(
+                        datFileName + ".idx", "rw") : ra, indexedIds);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
