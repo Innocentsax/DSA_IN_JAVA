51d4ec40f96676cd182747fe6ca8188a52d9cbed
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-08-04 20:24:15 +0800

    Writing into a file

--- DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/Thread/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/Thread/Main.java
@@ -3,5 +3,6 @@ package dev.Innocent.Section8.ConcurrencyAndMultithreading.Thread;
 public class Main {
     public static void main(String[] args) {
         var currentThread = Thread.currentThread();
+        System.out.println(currentThread.getClass().getName());
     }
 }
