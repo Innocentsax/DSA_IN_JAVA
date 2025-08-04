f0a23f8a3146405a3fb5412417600239fe638eea
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-08-04 22:25:32 +0800

    Writing into a file

--- DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/Thread/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/Thread/Main.java
@@ -4,5 +4,18 @@ public class Main {
     public static void main(String[] args) {
         var currentThread = Thread.currentThread();
         System.out.println(currentThread.getClass().getName());
+
+        System.out.println(currentThread);
+        printThreadState(currentThread);
+    }
+
+    public static void printThreadState(Thread thread){
+        System.out.println("----------------------------");
+        System.out.println("Thread ID: " + thread.getId());
+        System.out.println("Thread Name: " + thread.getName());
+        System.out.println("Thread Priority: " + thread.getPriority());
+        System.out.println("Thread State: " + thread.getState());
+        System.out.println("Thread Group: " + thread.getThreadGroup());
+        System.out.println("Thread is Alive: " + thread.isAlive());
     }
 }
