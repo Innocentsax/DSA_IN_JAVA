ce18ed9aa83bf5aa15022dee45aaae887ad74672
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-08-04 22:24:46 +0800

    Writing into a file

---
+++ DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/Thread/CustomThread.java
@@ -0,0 +1,15 @@
+package dev.Innocent.Section8.ConcurrencyAndMultithreading.Thread;
+
+public class CustomThread extends Thread{
+    @Override
+    public void run() {
+        for (int i = 1; i < 5; i++){
+            System.out.print(" 1 ");
+            try{
+                Thread.sleep(500); // Adding a 1-second delay between each count
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
