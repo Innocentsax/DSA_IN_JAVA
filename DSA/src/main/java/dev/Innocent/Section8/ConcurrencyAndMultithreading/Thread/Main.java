27f269d220b280c46a7fe7758a0775bd4c9be29b
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-08-05 21:43:26 +0800

    Writing into a file

--- DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/Thread/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/Thread/Main.java
@@ -1,12 +1,29 @@
 package dev.Innocent.Section8.ConcurrencyAndMultithreading.Thread;
 
+import java.util.concurrent.TimeUnit;
+
 public class Main {
     public static void main(String[] args) {
         var currentThread = Thread.currentThread();
         System.out.println(currentThread.getClass().getName());
 
         System.out.println(currentThread);
+
+        currentThread.setName("MainGuy");
+        currentThread.setPriority(Thread.MAX_PRIORITY);
         printThreadState(currentThread);
+
+        CustomThread customThread = new CustomThread();
+        customThread.start();
+
+        for (int i = 1; i <= 3; i++) {
+            System.out.print("0");
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public static void printThreadState(Thread thread){
