47ad02a2af82e25486e34461a28954cb630319d4
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-08-09 18:03:06 +0800

    Writing into a file

--- DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/ThreadChallenge/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/ThreadChallenge/Main.java
@@ -2,6 +2,33 @@ package dev.Innocent.Section8.ConcurrencyAndMultithreading.ThreadChallenge;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.print("Main Thread state here");
+        try {
+            System.out.println("get me some space");
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
+        SecondThread secondThread = new SecondThread();
+        secondThread.start();
+
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 8; i++) {
+                    System.out.println(" Udo ");
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        };
+
+        Thread toThread = new Thread(runnable);
+        toThread.start();
     }
 }
