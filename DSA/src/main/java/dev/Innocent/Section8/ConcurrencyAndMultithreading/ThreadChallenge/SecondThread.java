1ccf05b08d1fbd8cff99f4a1a568cccd504e9828
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-08-10 00:06:21 +0800

    Thread

--- DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/ThreadChallenge/SecondThread.java
+++ DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/ThreadChallenge/SecondThread.java
@@ -3,12 +3,13 @@ package dev.Innocent.Section8.ConcurrencyAndMultithreading.ThreadChallenge;
 public class SecondThread extends Thread{
     @Override
     public void run() {
-        for (int i = 0; i < 9; i++) {
-            System.out.println(" Innocent ");
+        for (int i = 0; i < 9; i+=2) {
+            System.out.println(" oddThread: " + i);
             try {
-                Thread.sleep(100);
+                Thread.sleep(1000);
             } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                System.out.println("OddThread interrupted!");
+                break;
             }
         }
     }
