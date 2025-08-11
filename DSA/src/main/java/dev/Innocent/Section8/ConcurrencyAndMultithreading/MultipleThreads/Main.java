5d40a7b10b0a876e4cea67d9cb678ab0f008e889
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-08-11 11:41:12 +0800

    Multiple Thread

--- DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/MultipleThreads/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/MultipleThreads/Main.java
@@ -1,7 +1,37 @@
 package dev.Innocent.Section8.ConcurrencyAndMultithreading.MultipleThreads;
 
+import java.util.concurrent.TimeUnit;
+
 public class Main {
     public static void main(String[] args) {
 
+    }
+}
+
+class StopWatch {
+    private TimeUnit timeUnit;
+
+    public StopWatch(TimeUnit timeUnit) {
+        this.timeUnit = timeUnit;
+    }
+
+    public void countDown(int unitCount){
+        String threadName = Thread.currentThread().getName();
+        ThreadColor threadColor = ThreadColor.ANSI_RESET;
+        try{
+            threadColor = ThreadColor.valueOf(threadName);
+        } catch (IllegalArgumentException e) {
+            //User may pass a bad color name, will just ignore this error.
+        }
+
+        String color = threadColor.color();
+        for(int i = unitCount; i > 0; i--){
+            try {
+                timeUnit.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.printf("%s%s Thread : i = %d%n", color, threadName, i);
+        }
     }
 }
