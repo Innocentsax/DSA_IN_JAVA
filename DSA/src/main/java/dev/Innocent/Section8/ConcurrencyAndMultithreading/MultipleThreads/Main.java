202022784dffa121e62befe66d6f11f2db5123cf
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-08-13 17:46:16 +0800

    Multiple Thread

--- DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/MultipleThreads/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/MultipleThreads/Main.java
@@ -4,7 +4,14 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
-
+        StopWatch stopWatch = new StopWatch(TimeUnit.SECONDS);
+        Thread green = new Thread(stopWatch::countDown, ThreadColor.ANSI_GREEN.name());
+        Thread purple = new Thread(() -> stopWatch.countDown(7),
+                ThreadColor.ANSI_PURPLE.name());
+        Thread red = new Thread(stopWatch::countDown, ThreadColor.ANSI_RED.name());
+        green.start();
+        purple.start();
+        red.start();
     }
 }
 
