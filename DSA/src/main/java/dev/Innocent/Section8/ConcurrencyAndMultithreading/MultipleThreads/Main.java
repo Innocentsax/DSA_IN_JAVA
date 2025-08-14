be6fb9cf662f1410e3576211683164f554c697e5
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-08-14 23:46:56 +0800

    Multiple Thread

--- DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/MultipleThreads/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/MultipleThreads/Main.java
@@ -4,13 +4,13 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
-        StopWatch stopWatch = new StopWatch(TimeUnit.SECONDS);
+        StopWatch greenWatch = new StopWatch(TimeUnit.SECONDS);
         StopWatch purpleWatch = new StopWatch(TimeUnit.SECONDS);
         StopWatch redWatch = new StopWatch(TimeUnit.SECONDS);
-        Thread green = new Thread(stopWatch::countDown, ThreadColor.ANSI_GREEN.name());
-        Thread purple = new Thread(() -> stopWatch.countDown(7),
+        Thread green = new Thread(greenWatch::countDown, ThreadColor.ANSI_GREEN.name());
+        Thread purple = new Thread(() -> purpleWatch.countDown(7),
                 ThreadColor.ANSI_PURPLE.name());
-        Thread red = new Thread(stopWatch::countDown, ThreadColor.ANSI_RED.name());
+        Thread red = new Thread(redWatch::countDown, ThreadColor.ANSI_RED.name());
         green.start();
         purple.start();
         red.start();
