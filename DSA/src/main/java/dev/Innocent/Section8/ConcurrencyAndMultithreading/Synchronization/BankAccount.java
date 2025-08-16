1688be8e896098355a30bfbea86c8ce2cda55c7b
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-08-16 14:11:30 +0800

    Multiple Thread

---
+++ DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/Synchronization/BankAccount.java
@@ -0,0 +1,13 @@
+package dev.Innocent.Section8.ConcurrencyAndMultithreading.Synchronization;
+
+public class BankAccount {
+    private double balance;
+
+    public BankAccount(double balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
