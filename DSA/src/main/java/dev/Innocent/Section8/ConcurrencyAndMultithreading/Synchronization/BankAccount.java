d07a632115bcff5ad59341fb328ed5d950e11f29
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-08-17 22:09:28 +0800

    Multiple Thread

--- DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/Synchronization/BankAccount.java
+++ DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/Synchronization/BankAccount.java
@@ -10,4 +10,39 @@ public class BankAccount {
     public double getBalance() {
         return balance;
     }
+
+    public void deposit(double amount){
+        try{
+            System.out.println("Deposit - Talking to the teller at the bank");
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        synchronized (this){
+            double origBalance = balance;
+            balance += amount;
+            System.out.printf("STARTING BALANCE: %.0f, DEPOSIT (%.0f)" +
+                    " : NEW BALANCE = %.0f%n", origBalance, amount, balance);
+        }
+    }
+
+    public synchronized void withdraw(double amount){
+        try{
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        double origBalance = balance;
+        if(amount <= balance){
+            balance -= amount;
+            System.out.printf("STARTING BALANCE: %.0f, WITHDRAW (%.0f)" +
+                    " : NEW BALANCE = %.0f%n", origBalance, amount, balance);
+        }else {
+            System.out.printf("STARTING BALANCE: %.0f, WITHDRAWAL (%.0f)" +
+                    ": INSUFFICIENT FUNDS!", origBalance, amount);
+        }
+
+    }
 }
