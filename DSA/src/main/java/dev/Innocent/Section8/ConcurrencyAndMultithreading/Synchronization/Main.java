fb10b49ac1bc32d4649129ccda0689082ec09899
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-08-17 22:09:39 +0800

    Multiple Thread

--- DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/Synchronization/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/Synchronization/Main.java
@@ -1,4 +1,28 @@
 package dev.Innocent.Section8.ConcurrencyAndMultithreading.Synchronization;
 
 public class Main {
+    public static void main(String[] args) {
+        BankAccount companyAccount = new BankAccount(10000);
+
+        Thread thread1 = new Thread(() -> companyAccount.withdraw(2500));
+        Thread thread2 = new Thread(() -> companyAccount.deposit(5000));
+        Thread thread3 = new Thread(() -> companyAccount.withdraw(2500));
+        Thread thread4 = new Thread(() -> companyAccount.withdraw(5000));
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        thread4.start();
+
+        try{
+            thread1.join();
+            thread2.join();
+            thread3.join();
+            thread4.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Final Balance: " + companyAccount.getBalance());
+    }
 }
