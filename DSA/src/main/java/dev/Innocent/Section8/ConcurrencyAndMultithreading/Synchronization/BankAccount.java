7bea85ac83cbaf06f899582f35a2aeecd52d3467
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-08-18 08:41:13 +0800

    Multiple Thread

--- DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/Synchronization/BankAccount.java
+++ DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/Synchronization/BankAccount.java
@@ -1,10 +1,32 @@
 package dev.Innocent.Section8.ConcurrencyAndMultithreading.Synchronization;
 
+import dev.Innocent.Section8.StreamIntermidiate.Seat;
+
 public class BankAccount {
     private double balance;
+    private String name;
 
-    public BankAccount(double balance) {
+    private final Object lockName = new Object();
+    private final Object lockBalance = new Object();
+
+    public BankAccount(String name, double balance) {
         this.balance = balance;
+        this.name = name;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        synchronized (lockName){
+            this.name = name;
+            System.out.println("Updated name = " + this.name);
+        }
     }
 
     public double getBalance() {
@@ -19,11 +41,21 @@ public class BankAccount {
             throw new RuntimeException(e);
         }
 
-        synchronized (this){
+        synchronized (lockBalance){
             double origBalance = balance;
             balance += amount;
             System.out.printf("STARTING BALANCE: %.0f, DEPOSIT (%.0f)" +
                     " : NEW BALANCE = %.0f%n", origBalance, amount, balance);
+            addPromoDollars(amount);
+        }
+    }
+
+    private void addPromoDollars(double amount){
+        if(amount >= 5000){
+            synchronized (lockBalance){
+                System.out.println("Congratulation, you earned a promotional deposit.");
+                balance += 25;
+            }
         }
     }
 
