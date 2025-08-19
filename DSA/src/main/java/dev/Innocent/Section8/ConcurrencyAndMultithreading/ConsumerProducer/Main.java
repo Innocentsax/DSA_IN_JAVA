cd18b0a87063a28c4122424210920aa5d50c9a15
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-08-19 08:32:22 +0800

    Multiple Thread

--- DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/ConsumerProducer/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/ConsumerProducer/Main.java
@@ -1,19 +1,24 @@
 package dev.Innocent.Section8.ConcurrencyAndMultithreading.ConsumerProducer;
 
-class MessageRepository{
+import java.util.Random;
+
+class MessageRepository {
+
     private String message;
     private boolean hasMessage = false;
 
-    public synchronized String read(){
-        while(!hasMessage){
+    public synchronized String read() {
+
+        while (!hasMessage) {
 
         }
         hasMessage = false;
         return message;
     }
 
-    public synchronized void write(String message){
-        while (hasMessage){
+    public synchronized void write(String message) {
+
+        while (hasMessage) {
 
         }
         hasMessage = true;
@@ -21,8 +26,74 @@ class MessageRepository{
     }
 }
 
+class MessageWriter implements Runnable {
+
+    private MessageRepository outgoingMessage;
+
+    private final String text = """
+            Humpty Dumpty sat on a wall,
+            Humpty Dumpty had a great fall,
+            All the king's horses and all the king's men,
+            Couldn't put Humpty together again.""";
+
+    public MessageWriter(MessageRepository outgoingMessage) {
+        this.outgoingMessage = outgoingMessage;
+    }
+
+    @Override
+    public void run() {
+
+        Random random = new Random();
+        String[] lines = text.split("\n");
+
+        for (int i = 0; i < lines.length; i++) {
+            outgoingMessage.write(lines[i]);
+            try {
+                Thread.sleep(random.nextInt(500, 2000));
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        outgoingMessage.write("Finished");
+    }
+}
+
+class MessageReader implements Runnable {
+
+    private MessageRepository incomingMessage;
+
+    public MessageReader(MessageRepository incomingMessage) {
+        this.incomingMessage = incomingMessage;
+    }
+
+    @Override
+    public void run() {
+
+        Random random = new Random();
+        String latestMessage = "";
+
+        do {
+            try {
+                Thread.sleep(random.nextInt(500, 2000));
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            latestMessage = incomingMessage.read();
+            System.out.println(latestMessage);
+        } while (!latestMessage.equals("Finished"));
+    }
+}
+
 public class Main {
+
     public static void main(String[] args) {
 
+        MessageRepository messageRepository = new MessageRepository();
+
+        Thread reader = new Thread(new MessageReader(messageRepository));
+        Thread writer = new Thread(new MessageWriter(messageRepository));
+
+        reader.start();
+        writer.start();
     }
 }
