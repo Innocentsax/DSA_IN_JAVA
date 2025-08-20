813a4c8b896e5b01c16c27972e3d635163801947
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-08-20 12:46:27 +0800

    Multiple Thread

--- DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/ConsumerProducer/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/ConsumerProducer/Main.java
@@ -10,18 +10,28 @@ class MessageRepository {
     public synchronized String read() {
 
         while (!hasMessage) {
-
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
         hasMessage = false;
+        notify();
         return message;
     }
 
     public synchronized void write(String message) {
 
         while (hasMessage) {
-
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
         hasMessage = true;
+        notifyAll();
         this.message = message;
     }
 }
