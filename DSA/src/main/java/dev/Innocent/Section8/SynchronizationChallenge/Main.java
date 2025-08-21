c49bc6b71391ca3d6726c96979206536934a0437
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-08-21 21:43:26 +0800

    Multiple Thread

--- DSA/src/main/java/dev/Innocent/Section8/SynchronizationChallenge/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/SynchronizationChallenge/Main.java
@@ -1,8 +1,12 @@
 package dev.Innocent.Section8.SynchronizationChallenge;
 
+record Order(long orderId, String item, int qty){
+
+}
+
 public class Main {
 
     public static void main(String[] args) {
-        
+
     }
 }
