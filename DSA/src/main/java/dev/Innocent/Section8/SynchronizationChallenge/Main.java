725a67150139331e6a664ef4c5d5059947b7417e
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-08-22 08:15:55 +0800

    Multiple Thread

--- DSA/src/main/java/dev/Innocent/Section8/SynchronizationChallenge/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/SynchronizationChallenge/Main.java
@@ -1,12 +1,33 @@
 package dev.Innocent.Section8.SynchronizationChallenge;
 
+import java.util.Random;
+
 record Order(long orderId, String item, int qty){
 
 }
 
 public class Main {
 
-    public static void main(String[] args) {
+    private static final Random random = new Random();
 
+    public static void main(String[] args) {
+        ShoeWarehouse warehouse = new ShoeWarehouse();
+        Thread producerThread = new Thread(() -> {
+            warehouse.receiveOrder(new Order(
+                    random.nextLong(1000000, 9999999),
+                    ShoeWarehouse.PRODUCT_LIST[random.nextInt(0, 5)],
+                    random.nextInt(1, 4)
+            ));
+        });
+        producerThread.start();
+
+        for (int i = 0; i < 2; i++) {
+            Thread consumerThread = new Thread(() -> {
+                for (int j = 0; j < 5; j++) {
+                    Order item = warehouse.fulfillOrder();
+                }
+            });
+            consumerThread.start();
+        }
     }
 }
