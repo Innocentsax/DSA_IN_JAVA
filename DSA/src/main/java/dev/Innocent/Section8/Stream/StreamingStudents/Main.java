b51f94008415cdd29f525b57ccb20cd874979957
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-08-23 18:07:09 +0800

    Multiple Thread

--- DSA/src/main/java/dev/Innocent/Section8/Stream/StreamingStudents/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/Stream/StreamingStudents/Main.java
@@ -1,11 +1,10 @@
-package dev.Innocent.Section8.StreamingStudents;
+package dev.Innocent.Section8.Stream.StreamingStudents;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
 
