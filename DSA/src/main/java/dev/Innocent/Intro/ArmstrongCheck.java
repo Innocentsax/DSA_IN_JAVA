c86720676e0961244a7f0bd1d7b9b1efcfbef395
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-10-03 16:08:44 +0800

    Multiple Thread

---
+++ DSA/src/main/java/dev/Innocent/Intro/ArmstrongCheck.java
@@ -0,0 +1,22 @@
+package dev.Innocent.Intro;
+
+public class ArmstrongCheck {
+    public static void main(String[] args) {
+        int number = 243;
+        int original = number;
+        int result = 0;
+        int digits = String.valueOf(number).length();
+
+        while (number > 0){
+            int digit = number % 10;
+            result += Math.pow(digit, digits);
+            number /= 10;
+        }
+
+        if(result == original){
+            System.out.println(original + " is Armstrong number");
+        }
+    }
+
+
+}
