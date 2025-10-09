bc4082791caedcc8aa28f14ab18bd5048d048aec
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-10-09 23:13:26 +0800

    Multiple Thread

---
+++ DSA/src/main/java/dev/Innocent/refresh/MinMax.java
@@ -0,0 +1,27 @@
+package dev.Innocent.refresh;
+
+public class MinMax {
+
+    public static void main(String[] args) {
+        int[] numbers = {5, 8, 2, 10, 3, 7};
+
+        findMaxMin(numbers);
+    }
+
+    public static void findMaxMin(int[] numbers){
+        int max = numbers[0];
+        int min = numbers[0];
+
+        for(int i = 1; i < numbers.length; i++){
+            if(numbers[i] > max){
+                max = numbers[i];
+            }
+            if(numbers[i] < min){
+                min = numbers[i];
+            }
+        }
+
+        System.out.println("Maximum element: " + max);
+        System.out.println("Minimum element: " + min);
+    }
+}
