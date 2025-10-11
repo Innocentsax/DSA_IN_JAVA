0dfa8f05d25541bf4928072a872f17173495a3b5
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-10-11 18:37:24 +0800

    DSA

---
+++ DSA/src/main/java/dev/Innocent/refresh/ReverseArray.java
@@ -0,0 +1,22 @@
+package dev.Innocent.refresh;
+
+public class ReverseArray {
+
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3,4,5,6};
+    }
+
+    private static void reverseArray(int[] array){
+        int start = 0;
+        int end = array.length - 1;
+
+        while (start < end){
+            int temp = array[start];
+            array[start] = array[end];
+            array[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+}
