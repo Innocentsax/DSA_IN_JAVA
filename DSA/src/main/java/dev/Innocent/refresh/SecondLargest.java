1982dfe80b81a0512675f7b7900c78916412a262
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-10-12 20:44:51 +0800

    DSA

--- DSA/src/main/java/dev/Innocent/refresh/SecondLargest.java
+++ DSA/src/main/java/dev/Innocent/refresh/SecondLargest.java
@@ -2,18 +2,24 @@ package dev.Innocent.refresh;
 
 public class SecondLargest {
     public static void main(String[] args) {
+        int[] number = {5, 8, 2, 10, 3, 7};
 
+        int result = findSecondLargest(number);
+        System.out.println(result);
     }
 
-    private static void findSecondLargest(int[] arr){
-        int first = Integer.MAX_VALUE;
-        int second = Integer.MAX_VALUE;
+    private static int findSecondLargest(int[] arr){
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
 
         for(int num : arr){
             if(num > first){
                 second = first;
                 first = num;
+            } else if (num > second && num != first) {
+                second = num;
             }
         }
+        return second;
     }
 }
