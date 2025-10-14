9badff5d92ac314229371d4d9d1a6ee338aa5a95
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-10-14 22:16:10 +0800

    DSA

--- DSA/src/main/java/dev/Innocent/refresh/MissingNumber.java
+++ DSA/src/main/java/dev/Innocent/refresh/MissingNumber.java
@@ -10,6 +10,11 @@ public class MissingNumber {
         List<String> list = Arrays.asList("Array", "David", "Man");
         java6version(list);
         java8version(list);
+
+        int[] nm = {1, 2, 4, 5, 6};
+        int n = 6;
+        int result = findMissingNumber(nm, n);
+        System.out.println(result);
     }
 
     //1.  Create a class in Java 6 version and Java 8 version. Filter out names that start with "A"
@@ -28,5 +33,15 @@ public class MissingNumber {
     public static void java8version(List<String> list){
         List<String> filtered = list.stream().filter(name -> !name.startsWith("A")).toList();
         System.out.println(filtered);
+    }
+
+    public static int findMissingNumber(int[] arr, int n){
+        int expectedNum = n * (n + 1) / 2;
+        int actual = 0;
+
+        for(int num : arr){
+            actual += num;
+        }
+        return expectedNum - actual;
     }
 }
