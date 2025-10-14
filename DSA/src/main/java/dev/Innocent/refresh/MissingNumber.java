17c1323541beab515906d9146a9ed75717d54437
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-10-14 21:24:24 +0800

    DSA

---
+++ DSA/src/main/java/dev/Innocent/refresh/MissingNumber.java
@@ -0,0 +1,17 @@
+package dev.Innocent.refresh;
+
+import java.util.List;
+
+public class MissingNumber {
+
+    public static void main(String[] args) {
+
+    }
+
+    //1.  Create a class in Java 6 version and Java 8 version. Filter out names that start with "A"
+    //2. Find the missing number in an array of 1…n.
+
+    public static void java6version(List<String> list){
+
+    }
+}
