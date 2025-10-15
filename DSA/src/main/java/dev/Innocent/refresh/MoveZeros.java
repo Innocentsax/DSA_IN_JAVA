25fb8acdc948483be3b4f805bbcb75b40e33c85c
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-10-15 19:36:42 +0800

    DSA

--- DSA/src/main/java/dev/Innocent/refresh/MoveZeros.java
+++ DSA/src/main/java/dev/Innocent/refresh/MoveZeros.java
@@ -2,6 +2,18 @@ package dev.Innocent.refresh;
 
 public class MoveZeros {
     public static void main(String[] args) {
+        int[] num = {1, 0, 3, 0, 8, 0, 7};
+    }
 
+    // Move all zeros to the end of an array.
+    private static void moveZerosToEnd(int[] zeros){
+        int index = 0;
+
+        for(int i = 0; i < zeros.length; i++){
+            if(zeros[i] != 0){
+                zeros[index] = zeros[i];
+                index++;
+            }
+        }
     }
 }
