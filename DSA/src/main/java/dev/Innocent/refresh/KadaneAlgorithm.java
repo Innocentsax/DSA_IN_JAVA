11564eef365694df6a36fd214f71b51211cfb0f2
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-10-18 15:10:06 +0800

    DSA

--- DSA/src/main/java/dev/Innocent/refresh/KadaneAlgorithm.java
+++ DSA/src/main/java/dev/Innocent/refresh/KadaneAlgorithm.java
@@ -1,9 +1,22 @@
 package dev.Innocent.refresh;
 
 public class KadaneAlgorithm {
+    public static void main(String[] args) {
+        int[] numbers = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+        int maxSum = maxSubArraySum(numbers);
+        System.out.println("Maximum subarray sum: " + maxSum);
+    }
 
     // Method to find the maximum subarray sum
     public static int maxSubArraySum(int[] arr){
+        int maxSoFar = arr[0];
+        int currentSum = arr[0];
 
+        for (int i = 1; i < arr.length; i++) {
+            currentSum = Math.max(arr[i], currentSum + arr[i]);
+            maxSoFar = Math.max(maxSoFar, currentSum);
+        }
+        return maxSoFar;
     }
 }
