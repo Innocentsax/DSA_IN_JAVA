f641787229466f46f3179d0b58fca30c43010eab
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-10-19 20:43:38 +0800

    DSA

--- DSA/src/main/java/dev/Innocent/refresh/RotateArray.java
+++ DSA/src/main/java/dev/Innocent/refresh/RotateArray.java
@@ -3,7 +3,8 @@ package dev.Innocent.refresh;
 public class RotateArray {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5, 6, 7};
-        reverse(numbers);
+        int k = 3;
+        rotateArray(numbers, k);
 
         for(int a : numbers){
             System.out.print(a);
@@ -13,6 +14,25 @@ public class RotateArray {
     public static void reverse(int[] arr){
         int start = 0;
         int end = arr.length - 1;
+        while (start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void rotateArray(int[] arr, int k){
+        int n = arr.length;
+        k = k % n;
+
+        reverse(arr, 0, n - 1);
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, n - 1);
+    }
+
+    public static void reverse(int[] arr, int start, int end){
         while (start < end){
             int temp = arr[start];
             arr[start] = arr[end];
