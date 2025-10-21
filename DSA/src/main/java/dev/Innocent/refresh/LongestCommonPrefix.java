a7c8a23564af1121cea1cc92a2154a45f6e837a6
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-10-21 23:12:07 +0800

    DSA

--- DSA/src/main/java/dev/Innocent/refresh/LongestCommonPrefix.java
+++ DSA/src/main/java/dev/Innocent/refresh/LongestCommonPrefix.java
@@ -6,7 +6,16 @@ public class LongestCommonPrefix {
     }
 
     public static String longestCommonPrefix(String[] strs) {
+        if(strs == null || strs.length == 0) return "";
 
-        return "";
+        for(int i = 0; i < strs[0].length(); i++){
+            char c = strs[0].charAt(i);
+            for(int j = 1; j < strs.length; j++){
+                if(i == strs[j].length() || strs[j].charAt(i) != c){
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
     }
 }
