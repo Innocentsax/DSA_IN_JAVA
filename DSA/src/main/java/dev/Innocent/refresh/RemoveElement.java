c2ea6f0c393e421a6cce9bb5540ecba162cb5e60
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-10-24 20:18:15 +0800

    DSA

--- DSA/src/main/java/dev/Innocent/refresh/RemoveElement.java
+++ DSA/src/main/java/dev/Innocent/refresh/RemoveElement.java
@@ -1,7 +1,20 @@
 package dev.Innocent.refresh;
 
+// https://leetcode.com/problems/remove-element/description/?envType=problem-list-v2&envId=array
 public class RemoveElement {
     public static void main(String[] args) {
+        int[] nums = {3,2,2,3}; int val = 3;
+        
+    }
 
+    public static int removeElement(int[] arr, int val){
+        int k = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] != val){
+                arr[k] = arr[i];
+                k++;
+            }
+        }
+        return k;
     }
 }
