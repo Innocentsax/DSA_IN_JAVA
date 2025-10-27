2daa267db1389a4057469b2f83ac4fe773cbe0ce
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-10-27 23:26:28 +0800

    DSA

--- DSA/src/main/java/dev/Innocent/refresh/PlusOne.java
+++ DSA/src/main/java/dev/Innocent/refresh/PlusOne.java
@@ -21,8 +21,8 @@ public class PlusOne {
     }
 
     public static int[] plusOne(int[] digits){
-        for(int i = digits.length - 1; i >= 0; i++){
-            if(digits[i] == 9){
+        for(int i = digits.length - 1; i >= 0; i--){
+            if(digits[i] < 9){
                 digits[i]++;
                 return digits;
             }
