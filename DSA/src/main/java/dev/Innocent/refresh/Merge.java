c14dfe03fa9cb9ed1a86969af62e9f7568df1f16
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-10-29 00:12:09 +0800

    DSA

--- DSA/src/main/java/dev/Innocent/refresh/Merge.java
+++ DSA/src/main/java/dev/Innocent/refresh/Merge.java
@@ -1,11 +1,32 @@
 package dev.Innocent.refresh;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Merge {
     public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int m = 3;
+        int[] nums2 = {2, 5, 6};
+        int n = 3;
 
+        List<Integer> result = merge(nums1, m, nums2, n);
+        System.out.println(result);
     }
 
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
+    public static List<Integer> merge(int[] nums1, int m, int[] nums2, int n) {
+        List<Integer> merged = new ArrayList<>();
 
+        for (int i = 0; i < m; i++) {
+            merged.add(nums1[i]);
+        }
+
+        for (int i = 0; i < n; i++) {
+            merged.add(nums2[i]);
+        }
+
+        Collections.sort(merged);
+        return merged;
     }
 }
