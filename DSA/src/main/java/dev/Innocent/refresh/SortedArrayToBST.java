b74c9cb4a6a083c3043f18666b04cd3811d8b036
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-10-31 21:42:26 +0800

    DSA

--- DSA/src/main/java/dev/Innocent/refresh/SortedArrayToBST.java
+++ DSA/src/main/java/dev/Innocent/refresh/SortedArrayToBST.java
@@ -1,7 +1,13 @@
 package dev.Innocent.refresh;
 
+import javax.swing.tree.TreeNode;
+
 public class SortedArrayToBST {
     public static void main(String[] args) {
+
+    }
+
+    public static TreeNode sortedArrayToBST(int[] nums) {
 
     }
 
