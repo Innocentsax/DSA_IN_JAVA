7dd89c595e36c8fedc99b7865e48eeda4647a7b7
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-11-02 18:38:06 +0800

    DSA

--- DSA/src/main/java/dev/Innocent/refresh/SortedArrayToBST.java
+++ DSA/src/main/java/dev/Innocent/refresh/SortedArrayToBST.java
@@ -2,6 +2,7 @@ package dev.Innocent.refresh;
 
 import javax.swing.tree.TreeNode;
 
+//https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/description/?envType=problem-list-v2&envId=array
 public class SortedArrayToBST {
     public static void main(String[] args) {
         System.out.println();
@@ -9,6 +10,13 @@ public class SortedArrayToBST {
 
     public static TreeNode sortedArrayToBST(int[] nums) {
 
+    }
+
+    public TreeNode CreateBST(int[] nums, int l, int r){
+        if(l > r){
+            return null;
+        }
+        int mid = l + (r - l) / 2;
     }
 
 }
