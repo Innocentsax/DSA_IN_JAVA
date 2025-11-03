c0e82755c8a271036eff3553f4b9f073af4d54fc
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-11-03 22:03:15 +0800

    DSA

---
+++ DSA/src/main/java/dev/Innocent/refresh/TreeNode.java
@@ -0,0 +1,11 @@
+package dev.Innocent.refresh;
+
+public class TreeNode {
+    int val;
+    TreeNode left, right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val; this.left = left; this.right = right;
+    }
+}
