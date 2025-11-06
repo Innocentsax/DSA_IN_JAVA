9061548a770c05698e317062f9a1dc0f6aa88e1c
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-11-06 20:45:46 +0800

    DSA

--- DSA/src/main/java/dev/Innocent/refresh/PascalTriangle.java
+++ DSA/src/main/java/dev/Innocent/refresh/PascalTriangle.java
@@ -1,8 +1,14 @@
 package dev.Innocent.refresh;
 
+import java.util.List;
+
 //https://leetcode.com/problems/pascals-triangle/description/?envType=problem-list-v2&envId=array
 public class PascalTriangle {
     public static void main(String[] args) {
 
+    }
+
+    public static List<List<Integer>> generate(int numRows) {
+        return null;
     }
 }
