84e2698b41a7ba56488741263c66e290b4b838e8
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-11-08 22:15:11 +0800

    DSA

--- DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/ExecutorChallenge/ShoeWarehouse.java
+++ DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/ExecutorChallenge/ShoeWarehouse.java
@@ -2,15 +2,25 @@ package dev.Innocent.Section8.ConcurrencyAndMultithreading.ExecutorChallenge;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ShoeWarehouse {
 
     private List<Order> shippingItems;
+
+    private final ExecutorService fulfillmentService;
+
     public final static String[] PRODUCT_LIST =
             {"Running Shoes", "Sandals", "Boots", "Slippers", "High Tops"};
 
     public ShoeWarehouse() {
         this.shippingItems = new ArrayList<>();
+        fulfillmentService = Executors.newFixedThreadPool(3);
+    }
+
+    public void shutDown(){
+        fulfillmentService.shutdown();
     }
 
     public synchronized void receiveOrder(Order item) {
@@ -23,7 +33,8 @@ public class ShoeWarehouse {
             }
         }
         shippingItems.add(item);
-        System.out.println("Incoming: " + item);
+        System.out.println(Thread.currentThread().getName() + "Incoming: " + item);
+        fulfillmentService.submit(this::fulfillOrder);
         notifyAll();
     }
 
