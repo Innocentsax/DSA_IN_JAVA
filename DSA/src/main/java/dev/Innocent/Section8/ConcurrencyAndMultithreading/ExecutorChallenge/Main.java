8f9922e781ccb77333cf4f781a6a84375e686a11
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-11-08 21:35:55 +0800

    DSA

---
+++ DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/ExecutorChallenge/Main.java
@@ -0,0 +1,34 @@
+package dev.Innocent.Section8.ConcurrencyAndMultithreading.ExecutorChallenge;
+
+import java.util.Random;
+
+record Order(long orderId, String item, int qty) {
+};
+
+public class Main {
+
+    private static final Random random = new Random();
+
+    public static void main(String[] args) {
+
+        ShoeWarehouse warehouse = new ShoeWarehouse();
+        Thread producerThread = new Thread(() -> {
+            for (int j = 0; j < 10; j++) {
+                warehouse.receiveOrder(new Order(
+                        random.nextLong(1000000, 9999999),
+                        ShoeWarehouse.PRODUCT_LIST[random.nextInt(0, 5)],
+                        random.nextInt(1, 4)));
+            }
+        });
+        producerThread.start();
+
+        for (int i = 0; i < 2; i++) {
+            Thread consumerThread = new Thread(() -> {
+                for (int j = 0; j < 5; j++) {
+                    Order item = warehouse.fulfillOrder();
+                }
+            });
+            consumerThread.start();
+        }
+    }
+}
