78a3211fc84056529700f3e78f01223ffe03692c
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-11-08 21:35:59 +0800

    DSA

---
+++ DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/ExecutorChallenge/ShoeWarehouse.java
@@ -0,0 +1,44 @@
+package dev.Innocent.Section8.ConcurrencyAndMultithreading.ExecutorChallenge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoeWarehouse {
+
+    private List<Order> shippingItems;
+    public final static String[] PRODUCT_LIST =
+            {"Running Shoes", "Sandals", "Boots", "Slippers", "High Tops"};
+
+    public ShoeWarehouse() {
+        this.shippingItems = new ArrayList<>();
+    }
+
+    public synchronized void receiveOrder(Order item) {
+
+        while (shippingItems.size() > 20) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        shippingItems.add(item);
+        System.out.println("Incoming: " + item);
+        notifyAll();
+    }
+
+    public synchronized Order fulfillOrder() {
+
+        while (shippingItems.isEmpty()) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        Order item = shippingItems.remove(0);
+        System.out.println(Thread.currentThread().getName() + " Fulfilled: " + item);
+        notifyAll();
+        return item;
+    }
+}
