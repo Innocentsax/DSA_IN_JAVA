68519b3717871a7e149975d8005dd25f762ff9bd
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-11-09 22:30:06 +0800

    DSA

--- DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/SchedulingTasks/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/SchedulingTasks/Main.java
@@ -1,7 +1,26 @@
 package dev.Innocent.Section8.ConcurrencyAndMultithreading.SchedulingTasks;
 
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
     public static void main(String[] args) {
+        var dtf = DateTimeFormatter.ofLocalizedDateTime(
+                FormatStyle.MEDIUM, FormatStyle.LONG
+        );
 
+        Callable<ZonedDateTime> waitThenDoIt = () -> {
+            ZonedDateTime zdt = null;
+            try{
+                TimeUnit.SECONDS.sleep(2);
+                zdt = ZonedDateTime.now();
+            } catch (InterruptedException e){
+                throw new RuntimeException(e);
+            }
+            return zdt;
+        }
     }
 }
