35fabeb2813921a0de50b53860b25e276ec3940f
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-11-11 23:05:42 +0800

    DSA

--- DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/SchedulingTasks/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/SchedulingTasks/Main.java
@@ -5,10 +5,7 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -32,8 +29,17 @@ public class Main {
         try{
             System.out.println("--->" + ZonedDateTime.now().format(dtf));
             List<Future<ZonedDateTime>> futureList = threadPool.invokeAll(list);
+            for(Future<ZonedDateTime> result : futureList){
+                try {
+                    System.out.println(result.get(1, TimeUnit.SECONDS).format(dtf));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
+        } finally {
+            threadPool.shutdown();
         }
     }
 }
