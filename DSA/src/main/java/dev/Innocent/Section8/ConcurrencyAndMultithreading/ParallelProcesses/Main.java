4922b4be5453cb158ece532f85e8c59e989f8ec4
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-11-15 21:44:32 +0800

    DSA

--- DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/ParallelProcesses/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/ParallelProcesses/Main.java
@@ -1,11 +1,23 @@
 package dev.Innocent.Section8.ConcurrencyAndMultithreading.ParallelProcesses;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) {
 
         int numbersLength = 100_000;
         long[] numbers = new Random().longs(numbersLength, 1, numbersLength).toArray();
+
+        long sum = Arrays.stream(numbers).sum();
+        System.out.println("sum = " + sum);
+
+        ExecutorService threadPool = Executors.newWorkStealingPool(4);
+        List<Callable<Long>> tasks = new ArrayList<>();
     }
 }
