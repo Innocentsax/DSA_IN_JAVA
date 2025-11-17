68b1d0e2ae9aa678cc6d1e2051af680227c09b3d
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-11-17 19:44:18 +0800

    DSA

--- DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/ParallelProcesses/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/ParallelProcesses/Main.java
@@ -4,10 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -18,7 +15,7 @@ public class Main {
         long sum = Arrays.stream(numbers).sum();
         System.out.println("sum = " + sum);
 
-        ExecutorService threadPool = Executors.newWorkStealingPool(4);
+        ForkJoinPool threadPool = (ForkJoinPool) Executors.newWorkStealingPool(4);
         List<Callable<Long>> tasks = new ArrayList<>();
 
         int taskNo = 10;
