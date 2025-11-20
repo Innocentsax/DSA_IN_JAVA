92a8164af19af473b129480ca4204c76e7360f10
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-11-20 23:17:11 +0800

    DSA

--- DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/ParallelStreams/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/ParallelStreams/Main.java
@@ -1,7 +1,12 @@
 package dev.Innocent.Section8.ConcurrencyAndMultithreading.ParallelStreams;
 
+
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         int numberLength = 100_000;
+        long[] number = new Random().longs(numberLength, 1, numberLength).toArray();
+        long start = System.nanoTime();
     }
 }
