e40507470842918eae831610b0f6e7998ecd26ad
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-11-23 17:50:59 +0800

    DSA

--- DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/ParallelStreams/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/ParallelStreams/Main.java
@@ -8,15 +8,21 @@ public class Main {
     public static void main(String[] args) {
         int numberLength = 100_000;
         long[] number = new Random().longs(numberLength, 1, numberLength).toArray();
-        long start = System.nanoTime();
-        double averageSerial = Arrays.stream(number).average().orElseThrow();
-        long elapsedSerial = System.nanoTime() - start;
-        System.out.printf("Ave = %.2f , elapsed = %d nanos or %.2f ms%n", averageSerial, elapsedSerial, elapsedSerial/1000000.0);
 
-        start = System.nanoTime();
-        double averageParallel = Arrays.stream(number).parallel().average().orElseThrow();
-        long elapsedParallel = System.nanoTime() - start;
-        System.out.printf("Ave = %.2f , elapsed = %d nanos or %.2f ms%n",
-                averageParallel, elapsedParallel, elapsedParallel/1000000.0);
+        long delta = 0;
+        int iteration = 100;
+
+        for (int i = 0; i < iteration; i++) {
+            long start = System.nanoTime();
+            double averageSerial = Arrays.stream(number).average().orElseThrow();
+            long elapsedSerial = System.nanoTime() - start;
+//            System.out.printf("Ave = %.2f , elapsed = %d nanos or %.2f ms%n", averageSerial, elapsedSerial, elapsedSerial / 1000000.0);
+            start = System.nanoTime();
+            double averageParallel = Arrays.stream(number).parallel().average().orElseThrow();
+            long elapsedParallel = System.nanoTime() - start;
+//            System.out.printf("Ave = %.2f , elapsed = %d nanos or %.2f ms%n", averageParallel, elapsedParallel, elapsedParallel / 1000000.0);
+            
+
+        }
     }
 }
