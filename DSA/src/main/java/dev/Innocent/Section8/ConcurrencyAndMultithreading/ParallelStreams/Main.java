9253ccc22949c4e42343fb24c5be368681b3db1a
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-11-23 17:56:57 +0800

    DSA

--- DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/ParallelStreams/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/ParallelStreams/Main.java
@@ -6,11 +6,11 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        int numberLength = 100_000;
+        int numberLength = 100_000_000;
         long[] number = new Random().longs(numberLength, 1, numberLength).toArray();
 
         long delta = 0;
-        int iteration = 100;
+        int iteration = 25;
 
         for (int i = 0; i < iteration; i++) {
             long start = System.nanoTime();
@@ -21,8 +21,9 @@ public class Main {
             double averageParallel = Arrays.stream(number).parallel().average().orElseThrow();
             long elapsedParallel = System.nanoTime() - start;
 //            System.out.printf("Ave = %.2f , elapsed = %d nanos or %.2f ms%n", averageParallel, elapsedParallel, elapsedParallel / 1000000.0);
-            
 
+            System.out.printf("Parallel is [%d] nanos or [%.2f] ms faster on average %n",
+                    delta / iteration, delta / 1000000.0 / iteration);
         }
     }
 }
