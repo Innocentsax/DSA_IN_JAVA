397943c082df8cdc80127d53969e00218b00474f
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-12-03 09:18:37 +0800

    DSA

--- DSA/src/main/java/dev/Innocent/refresh/ValidParentheses.java
+++ DSA/src/main/java/dev/Innocent/refresh/ValidParentheses.java
@@ -7,6 +7,7 @@ import java.util.Stack;
 public class ValidParentheses {
     public static void main(String[] args) {
         System.out.println(isValid("() [] {}"));
+        System.out.println(isValid("(}"));
     }
 
     public static boolean isValid(String s){
