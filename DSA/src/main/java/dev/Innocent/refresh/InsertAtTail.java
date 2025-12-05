1db4b92784e8cd71de5191caa175f13fe50a5808
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-12-05 22:59:37 +0800

    DSA

---
+++ DSA/src/main/java/dev/Innocent/refresh/InsertAtTail.java
@@ -0,0 +1,12 @@
+package dev.Innocent.refresh;
+
+public class InsertAtTail {
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode insertAtTail(ListNode head, int value){
+
+        return head;
+    }
+}
