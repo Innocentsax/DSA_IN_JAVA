cd1492a9e0ffb172bad092c6c5128c2fbe884dd3
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-12-07 13:52:47 +0800

    DSA

---
+++ DSA/src/main/java/dev/Innocent/refresh/Reverse.java
@@ -0,0 +1,16 @@
+package dev.Innocent.refresh;
+
+public class Reverse {
+
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+
+}
