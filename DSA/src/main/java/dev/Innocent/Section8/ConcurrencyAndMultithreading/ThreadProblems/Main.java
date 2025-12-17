40d05f7b0b7b07a91885c6790c73e6e363fae4ae
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-12-17 09:59:49 +0800

    DSA

--- DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/ThreadProblems/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/ThreadProblems/Main.java
@@ -7,6 +7,18 @@ public class Main {
         File resourceA = new File("inputData.csv");
         File resourceB = new File("outputData.json");
 
-        
+        Thread thread = new Thread(() -> {
+            String threadName = Thread.currentThread().getName();
+            System.out.println(threadName + "attempting to lock resourcesA (csv)");
+            synchronized (resourceA){
+                System.out.println(threadName + " has lock on resourceA (csv)");
+                try{
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println(threadName + " has released lock on resourceA (csv)");
+        }, "THREAD-A");
     }
 }
