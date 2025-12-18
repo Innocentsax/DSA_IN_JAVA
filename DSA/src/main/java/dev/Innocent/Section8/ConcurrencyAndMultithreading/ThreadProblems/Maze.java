d3e2a1893107075d6e140de56f9c38b6ae27910a
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-12-19 07:56:54 +0800

    DSA

--- DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/ThreadProblems/Maze.java
+++ DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/ThreadProblems/Maze.java
@@ -1,4 +1,14 @@
 package dev.Innocent.Section8.ConcurrencyAndMultithreading.ThreadProblems;
 
+import java.util.Arrays;
+
 public class Maze {
+    public static final int  MAZE_SIZE = 4;
+    private final String[][] cells = new String[MAZE_SIZE][MAZE_SIZE];
+
+    public Maze(){
+        for(var row : cells){
+            Arrays.fill(row, "");
+        }
+    }
 }
