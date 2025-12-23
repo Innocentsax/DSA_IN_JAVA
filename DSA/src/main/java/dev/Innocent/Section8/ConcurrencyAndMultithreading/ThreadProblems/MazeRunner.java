dd7a6946b11ec7eb8414c0648f4c9f9f8e2cd748
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-12-23 22:32:05 +0800

    DSA

--- DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/ThreadProblems/MazeRunner.java
+++ DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/ThreadProblems/MazeRunner.java
@@ -1,10 +1,50 @@
 package dev.Innocent.Section8.ConcurrencyAndMultithreading.ThreadProblems;
 
+import java.util.Arrays;
+
 record Participant(String name, String searchingFor, Maze maze, int[] startingPosition){
     Participant{
         maze.moveLocation(startingPosition[0], startingPosition[1], name);
     }
 }
 
+class ParticipantThread extends Thread{
+    public final Participant participant;
+
+    public ParticipantThread(Participant participant) {
+        super(participant.name());
+        this.participant = participant;
+    }
+
+    @Override
+    public void run(){
+        int[] lastSpot = participant.startingPosition();
+        Maze maze = participant.maze();
+
+        while (true){
+            int[] newSpot = maze.getNextLocation(lastSpot);
+            try{
+                Thread.sleep(500);
+                if(maze.searchCell(participant.searchingFor(), newSpot, lastSpot)){
+                    System.out.printf("%s found %s at %s! %n", participant.name(),
+                            participant.searchingFor(), Arrays.toString(newSpot));
+                    break;
+                }
+                synchronized (maze){
+                    maze.moveLocation(newSpot[0], newSpot[1], participant.name());
+                }
+                lastSpot = new int[]{newSpot[0], newSpot[1]};
+            } catch (InterruptedException e){
+                Thread.currentThread().interrupt();
+                return;
+            }
+            System.out.println(participant.name() + " searching " + participant.maze());
+        }
+    }
+}
+
 public class MazeRunner {
+    public static void main(String[] args) {
+
+    }
 }
