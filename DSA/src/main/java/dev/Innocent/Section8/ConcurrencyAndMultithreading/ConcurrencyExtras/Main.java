1e22a2210e656315b33f3d7f644fd879a5668af0
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-12-25 10:06:19 +0800

    DSA

--- DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/ConcurrencyExtras/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/ConcurrencyAndMultithreading/ConcurrencyExtras/Main.java
@@ -3,6 +3,7 @@ package dev.Innocent.Section8.ConcurrencyAndMultithreading.ConcurrencyExtras;
 import java.util.Collections;
 import java.util.Random;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 record Student(String name, int enrolledYear, int studentId)
     implements Comparable<Student>{
@@ -24,13 +25,25 @@ class StudentId{
     }
 }
 
+class AtomicStudentId {
+    private final AtomicInteger nextId = new AtomicInteger(0);
+
+    public int getId(){
+        return nextId.get();
+    }
+
+    public int getNextId(){
+        return nextId.incrementAndGet();
+    }
+}
+
 public class Main {
     private static Random random = new Random();
 
     private static ConcurrentSkipListSet<Student> studentSet = new ConcurrentSkipListSet<>();
 
     public static void main(String[] args) {
-        StudentId idGenerator = new StudentId();
+        AtomicStudentId idGenerator = new AtomicStudentId();
         Callable<Student> studentMaker = () -> {
             int studentId = idGenerator.getNextId();
             Student student = new Student("Udo " + studentId, random.nextInt(2018, 2025), studentId);
