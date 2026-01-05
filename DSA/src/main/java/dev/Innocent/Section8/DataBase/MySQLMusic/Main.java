e0a8354368c90c5baa1792b37d60e6f2db58b427
INNOCENT UDO <innocentcharlesudo@gmail.com>
2026-01-05 21:36:03 +0800

    Data base

--- DSA/src/main/java/dev/Innocent/Section8/DataBase/MySQLMusic/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/DataBase/MySQLMusic/Main.java
@@ -1,6 +1,10 @@
 package dev.Innocent.Section8.DataBase.MySQLMusic;
 
 import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Arrays;
 
 public class Main {
 
@@ -13,5 +17,13 @@ public class Main {
         int okCxl = JOptionPane.showConfirmDialog(null, pf,
                 "Enter DB Password", JOptionPane.OK_CANCEL_OPTION);
         final char[] password = (okCxl == JOptionPane.OK_OPTION) ? pf.getPassword() : null;
+
+        try(Connection connection = DriverManager.getConnection(
+                CONN_STRING, username, String.valueOf(password))){
+            System.out.println("Success!! Connection made to the music database");
+            Arrays.fill(password, ' ');
+        } catch (SQLException e){
+            throw new RuntimeException(e);
+        }
     }
 }
