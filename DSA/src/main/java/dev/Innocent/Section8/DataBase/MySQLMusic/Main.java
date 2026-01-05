bd7f1ab5984720b6a46683d336254451017a1007
INNOCENT UDO <innocentcharlesudo@gmail.com>
2026-01-05 21:22:33 +0800

    Kafka

--- DSA/src/main/java/dev/Innocent/Section8/DataBase/MySQLMusic/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/DataBase/MySQLMusic/Main.java
@@ -1,7 +1,17 @@
 package dev.Innocent.Section8.DataBase.MySQLMusic;
 
-public class Main {
-    public static void main(String[] args) {
+import javax.swing.*;
 
+public class Main {
+
+    private final static String CONN_STRING = "jdbc:mysql://localhost:3306/music";
+
+    public static void main(String[] args) {
+        String username = JOptionPane.showInputDialog(null, "Enter DB Username");
+
+        JPasswordField pf = new JPasswordField();
+        int okCxl = JOptionPane.showConfirmDialog(null, pf,
+                "Enter DB Password", JOptionPane.OK_CANCEL_OPTION);
+        final char[] password = (okCxl == JOptionPane.OK_OPTION) ? pf.getPassword() : null;
     }
 }
