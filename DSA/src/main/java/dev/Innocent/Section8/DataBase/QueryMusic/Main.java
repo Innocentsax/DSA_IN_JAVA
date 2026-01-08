9f44a85dd61a50f7f4f4924e7d70619dd4125d74
INNOCENT UDO <innocentcharlesudo@gmail.com>
2026-01-08 20:04:33 +0800

    Data base

--- DSA/src/main/java/dev/Innocent/Section8/DataBase/QueryMusic/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/DataBase/QueryMusic/Main.java
@@ -34,12 +34,32 @@ public class Main {
         try(var connection = dataSource.getConnection(prop.getProperty("user"),
                 System.getenv("MYSQL_PASS")); Statement statement = connection.createStatement();)
         {
+
             ResultSet resultSet = statement.executeQuery(query);
+
+            var meta = resultSet.getMetaData();
+            for(int i = 1; i <= meta.getColumnCount(); i++){
+                System.out.printf("%d %s %s%n",
+                        i,
+                        meta.getColumnName(i),
+                        meta.getColumnTypeName(i));
+            }
+            System.out.println("======================");
+
+            for(int i = 1; i <= meta.getColumnCount(); i++){
+                System.out.printf("%-15s", meta.getColumnName(i).toUpperCase());
+            }
+            System.out.println();
             while (resultSet.next()){
-                System.out.printf("%d %s %s %n",
-                        resultSet.getInt("track_number"),
-                        resultSet.getString("artist_name"),
-                        resultSet.getString("song_title"));
+//                System.out.printf("%d %s %s %n",
+//                        resultSet.getInt("track_number"),
+//                        resultSet.getString("artist_name"),
+//                        resultSet.getString("song_title"));
+
+                for(int i = 1; i <= meta.getColumnCount(); i++){
+                    System.out.printf("%-15s", resultSet.getString(i));
+                }
+                System.out.println();
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
