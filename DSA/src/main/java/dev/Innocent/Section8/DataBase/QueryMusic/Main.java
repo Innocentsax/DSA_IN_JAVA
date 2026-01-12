cf7dddb488a77a9ff4b4e3caaebc9b78c5127060
INNOCENT UDO <innocentcharlesudo@gmail.com>
2026-01-12 21:01:58 +0800

    Data base

--- DSA/src/main/java/dev/Innocent/Section8/DataBase/QueryMusic/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/DataBase/QueryMusic/Main.java
@@ -17,62 +17,53 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Properties prop = new Properties();
-        try{
-            prop.load(Files.newInputStream(Path.of("music.properties"), StandardOpenOption.READ));
+        Properties props = new Properties();
+        try {
+            props.load(Files.newInputStream(Path.of("music.properties"),
+                    StandardOpenOption.READ));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-//        String albumName = "Tapestry";
-
-//        String query = "SELECT * FROM music.artists";
-
         var dataSource = new MysqlDataSource();
-        dataSource.setServerName(prop.getProperty("serverName"));
-        dataSource.setPort(Integer.parseInt(prop.getProperty("port")));
-        dataSource.setDatabaseName(prop.getProperty("databaseName"));
+        dataSource.setServerName(props.getProperty("serverName"));
+        dataSource.setPort(Integer.parseInt(props.getProperty("port")));
+        dataSource.setDatabaseName(props.getProperty("databaseName"));
         try {
             dataSource.setMaxRows(10);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Enter an Artist Id: ");
-//        String artistId = scanner.nextLine();
-//        int artistid = Integer.parseInt(artistId);
         String query = "SELECT * FROM music.artists limit 10";
 
-        try(var connection = dataSource.getConnection(prop.getProperty("user"),
-                System.getenv("MYSQL_PASS")); Statement statement = connection.createStatement();)
-        {
+//        String query = """
+//            WITH RankedRows AS (
+//                                SELECT *,
+//                                ROW_NUMBER() OVER (ORDER BY artist_id) AS row_num
+//                                FROM music.artists
+//                            )
+//                            SELECT *
+//                                FROM RankedRows
+//                            WHERE row_num <= 10""";
 
+        try (var connection = dataSource.getConnection(
+                props.getProperty("user"),
+                System.getenv("MYSQL_PASS"));
+             Statement statement = connection.createStatement();
+        ) {
             ResultSet resultSet = statement.executeQuery(query);
 
             var meta = resultSet.getMetaData();
-//            for(int i = 1; i <= meta.getColumnCount(); i++){
-//                System.out.printf("%d %s %s%n",
-//                        i,
-//                        meta.getColumnName(i),
-//                        meta.getColumnTypeName(i));
-//            }
-            System.out.println("================================");
 
+            System.out.println("===================");
 
-            BigDecimal a = BigDecimal.valueOf(1000);
-
-            for(int i = 1; i <= meta.getColumnCount(); i++){
+            for (int i = 1; i <= meta.getColumnCount(); i++) {
                 System.out.printf("%-15s", meta.getColumnName(i).toUpperCase());
             }
             System.out.println();
-            while (resultSet.next()){
-//                System.out.printf("%d %s %s %n",
-//                        resultSet.getInt("track_number"),
-//                        resultSet.getString("artist_name"),
-//                        resultSet.getString("song_title"));
 
-                for(int i = 1; i <= meta.getColumnCount(); i++){
+            while (resultSet.next()) {
+                for (int i = 1; i <= meta.getColumnCount(); i++) {
                     System.out.printf("%-15s", resultSet.getString(i));
                 }
                 System.out.println();
