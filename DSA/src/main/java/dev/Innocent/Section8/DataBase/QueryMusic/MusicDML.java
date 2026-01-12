1a35bc85dc55f12ebcd12030b729ab0a0e4397fe
INNOCENT UDO <innocentcharlesudo@gmail.com>
2026-01-12 20:55:50 +0800

    Data base

--- DSA/src/main/java/dev/Innocent/Section8/DataBase/QueryMusic/MusicDML.java
+++ DSA/src/main/java/dev/Innocent/Section8/DataBase/QueryMusic/MusicDML.java
@@ -1,10 +1,31 @@
 package dev.Innocent.Section8.DataBase.QueryMusic;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class MusicDML {
     public static void main(String[] args) {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
+//        try {
+//            Class.forName("com.mysql.jdbc.Driver");
+//        } catch (ClassNotFoundException e) {
+//            throw new RuntimeException(e);
+//        }
+
+        try(Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/music",
+                System.getenv("MYSQL_USER"), System.getenv("MYSQL_PASS"));
+            Statement statement = connection.createStatement();
+        ){
+            String artist = "Neil Young";
+            String query = "SELECT * FROM artists WHERE artist_name='%s'".formatted(artist);
+            boolean result = statement.execute(query);
+            System.out.println("result = " + result);
+            var rs = statement.getResultSet();
+            boolean found = (rs != null && rs.next());
+            System.out.println("Artist was " + (found ? "found" : "not found"));
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
