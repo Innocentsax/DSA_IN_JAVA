0868a6322940f48aa21f73bdcd4b894467e10480
INNOCENT UDO <innocentcharlesudo@gmail.com>
2026-01-13 22:08:02 +0800

    Data base

--- DSA/src/main/java/dev/Innocent/Section8/DataBase/QueryMusic/MusicDML.java
+++ DSA/src/main/java/dev/Innocent/Section8/DataBase/QueryMusic/MusicDML.java
@@ -15,13 +15,22 @@ public class MusicDML {
                 System.getenv("MYSQL_USER"), System.getenv("MYSQL_PASS"));
             Statement statement = connection.createStatement();
         ){
-            String artist = "Neil Young";
-            String query = "SELECT * FROM artists WHERE artist_name='%s'".formatted(artist);
-            boolean result = statement.execute(query);
-            System.out.println("result = " + result);
-            var rs = statement.getResultSet();
-            boolean found = (rs != null && rs.next());
-            System.out.println("Artist was " + (found ? "found" : "not found"));
+//            String artist = "Neil Young";
+//            String query = "SELECT * FROM artists WHERE artist_name='%s'".formatted(artist);
+//            boolean result = statement.execute(query);
+//            System.out.println("result = " + result);
+//            var rs = statement.getResultSet();
+//            boolean found = (rs != null && rs.next());
+//            System.out.println("Artist was " + (found ? "found" : "not found"));
+
+            String tableName = "music.artists";
+            String columnName = "artist_name";
+            String columnValue = "Innocent Udo";
+            if(!executeSelect(statement, tableName, columnName, columnValue)){
+                System.out.println("Maybe we should add this record");
+                insertRecord(statement, tableName, new String[]{columnName}, new String[]{columnValue});
+
+            }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -45,5 +54,29 @@ public class MusicDML {
             foundData = true;
         }
         return foundData;
+    }
+
+    private static boolean executeSelect(Statement statement, String table,
+                                         String columnName, String columnValue) throws SQLException{
+        String query = "SELECT * FROM %s WHERE %s='%s'".formatted(table, columnName, columnValue);
+        var rs = statement.executeQuery(query);
+        if(rs != null){
+            return printRecords(rs);
+        }
+        return false;
+    }
+
+    private static boolean insertRecord(Statement statement, String table,
+                                        String[] columnNames, String[] columnValues) throws SQLException{
+        String colNames = String.join(",", columnNames);
+        String colValues = String.join("','", columnValues);
+        String query = "INSERT INTO %s (%s) VALUES ('%s')".formatted(table, colNames, colValues);
+        System.out.println(query);
+        boolean insertResult = statement.execute(query);
+        int recordsInserted = statement.getUpdateCount();
+        if(recordsInserted > 0){
+            executeSelect(statement, table, columnNames[0], columnValues[0]);
+        }
+        return recordsInserted > 0;
     }
 }
