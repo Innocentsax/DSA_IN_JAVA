4f5d3aa3bcdc1ee05e09137d22be26e27b4bcf61
INNOCENT UDO <innocentcharlesudo@gmail.com>
2026-01-26 22:42:40 +0800

    Data base

--- DSA/src/main/java/dev/Innocent/Section8/DataBase/QueryMusic/MusicDML.java
+++ DSA/src/main/java/dev/Innocent/Section8/DataBase/QueryMusic/MusicDML.java
@@ -168,5 +168,12 @@ public class MusicDML {
     private static void deleteArtistAlbum(Connection conn, Statement statement,
                                           String artistName, String albumName) throws SQLException {
         System.out.println("AUTOCOMMIT = " + conn.getAutoCommit());
+        String deleteSongs = """
+                DELETE FROM music.songs WHERE album_id = 
+                (SELECT ALBUM_ID from music.albums WHERE album_name = '%s')
+                """.formatted(albumName);
+        int deletedSongs = statement.executeUpdate(deleteSongs);
+        System.out.printf("Deleted %d rows from music.songs%n", deletedSongs);
+        String deleteAlbums = "DELETE FROM music.albums WHERE album_name='%s".formatted(albumName);
     }
 }
