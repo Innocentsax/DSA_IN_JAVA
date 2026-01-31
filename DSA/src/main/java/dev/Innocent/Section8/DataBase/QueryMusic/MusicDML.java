487b8912fb8fea6afe72997dcaade91b0c62295d
INNOCENT UDO <innocentcharlesudo@gmail.com>
2026-01-31 17:56:06 +0800

    Data base

--- DSA/src/main/java/dev/Innocent/Section8/DataBase/QueryMusic/MusicDML.java
+++ DSA/src/main/java/dev/Innocent/Section8/DataBase/QueryMusic/MusicDML.java
@@ -174,15 +174,15 @@ public class MusicDML {
                     DELETE FROM music.songs WHERE album_id = 
                     (SELECT ALBUM_ID from music.albums WHERE album_name = '%s')
                     """.formatted(albumName);
-            int deletedSongs = statement.executeUpdate(deleteSongs);
-            System.out.printf("Deleted %d rows from music.songs%n", deletedSongs);
+//            int deletedSongs = statement.executeUpdate(deleteSongs);
+//            System.out.printf("Deleted %d rows from music.songs%n", deletedSongs);
             String deleteAlbums = "DELETE FROM music.albums WHERE album_name='%s'".formatted(albumName);
-            int deletedAlbums = statement.executeUpdate(deleteAlbums);
-            System.out.printf("Deleted %d rows from music.albums%n", deletedAlbums);
+//            int deletedAlbums = statement.executeUpdate(deleteAlbums);
+//            System.out.printf("Deleted %d rows from music.albums%n", deletedAlbums);
 
-            String deleteArtist = "DELETE FROM music.artists WHERE artist_name='%s".formatted(artistName);
-            int deletedArtists = statement.executeUpdate(deleteArtist);
-            System.out.printf("Deleted %d rows from music.artists%n", deletedArtists);
+            String deleteArtist = "DELETE FROM music.artists WHERE artist_name='%s'".formatted(artistName);
+//            int deletedArtists = statement.executeUpdate(deleteArtist);
+//            System.out.printf("Deleted %d rows from music.artists%n", deletedArtists);
             conn.commit();
         } catch (SQLException e){
             e.printStackTrace();
