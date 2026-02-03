56ef2e7b8f92bfd7a919b55ff07ef9d16a083918
INNOCENT UDO <innocentcharlesudo@gmail.com>
2026-02-03 15:53:20 +0800

    Data base

--- DSA/src/main/java/dev/Innocent/Section8/DataBase/JDBCChallenges/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/DataBase/JDBCChallenges/Main.java
@@ -1,6 +1,8 @@
 package dev.Innocent.Section8.DataBase.JDBCChallenges;
 
 public class Main {
+    private static String USE_SCHEMA = "USE storefront";
+
     public static void main(String[] args) {
 
     }
