a409a95f66357d0659fad666de8d092f923bae2d
INNOCENT UDO <innocentcharlesudo@gmail.com>
2026-02-05 19:32:34 +0800

    Data base

--- DSA/src/main/java/dev/Innocent/Section8/DataBase/JDBCChallenges/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/DataBase/JDBCChallenges/Main.java
@@ -2,6 +2,10 @@ package dev.Innocent.Section8.DataBase.JDBCChallenges;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
     private static String USE_SCHEMA = "USE storefront";
 
@@ -11,5 +15,15 @@ public class Main {
         dataSource.setPort(3306);
         dataSource.setUser(System.getenv("MYSQLUSER"));
         dataSource.setPassword(System.getenv("MYSQLPASS"));
+    }
+
+    private static boolean checkSchema(Connection conn){
+        try(Statement statement = conn.createStatement()){
+            statement.execute(USE_SCHEMA);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
     }
 }
