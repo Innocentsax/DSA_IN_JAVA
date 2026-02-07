91a557fd97621bcf152a5b19e212acdf1f58b18d
INNOCENT UDO <innocentcharlesudo@gmail.com>
2026-02-07 12:06:11 +0800

    Data base

--- DSA/src/main/java/dev/Innocent/Section8/DataBase/JDBCChallenges/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/DataBase/JDBCChallenges/Main.java
@@ -10,36 +10,81 @@ import java.sql.Statement;
 public class Main {
     private static String USE_SCHEMA = "USE storefront";
 
+    private static int MYSQL_DB_NOT_FOUND = 1049;
+
     public static void main(String[] args) {
+
         var dataSource = new MysqlDataSource();
         dataSource.setServerName("localhost");
         dataSource.setPort(3306);
         dataSource.setUser(System.getenv("MYSQLUSER"));
         dataSource.setPassword(System.getenv("MYSQLPASS"));
 
-        try(Connection conn = dataSource.getConnection()){
+        try (Connection conn = dataSource.getConnection()) {
 
             DatabaseMetaData metaData = conn.getMetaData();
             System.out.println(metaData.getSQLStateType());
-
-            if(!checkSchema(conn)){
+            if (!checkSchema(conn)) {
                 System.out.println("storefront schema does not exist");
+                setUpSchema(conn);
             }
-        } catch(SQLException e){
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static boolean checkSchema(Connection conn){
-        try(Statement statement = conn.createStatement()){
+    private static boolean checkSchema(Connection conn) throws SQLException {
+
+        try (Statement statement = conn.createStatement()) {
             statement.execute(USE_SCHEMA);
         } catch (SQLException e) {
             e.printStackTrace();
             System.err.println("SQLState: " + e.getSQLState());
             System.err.println("Error Code: " + e.getErrorCode());
-            System.out.println("Message: " + e.getMessage());
-            return false;
+            System.err.println("Message: " + e.getMessage());
+
+            if (conn.getMetaData().getDatabaseProductName().equals("MySQL")
+                    && e.getErrorCode() == MYSQL_DB_NOT_FOUND) {
+                return false;
+            } else throw e;
         }
         return true;
+    }
+
+    private static void setUpSchema(Connection conn) throws SQLException {
+
+        String createSchema = "CREATE SCHEMA storefront";
+
+        String createOrder = """
+                CREATE TABLE storefront.order (
+                order_id int NOT NULL AUTO_INCREMENT,
+                order_date DATETIME NOT NULL,
+                PRIMARY KEY (order_id)
+                )""";
+
+        String createOrderDetails = """
+                CREATE TABLE storefront.order_details (
+                order_detail_id int NOT NULL AUTO_INCREMENT,
+                item_description text,
+                order_id int DEFAULT NULL,
+                PRIMARY KEY (order_detail_id),
+                KEY FK_ORDERID (order_id),
+                CONSTRAINT FK_ORDERID FOREIGN KEY (order_id)
+                REFERENCES storefront.order (order_id) ON DELETE CASCADE
+                ) """;
+
+        try (Statement statement = conn.createStatement()) {
+
+            System.out.println("Creating storefront Database");
+            statement.execute(createSchema);
+            if (checkSchema(conn)) {
+                statement.execute(createOrder);
+                System.out.println("Successfully Created Order");
+                statement.execute(createOrderDetails);
+                System.out.println("Successfully Created Order Details");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
