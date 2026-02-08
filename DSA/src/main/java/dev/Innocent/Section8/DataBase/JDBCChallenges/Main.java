21ea9f409406445d8a1ed8307f7a1ed0bc19d447
INNOCENT UDO <innocentcharlesudo@gmail.com>
2026-02-08 20:55:45 +0800

    Data base

--- DSA/src/main/java/dev/Innocent/Section8/DataBase/JDBCChallenges/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/DataBase/JDBCChallenges/Main.java
@@ -6,6 +6,8 @@ import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
     private static String USE_SCHEMA = "USE storefront";
@@ -28,6 +30,11 @@ public class Main {
                 System.out.println("storefront schema does not exist");
                 setUpSchema(conn);
             }
+
+            deleteOrder(conn, 2);
+//            int newOrder = addOrder(conn, new String[]{"shoes", "shirt", "socks"});
+//            System.out.println("New Order = " + newOrder);
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -85,6 +92,88 @@ public class Main {
             }
         } catch (SQLException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static int addOrder(Connection conn, String[] items) throws SQLException {
+
+        int orderId = -1;
+        String insertOrder = "INSERT INTO storefront.order (order_date) VALUES ('%s')";
+        String insertDetail = "INSERT INTO storefront.order_details " +
+                "(order_id, item_description) values(%d, %s)";
+
+        DateTimeFormatter dtf =
+                DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        String orderDateTime = LocalDateTime.now().format(dtf);
+        System.out.println(orderDateTime);
+        String formattedString = insertOrder.formatted(orderDateTime);
+        System.out.println(formattedString);
+
+        String insertOrderAlternative = "INSERT INTO storefront.order (order_date) " +
+                "VALUES ('%1$tF %1$tT')";
+        System.out.println(insertOrderAlternative.formatted(LocalDateTime.now()));
+
+        try (Statement statement = conn.createStatement()) {
+
+            conn.setAutoCommit(false);
+            int inserts = statement.executeUpdate(formattedString,
+                    Statement.RETURN_GENERATED_KEYS);
+
+            if (inserts == 1) {
+                var rs = statement.getGeneratedKeys();
+                if (rs.next()) {
+                    orderId = rs.getInt(1);
+                }
+            }
+
+            int count = 0;
+            for (var item : items) {
+                formattedString = insertDetail.formatted(orderId,
+                        statement.enquoteLiteral(item));
+                inserts = statement.executeUpdate(formattedString);
+                count += inserts;
+            }
+
+            if (count != items.length) {
+                orderId = -1;
+                System.out.println("Number of records inserted doesn't equal items received");
+                conn.rollback();
+            } else {
+                conn.commit();
+            }
+            conn.setAutoCommit(true);
+        } catch (SQLException e) {
+            conn.rollback();
+            throw new RuntimeException(e);
+        }
+
+        return orderId;
+    }
+
+    private static void deleteOrder(Connection conn, int orderId) throws SQLException {
+
+        String deleteOrder = "DELETE FROM %s where order_id=%d";
+        String parentQuery = deleteOrder.formatted("storefront.order", orderId);
+        String childQuery = deleteOrder.formatted("storefront.order_details",
+                orderId);
+
+        try (Statement statement = conn.createStatement()) {
+            conn.setAutoCommit(false);
+            int deletedRecords = statement.executeUpdate(childQuery);
+            System.out.printf("%d child records deleted%n", deletedRecords);
+            deletedRecords = statement.executeUpdate(parentQuery);
+            if (deletedRecords == 1) {
+                conn.commit();
+                System.out.printf("Order %d was successfully deleted%n", orderId);
+            } else {
+                conn.rollback();
+            }
+        } catch (SQLException e) {
+            conn.rollback();
+            throw new RuntimeException(e);
+        } finally {
+            conn.setAutoCommit(true);
         }
     }
 }
