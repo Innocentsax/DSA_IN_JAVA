9f21d899fef81029479ae88baee4e0f7e033481e
INNOCENT UDO <innocentcharlesudo@gmail.com>
2026-02-10 20:06:54 +0800

    Data base

--- DSA/src/main/java/dev/Innocent/Section8/DataBase/PreparedStatement/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/DataBase/PreparedStatement/Main.java
@@ -1,6 +1,15 @@
 package dev.Innocent.Section8.DataBase.PreparedStatement;
 
 public class Main {
+
+    private static String ARTIST_INSERT =
+            "INSERT INTO music.artists (artist_name) VALUES (?)";
+    private static String ALBUM_INSERT =
+            "INSERT INTO music.albums (artist_id, album_name) VALUES (?, ?)";
+    private static String SONG_INSERT =
+            "INSERT INTO music.songs (album_id, track_number, song_title) " +
+                    "VALUES (?, ?, ?)";
+
     public static void main(String[] args) {
 
     }
