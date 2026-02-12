de5fb285c1375ce4f136f8c73071856df419507d
INNOCENT UDO <innocentcharlesudo@gmail.com>
2026-02-12 08:07:30 +0800

    Data base

--- DSA/src/main/java/dev/Innocent/Section8/DataBase/PreparedStatement/Main.java
+++ DSA/src/main/java/dev/Innocent/Section8/DataBase/PreparedStatement/Main.java
@@ -38,9 +38,31 @@ public class Main {
             PreparedStatement ps = connection.prepareStatement(sql);
             ps.setString(1, "Elf");
             ResultSet resultSet = ps.executeQuery();
-//            printRecords(resultSet);
+            printRecords(resultSet);
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    private static boolean printRecords(ResultSet resultSet) throws SQLException {
+
+        boolean foundData = false;
+        var meta = resultSet.getMetaData();
+
+        System.out.println("===================");
+
+        for (int i = 1; i <= meta.getColumnCount(); i++) {
+            System.out.printf("%-15s", meta.getColumnName(i).toUpperCase());
+        }
+        System.out.println();
+
+        while (resultSet.next()) {
+            for (int i = 1; i <= meta.getColumnCount(); i++) {
+                System.out.printf("%-15s", resultSet.getString(i));
+            }
+            System.out.println();
+            foundData = true;
+        }
+        return foundData;
     }
 }
