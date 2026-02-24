dc469d3502b544ec498c8681d8ff4fb5bb9ee44a
INNOCENT UDO <innocentcharlesudo@gmail.com>
2026-02-24 21:16:56 +0800

    Data base

--- DSA/src/main/java/dev/Innocent/refresh/FindDuplicate.java
+++ DSA/src/main/java/dev/Innocent/refresh/FindDuplicate.java
@@ -24,8 +24,32 @@ public class FindDuplicate {
     }
 
     // Remove Duplicate without Set
-    private static int[] removeDuplicate(int[] arr){
-        int[] temp = new int[arr.length];
+    public static int[] removeDuplicates(int[] arr) {
+        int n = arr.length;
+        int[] temp = new int[n];
+        int index = 0;
 
+        for (int i = 0; i < n; i++) {
+            boolean isDuplicate = false;
+
+            for (int j = 0; j < index; j++) {
+                if (arr[i] == temp[j]) {
+                    isDuplicate = true;
+                    break;
+                }
+            }
+
+            if (!isDuplicate) {
+                temp[index++] = arr[i];
+            }
+        }
+
+        // create array of correct size
+        int[] result = new int[index];
+        for (int i = 0; i < index; i++) {
+            result[i] = temp[i];
+        }
+
+        return result;
     }
 }
