c7961e8af2152963867210fd23adc79c120b6227
INNOCENT UDO <innocentcharlesudo@gmail.com>
2026-03-02 20:31:56 +0800

    Data base

--- DSA/src/main/java/dev/Innocent/LeetCode/Arrays/SumZero.java
+++ DSA/src/main/java/dev/Innocent/LeetCode/Arrays/SumZero.java
@@ -8,6 +8,8 @@ class SumZero {
         int n = 5;
         System.out.println(Arrays.toString(sumZero(n)));
     }
+
+    // This method is to create an array of 5 element that will equal 0
     public static int[] sumZero(int n) {
         int[] newArr = new int[n];
         for (int i = 0; i < n; i++) {
